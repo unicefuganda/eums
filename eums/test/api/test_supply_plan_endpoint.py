a96b65039a293c3b48091cf4b7bf695c64dfb41e
wanderanimrod <wanderanimrod@gmail.com>
2014-09-08 15:00:35 +0300

    [nim] #X Extracts api endpoint urls. Adds test for getting supply plans

--- eums/test/api/test_supply_plan_endpoint.py
+++ eums/test/api/test_supply_plan_endpoint.py
@@ -1,13 +1,30 @@
 from rest_framework.test import APITestCase
+from eums.test.config import BACKEND_URL
+
+
+ENDPOINT_URL = BACKEND_URL + 'supply-plan/'
 
 
 class SupplyPlanEndpointTest(APITestCase):
     def test_should_create_supply_plan(self):
-        data = {'program_name': 'test_program'}
-        response = self.client.post('/api/supply-plan/', data, format='json')
-        self.assertEqual(response.status_code, 201)
-        self.assertDictContainsSubset({'program_name': 'test_program'}, response.data)
+        data = {'program_name': 'SAFE'}
 
-    def tearDown(self):
-        # clean up after yourself!
-        pass
+        response = self.client.post(ENDPOINT_URL, data, format='json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertDictContainsSubset(data, response.data)
+
+    def test_should_get_all_supply_plans(self):
+        plan_details = {'program_name': 'SAFE'}
+        self.add_supply_plan(plan_params=plan_details)
+
+        response = self.client.get(ENDPOINT_URL)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictContainsSubset(plan_details, response.data[0])
+
+    def add_supply_plan(self, plan_params=None):
+        if not plan_params:
+            plan_params = {'program_name': 'SAFE'}
+        response = self.client.post(ENDPOINT_URL, plan_params, format='json')
+        return response
