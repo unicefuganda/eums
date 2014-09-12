e2ce14fb56ba69bb244b2bb457a14491b592a059
wanderanimrod <wanderanimrod@gmail.com>
2014-09-12 10:36:07 +0300

    [nim, brain] #3 Tests app routes \o/

---
+++ eums/client/test/spec/app-spec.js
@@ -0,0 +1,24 @@
+describe('Route Provider', function() {
+    var routes;
+
+    beforeEach(function() {
+        module("eums");
+
+        inject(function($route) {
+            routes = $route;
+        })
+    });
+
+    it('should have a home route', function() {
+        expect((Object.keys(routes.routes))).toContain('/');
+        expect(routes.routes['/'].controller).toBe('HomeController');
+        expect(routes.routes['/'].templateUrl).toBe('/static/app/views/home.html');
+    });
+
+    it('should know distribution plan route exists', function() {
+        expect((Object.keys(routes.routes))).toContain('/distribution-planning');
+        expect(routes.routes['/distribution-planning'].controller).toBe('DistributionPlanController');
+        expect(routes.routes['/distribution-planning'].templateUrl).toBe('/static/app/views/distribution-planning.html');
+    });
+
+});
