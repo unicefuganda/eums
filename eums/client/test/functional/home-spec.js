8fc50a7dbb779ebb2c9fdfc01a5a4fe0ac18cef2
Tunji Sunmonu <tunji.sunmonu@gmail.com>
2014-09-15 11:26:24 +0300

    [Tunji & Brain] revert the removal of bower synchronisattion flag

--- eums/client/test/functional/home-spec.js
+++ eums/client/test/functional/home-spec.js
@@ -3,13 +3,14 @@
 describe('supply plan', function () {
     var supplyPlanPage;
 
+
     beforeEach(function () {
         supplyPlanPage = require('./pages/home-page');
+        browser.ignoreSynchronization = true;
     });
-
+    
     it('should go to home page', function () {
-//        browser.ignoreSynchronization = true;
-        browser.driver.get('/');
+        browser.get('/');
         expect(supplyPlanPage.elementisPresent()).toBeTruthy();
     });
 });
