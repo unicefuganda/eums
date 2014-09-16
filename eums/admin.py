bb077e1a83599d1aac8c049cd93857f2ba6f564b
wanderanimrod <wanderanimrod@gmail.com>
2014-09-16 12:08:27 +0300

    [nim, brain] #X Makes all models we have so far editable with the admin site

--- eums/admin.py
+++ eums/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from eums.models import SupplyPlan
+from eums.models import SupplyPlan, DistributionPlan, Programme, DistributionPlanItem, Item, ItemUnit, Consignee
 
 admin.site.register(SupplyPlan)
+admin.site.register(DistributionPlan)
+admin.site.register(Programme)
+admin.site.register(DistributionPlanItem)
+admin.site.register(Item)
+admin.site.register(ItemUnit)
+admin.site.register(Consignee)
