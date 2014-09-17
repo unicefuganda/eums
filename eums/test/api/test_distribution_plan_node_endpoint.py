fff42be6ada0587ea412a628a3b704d057511256
wanderanimrod <wanderanimrod@gmail.com>
2014-09-17 21:48:50 +0300

    [nim] #8 Creates distribution plan node with parent

--- eums/test/api/test_distribution_plan_node_endpoint.py
+++ eums/test/api/test_distribution_plan_node_endpoint.py
@@ -12,10 +12,27 @@ class DistributionPlanNodeEndpoint(APITestCase):
         plan_id = create_distribution_plan(self)
         node_details = {'distribution_plan': plan_id}
 
-        response = self.client.post(ENDPOINT_URL, node_details, format='json')
+        created_node_data = self.create_distribution_plan_node(node_details)
 
-        self.assertEqual(response.status_code, 201)
-        self.assertDictContainsSubset(node_details, response.data)
+        self.assertDictContainsSubset(node_details, created_node_data)
 
     def test_should_create_distribution_plan_node_with_parent_node(self):
+        parent_node = self.create_distribution_plan_node()
+        plan_id = create_distribution_plan(self)
+        child_node_data = {'distribution_plan': plan_id, 'parent': parent_node['id']}
+
+        created_node_details = self.create_distribution_plan_node(child_node_data)
+
+        self.assertDictContainsSubset(child_node_data, created_node_details)
+
+    def test_should_get_distribution_plan_node_with_children(self):
         pass
+
+    def create_distribution_plan_node(self, node_details=None):
+        plan_id = create_distribution_plan(self)
+        if not node_details:
+            node_details = {'distribution_plan': plan_id}
+
+        response = self.client.post(ENDPOINT_URL, node_details, format='json')
+        self.assertEqual(response.status_code, 201)
+        return response.data
