77dfaa43f1ebfa39f78b191dce16e936502756dc
wanderanimrod <wanderanimrod@gmail.com>
2014-09-18 13:31:38 +0300

    [nim, brain, tunji, billy] #X Using httpBackend to mock backend. Contacts service test now works.

--- eums/client/test/spec/services/contacts-service-spec.js
+++ eums/client/test/spec/services/contacts-service-spec.js
@@ -1,32 +1,27 @@
 describe('Contacts Service', function () {
-    var mockHttpService, contactService;
+    var mockContactsBackend, contactService;
     var stubContact = {
         firstname: 'Tunji',
         lastname: 'Sunmonu',
         phone: '+234778945674'
 
     };
+
     beforeEach(function () {
         module('contacts');
 
-        mockHttpService = jasmine.createSpyObj('mockHttpService', ['post']);
-
-        module(function ($provide) {
-            $provide.value('$http', mockHttpService);
-        });
-
-        inject(function (ContactService, $q) {
-            var defer = $q.defer();
-            defer.resolve(stubContact);
-            mockHttpService.post.and.returnValue(defer.promise);
+        inject(function (ContactService, $q, $httpBackend, EumsConfig) {
+            mockContactsBackend = $httpBackend;
+            mockContactsBackend.whenPOST(EumsConfig.CONTACTSERVICEURL + "/add").respond(stubContact);
             contactService = ContactService;
-
         });
     });
 
-    it('should add contact to', function () {
+    it('should add contact to', function (done) {
         contactService.addContact(stubContact).then(function (response) {
-            expect(response).toEqual(stubContact);
+            expect(response.data).toEqual(stubContact);
+            done();
         });
+        mockContactsBackend.flush();
     });
 });
