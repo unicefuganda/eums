745f0cf055e70d00a9874bc0beebbcebdc9dbcd2
wanderanimrod <wanderanimrod@gmail.com>
2014-09-19 11:21:54 +0300

    [nim] #8 Cleans up contacts service operations. We should expect a REST-style contacts service api

--- eums/client/test/spec/services/contacts-service-spec.js
+++ eums/client/test/spec/services/contacts-service-spec.js
@@ -1,27 +1,38 @@
-describe('Contacts Service', function () {
-    var mockContactsBackend, contactService;
+describe('Contacts Service', function() {
+    var mockContactsBackend, contactService, config;
     var stubContact = {
-        id: 1,
-        firstname: 'Tunji',
-        lastname: 'Sunmonu',
+        firstName: 'Andrew',
+        lastName: 'Mukiza',
         phone: '+234778945674'
     };
 
-    beforeEach(function () {
+    var expectedContact = {
+        id: 1,
+        firstName: 'Andrew',
+        lastName: 'Mukiza',
+        phone: '+234778945674'
+    };
+
+    beforeEach(function() {
         module('contacts');
 
-        inject(function (ContactService, $httpBackend, EumsConfig) {
+        inject(function(ContactService, $httpBackend, EumsConfig) {
             mockContactsBackend = $httpBackend;
-            mockContactsBackend.whenPOST(EumsConfig.CONTACT_SERVICE_URL + 'add').respond(stubContact);
             contactService = ContactService;
+            config = EumsConfig;
         });
     });
 
-    it('should add contact to', function (done) {
-        contactService.addContact(stubContact).then(function (response) {
-            expect(response.data).toEqual(stubContact);
+    it('should add contact to', function(done) {
+        mockContactsBackend.whenPOST(config.CONTACT_SERVICE_URL, stubContact).respond(expectedContact);
+        contactService.addContact(stubContact).then(function(response) {
+            expect(response.data).toEqual(expectedContact);
             done();
         });
         mockContactsBackend.flush();
+    });
+
+    it('should get contact from contacts backend', function(done) {
+        done();
     });
 });
