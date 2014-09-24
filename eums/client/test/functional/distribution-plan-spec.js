83ea6da17755de28b0adfaf147b647fbc01d1622
Brain Leke <bleke@thoughtworks.com>
2014-09-24 14:25:17 +0300

    Brain: Removing redundant tests

--- eums/client/test/functional/distribution-plan-spec.js
+++ eums/client/test/functional/distribution-plan-spec.js
@@ -17,15 +17,15 @@ describe('supply plan', function () {
         var time = Date.now().toString();
         var phoneNumber = time.substring(4);
 
-        distributionPlanPage.addContact({firstName: 'Tunji', lastName: 'Sola', phone: '+256'+ phoneNumber});
+//        distributionPlanPage.addContact({firstName: 'Tunji', lastName: 'Sola', phone: '+256'+ phoneNumber});
 
         expect(homePage.getHomeNavClass()).toEqual('active');
     });
 
-    it('should show error when contact is NOT added', function () {
-        distributionPlanPage.addInvalidContact({firstName: 'Tunji', lastName: 'Sola', phone: '778934543'});
-
-        expect(distributionPlanPage.errorSpan.getText()).toEqual('Phone number format is wrong');
-    });
+//    it('should show error when contact is NOT added', function () {
+//        distributionPlanPage.addInvalidContact({firstName: 'Tunji', lastName: 'Sola', phone: '778934543'});
+//
+//        expect(distributionPlanPage.errorSpan.getText()).toEqual('Phone number format is wrong');
+//    });
 
 });
--- eums/client/test/functional/pages/distribution-plan-page.js
+++ eums/client/test/functional/pages/distribution-plan-page.js
@@ -1,33 +1,33 @@
 var DistributionPlanPage = function () {
     this.text = element(by.css('.container'));
-    this.firstNameElement = element(by.model('contact.firstName'));
-    this.lastNameElement = element(by.model('contact.lastName'));
-    this.phone = element(by.model('contact.phone'));
-    this.addContactButton = element(by.id('add-contact'));
+//    this.firstNameElement = element(by.model('contact.firstName'));
+//    this.lastNameElement = element(by.model('contact.lastName'));
+//    this.phone = element(by.model('contact.phone'));
+//    this.addContactButton = element(by.id('add-contact'));
     this.errorSpan = element(by.id('error-msg'));
 
     this.elementisPresent = function () {
         return this.text.isPresent();
     };
 
-    this.enterContactDetails = function (contactDetails) {
-        this.firstNameElement.sendKeys(contactDetails.firstname);
-        this.lastNameElement.sendKeys(contactDetails.lastname);
-        this.phone.sendKeys(contactDetails.phone);
-
-    };
-
-    this.addContact = function (contactDetails) {
-        this.enterContactDetails(contactDetails);
-        this.addContactButton.click();
-
-        return require('./home-page');
-    };
-
-    this.addInvalidContact = function (invalidContactDetails) {
-        this.enterContactDetails(invalidContactDetails);
-        this.addContactButton.click();
-    };
+//    this.enterContactDetails = function (contactDetails) {
+//        this.firstNameElement.sendKeys(contactDetails.firstname);
+//        this.lastNameElement.sendKeys(contactDetails.lastname);
+//        this.phone.sendKeys(contactDetails.phone);
+//
+//    };
+//
+//    this.addContact = function (contactDetails) {
+//        this.enterContactDetails(contactDetails);
+//        this.addContactButton.click();
+//
+//        return require('./home-page');
+//    };
+//
+//    this.addInvalidContact = function (invalidContactDetails) {
+//        this.enterContactDetails(invalidContactDetails);
+//        this.addContactButton.click();
+//    };
 };
 
 module.exports = new DistributionPlanPage;
