77e02cae4a6d62c14bc29a65b99f80f77bf75ef1
wanderanimrod <wanderanimrod@gmail.com>
2014-09-25 17:16:36 +0300

    [nim, billy] #X Attempt 3 Fixing build

--- eums/snap_settings.py
+++ eums/snap_settings.py
@@ -9,3 +9,18 @@ DATABASES = {
        "HOST": "localhost",
    }
 }
+
+# Number of days after expected delivery date after which messages to consignees are sent out
+DELIVERY_STATUS_CHECK_DELAY = 7
+
+# Contacts service settings
+CONTACTS_SERVICE_URL = 'http://localhost:8005/api/contacts/'
+
+# RapidPro settings
+RAPIDPRO_URL = 'https://rapidpro.io/api/v1/'
+RAPIDPRO_FLOW_ID = 2436
+RAPIDPRO_URLS = {
+    'FLOW': "%sflows.json?id=%d" % (RAPIDPRO_URL, RAPIDPRO_FLOW_ID),
+    'RUNS': "%sruns.json" % RAPIDPRO_URL
+}
+RAPIDPRO_EXTRAS = {'CONTACT_NAME': 'contactName', 'SENDER': 'sender', 'PRODUCT': 'product'}
