4af0697d3eeac468e4c8132cde91945c37b2444c
wanderanimrod <wanderanimrod@gmail.com>
2014-09-26 13:51:31 +0300

    [nim, billy] #14 Smoke test that rapid pro runs endpoint works

--- eums/test/smoke/smoke_rapid_pro.py
+++ eums/test/smoke/smoke_rapid_pro.py
@@ -1,12 +1,19 @@
 from unittest import TestCase
+
 from django.conf import settings
 import requests
 
 
 class RapidProSmoke(TestCase):
+    def setUp(self):
+        self.headers = {'Authorization': "Token %s" % settings.RAPIDPRO_API_TOKEN}
+
+    def test_runs_endpoint_is_up(self):
+        response = requests.get(settings.RAPIDPRO_URLS['RUNS'], headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
     def test_flow_with_id_specified_in_settings_exists(self):
-        headers = {'Authorization': "Token %s" % settings.RAPIDPRO_API_TOKEN}
-        response = requests.get(settings.RAPIDPRO_URLS['FLOWS'], headers=headers)
+        response = requests.get(settings.RAPIDPRO_URLS['FLOWS'], headers=self.headers)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(self.flow_exists(settings.RAPIDPRO_FLOW_ID, response.json()['results']))
 
@@ -14,4 +21,4 @@ class RapidProSmoke(TestCase):
         matching_flows = filter(lambda flow: flow['flow'] == flow_id, flows)
         return len(matching_flows) == 1
 
-    # Test that flow takes the parameters we provide
+    # TODO Test that flow takes the parameters we provide when RapidPro API makes the @extra.* fields available
