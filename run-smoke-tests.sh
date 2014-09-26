0230498a0c5da4660f81e9baa820afd70fc7de12
wanderanimrod <wanderanimrod@gmail.com>
2014-09-26 11:26:39 +0300

    [nim, billy] #14 Allows specifying settings module when running smoke tests

--- run-smoke-tests.sh
+++ run-smoke-tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-./manage.py test -p smoke*.py
+SETTINGS_MODULE=$1
+
+if [ -z "$SETTINGS_MODULE" ]; then
+    ./manage.py test -p smoke*.py
+else
+    ./manage.py test --settings=${SETTINGS_MODULE} -p smoke*.py
+fi
