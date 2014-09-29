7f0ae056e7cb3e8d3fa3f6890db98977910c71d2
wanderanimrod <wanderanimrod@gmail.com>
2014-09-29 12:56:33 +0300

    [nim, billy] #14 Cleans up debug statements

--- eums/rapid_pro/rapid_pro_facade.py
+++ eums/rapid_pro/rapid_pro_facade.py
@@ -5,13 +5,9 @@ from eums.rapid_pro import fake_rapid_pro
 
 
 def start_delivery_run(*_, **kwargs):
-    print "*" * 20, "IN start delivery run method", "*" * 20
     consignee = kwargs['consignee']
     item_description = kwargs['item_description']
     sender = kwargs['sender']
-    print "*" * 20, "Consignee", consignee, "*" * 20
-    print "*" * 20, "Item ", item_description, "*" * 20
-    print "*" * 20, "sender ", sender, "*" * 20
     payload = {
         "flow": settings.RAPIDPRO_FLOW_ID,
         "phone": [consignee['phone']],
@@ -21,10 +17,7 @@ def start_delivery_run(*_, **kwargs):
             settings.RAPIDPRO_EXTRAS['PRODUCT']: item_description
         }
     }
-    print "*" * 20, "Payload ", payload, "*" * 20
     headers = {'Authorization': 'Token %s' % settings.RAPIDPRO_API_TOKEN}
-    print '*' * 20, 'EXPECTED HEADER', headers, '*' * 20
-    print '*' * 20, 'URLS', settings.RAPIDPRO_URLS['RUNS'], '*' * 20
 
     if settings.RAPIDPRO_LIVE:
         requests.post(settings.RAPIDPRO_URLS['RUNS'], data=payload, headers=headers)
