8314d166104ca71ee7607c6e7452572650ecc475
wanderanimrod <wanderanimrod@gmail.com>
2014-09-30 20:59:25 +0300

    [nim] #X Attempt at fixing build

--- eums/rapid_pro/fake_endpoints/runs.py
+++ eums/rapid_pro/fake_endpoints/runs.py
@@ -7,7 +7,9 @@ def post(data=None):
     global last_run_id
     last_run_id += 1
     phone = data.get('phone', '+256 772 123456')
-    return FakeResponse([{"run": last_run_id, "phone": phone}], 201)
+    response = FakeResponse([{"run": last_run_id, "phone": phone}], 201)
+    print "*" * 20, "Post called. Going to return fake response", response, "*" * 20
+    return response
 
 
 def clear():
