1acb1a3a6816dee37120c4db050290a60037e5cf
wanderanimrod <wanderanimrod@gmail.com>
2014-09-30 14:30:04 +0300

    [nim, billy] #14 Enables starting celery by specifying settings module

--- eums/celery.py
+++ eums/celery.py
@@ -1,7 +1,14 @@
 from __future__ import absolute_import
+import os
 from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eums.settings')
 
 app = Celery('eums', broker='redis://', backend='redis://', include=['eums.services.flow_scheduler'])
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 if __name__ == '__main__':
     app.start()
