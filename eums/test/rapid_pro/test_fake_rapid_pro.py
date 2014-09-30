4c01c4beb37aace976417955771e468db6ac4b0e
wanderanimrod <wanderanimrod@gmail.com>
2014-09-30 19:44:53 +0300

    [nim] #X Attempt at fixing build

--- eums/test/rapid_pro/test_fake_rapid_pro.py
+++ eums/test/rapid_pro/test_fake_rapid_pro.py
@@ -1,25 +1,26 @@
 from unittest import TestCase
 
-from eums.rapid_pro.fake_rapid_pro import runs
+from eums.rapid_pro.fake_rapid_pro import RunsEndpoint
 
 
 class FakeRapidProdRunsEndpointTest(TestCase):
     def setUp(self):
         self.phone = '+256 772 123456'
+        self.runs = RunsEndpoint()
 
     def test_should_respond_with_run_list_on_post_to_runs(self):
         payload = {"phone": [self.phone]}
-        response = runs.post(data=payload).json()
+        response = self.runs.post(data=payload).json()
         self.assertEqual(response, [{"run": 1, "phone": [self.phone]}])
 
     def test_should_increment_run_ids_on_successive_post_to_runs(self):
         payload = {'phone': [self.phone]}
 
-        response_one = runs.post(data=payload).json()
-        response_two = runs.post(data=payload).json()
+        response_one = self.runs.post(data=payload).json()
+        response_two = self.runs.post(data=payload).json()
 
         self.assertEqual(response_one, [{"run": 1, "phone": [self.phone]}])
         self.assertEqual(response_two, [{"run": 2, "phone": [self.phone]}])
 
     def tearDown(self):
-        runs.clear()
+        self.runs.clear()
