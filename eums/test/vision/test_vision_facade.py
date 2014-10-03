0948efd39dab7bdda779ae334a90002e44429eed
James Oloo Onyango <jamesony@thoughtworks.com>
2014-10-03 14:41:04 +0300

    [Remo/Billy] Changed import sales orders to use sales order items

--- eums/test/vision/test_vision_facade.py
+++ eums/test/vision/test_vision_facade.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 import os
 
 from xlwt import Workbook
-from eums.models import SalesOrder, Item
+from eums.models import SalesOrder, Item, SalesOrderItem
 from eums.test.factories.item_factory import ItemFactory
 
 from eums.vision.vision_facade import load_sales_order_data, save_sales_order_data, import_sales_orders
@@ -28,7 +28,11 @@ class TestVisionFacade(TestCase):
                            '224,000', '9/2/2014', '9/23/2014', '9/23/2014', 'Tally sheets', '9/2/2014', '0.19',
                            '42,560.00', '2', 'Round Neck T-Shirts']
 
-        for row_index, row in enumerate([self.header, self.first_row, self.second_row]):
+        self.third_row = ['ZCOM', '20155981', 'Z43801', '1500', 'Open', 'RNAULA', '10', 'SL004594', '224,000',
+                          '224,000', '9/2/2014', '9/23/2014', '9/23/2014', 'Tally sheets', '9/2/2014', '0.19',
+                          '42,560.00', '2', 'Round Neck T-Shirts']
+
+        for row_index, row in enumerate([self.header, self.first_row, self.second_row, self.third_row]):
             for col_index, item in enumerate(row):
                 sheet.write(row_index, col_index, item)
 
@@ -38,34 +42,48 @@ class TestVisionFacade(TestCase):
         os.remove(self.sales_order_file_location)
         Item.objects.all().delete()
         SalesOrder.objects.all().delete()
+        SalesOrderItem.objects.all().delete()
 
     def test_should_load_sales_order_data(self):
-        sales_order_data = load_sales_order_data(self.sales_order_file_location)
+        expected_data = [{'order_number': '20155548',
+                          'items': [{'material_code': 'SL004594', 'quantity': '224,000', 'issue_date': '9/23/2014',
+                                     'delivery_date': '9/23/2014', 'net_price': '0.19', 'net_value': '42,560.00',
+                                     'order_number': '20155548'}]},
+                         {'order_number': '20155981',
+                          'items': [{'material_code': 'SL006645', 'quantity': '224,000', 'issue_date': '9/23/2014',
+                                     'delivery_date': '9/23/2014', 'net_price': '0.19', 'net_value': '42,560.00',
+                                     'order_number': '20155981'},
+                                    {'material_code': 'SL004594', 'quantity': '224,000', 'issue_date': '9/23/2014',
+                                     'delivery_date': '9/23/2014', 'net_price': '0.19', 'net_value': '42,560.00',
+                                     'order_number': '20155981'}]}]
 
-        expected_data = [{'order_number': '20155548', 'material_code': 'SL004594', 'quantity': '224,000',
-                          'issue_date': '9/23/2014', 'delivery_date': '9/23/2014', 'net_price': '0.19',
-                          'net_value': '42,560.00'},
-                         {'order_number': '20155981', 'material_code': 'SL006645', 'quantity': '224,000',
-                          'issue_date': '9/23/2014', 'delivery_date': '9/23/2014', 'net_price': '0.19',
-                          'net_value': '42,560.00'}]
+        sales_order_data = load_sales_order_data(self.sales_order_file_location)
 
         self.assertEqual(sales_order_data, expected_data)
 
     def test_should_save_sales_order_data(self):
         self.assertEqual(SalesOrder.objects.count(), 0)
+        self.assertEqual(SalesOrderItem.objects.count(), 0)
 
         ItemFactory(material_code='SL004594')
         ItemFactory(material_code='SL006645')
 
-        sales_order_data = [{'order_number': '20155548', 'material_code': 'SL004594', 'quantity': '224,000',
-                             'issue_date': '9/23/2014', 'delivery_date': '9/23/2014', 'net_price': '0.19',
-                             'net_value': '42,560.00'},
-                            {'order_number': '20155981', 'material_code': 'SL006645', 'quantity': '224,000',
-                             'issue_date': '9/23/2014', 'delivery_date': '9/23/2014', 'net_price': '0.19',
-                             'net_value': '42,560.00'}]
+        sales_order_data = [{'order_number': '20155548',
+                             'items': [{'material_code': 'SL004594', 'quantity': '224,000', 'issue_date': '9/23/2014',
+                                        'delivery_date': '9/23/2014', 'net_price': '0.19', 'net_value': '42,560.00',
+                                        'order_number': '20155548'}]},
+                            {'order_number': '20155981',
+                             'items': [{'material_code': 'SL006645', 'quantity': '224,000', 'issue_date': '9/23/2014',
+                                        'delivery_date': '9/23/2014', 'net_price': '0.19', 'net_value': '42,560.00',
+                                        'order_number': '20155981'},
+                                       {'material_code': 'SL004594', 'quantity': '224,000', 'issue_date': '9/23/2014',
+                                        'delivery_date': '9/23/2014', 'net_price': '0.19', 'net_value': '42,560.00',
+                                        'order_number': '20155981'}]}]
+
         save_sales_order_data(sales_order_data)
 
         self.assertEqual(SalesOrder.objects.count(), 2)
+        self.assertEqual(SalesOrderItem.objects.count(), 3)
 
     def test_should_load_sales_orders_from_excel_and_save(self):
         self.assertEqual(SalesOrder.objects.count(), 0)
@@ -76,3 +94,4 @@ class TestVisionFacade(TestCase):
         import_sales_orders(self.sales_order_file_location)
 
         self.assertEqual(SalesOrder.objects.count(), 2)
+        self.assertEqual(SalesOrderItem.objects.count(), 3)
--- eums/vision/vision_facade.py
+++ eums/vision/vision_facade.py
@@ -1,5 +1,5 @@
 from xlutils.view import View
-from eums.models import SalesOrder, Item
+from eums.models import SalesOrder, Item, SalesOrderItem
 from datetime import datetime
 
 
@@ -18,29 +18,48 @@ def load_sales_order_data(location):
 
 def save_sales_order_data(sales_orders_data):
     for sales_order_data in sales_orders_data:
-        sales_order = _create_sales_order_from_dict(sales_order_data)
-        sales_order.save()
+        _create_sales_order_from_dict(sales_order_data)
 
 
 def _create_sales_order_from_dict(sales_order_data):
     sales_order = SalesOrder()
     sales_order.order_number = sales_order_data['order_number']
-    sales_order.item = Item.objects.get(material_code=sales_order_data['material_code'])
-    sales_order.quantity = float(sales_order_data['quantity'].replace(',', ''))
-    sales_order.issue_date = datetime.strptime(sales_order_data['issue_date'], "%m/%d/%Y")
-    sales_order.delivery_date = datetime.strptime(sales_order_data['delivery_date'], "%m/%d/%Y")
-    sales_order.net_price = float(sales_order_data['net_price'].replace(',', ''))
-    sales_order.net_value = float(sales_order_data['net_value'].replace(',', ''))
-    return sales_order
+    sales_order.save()
+
+    for sales_order_item_data in sales_order_data['items']:
+        sales_order_item = SalesOrderItem()
+        sales_order_item.sales_order = sales_order
+        sales_order_item.item = Item.objects.get(material_code=sales_order_item_data['material_code'])
+        sales_order_item.quantity = float(sales_order_item_data['quantity'].replace(',', ''))
+        sales_order_item.issue_date = datetime.strptime(sales_order_item_data['issue_date'], "%m/%d/%Y")
+        sales_order_item.delivery_date = datetime.strptime(sales_order_item_data['delivery_date'], "%m/%d/%Y")
+        sales_order_item.net_price = float(sales_order_item_data['net_price'].replace(',', ''))
+        sales_order_item.net_value = float(sales_order_item_data['net_value'].replace(',', ''))
+        sales_order_item.save()
 
 
 def _convert_view_to_list_of_dicts(sheet, relevant_data):
-    sales_orders_lists = []
+    sales_orders_list = []
     for row in sheet:
-        sales_order_dict = {}
+        sales_order_item_dict = {}
+
         for col_index, value in enumerate(row):
             if relevant_data.get(col_index):
-                sales_order_dict[relevant_data.get(col_index)] = str(value)
-        sales_orders_lists.append(sales_order_dict)
+                sales_order_item_dict[relevant_data.get(col_index)] = str(value)
 
-    return sales_orders_lists
+        sales_order_number = sales_order_item_dict['order_number']
+        sales_order_index = _sales_order_index_in_list(sales_order_number, sales_orders_list)
+
+        if sales_order_index > -1:
+            sales_orders_list[sales_order_index].get('items').append(sales_order_item_dict)
+        else:
+            sales_orders_list.append({'order_number': sales_order_number, 'items': [sales_order_item_dict]})
+
+    return sales_orders_list
+
+
+def _sales_order_index_in_list(sales_order_number, sales_order_list):
+    for index, sales_order in enumerate(sales_order_list):
+        if sales_order.get('order_number') == sales_order_number:
+            return index
+    return -1
