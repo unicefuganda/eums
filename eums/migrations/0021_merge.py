b02fc742c4783c365305a7b809b799f12b2f07e0
wanderanimrod <wanderanimrod@gmail.com>
2014-10-03 14:57:28 +0300

    [nim, mukiza] #15 Resolves conflicts in migrations

---
+++ eums/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eums', '0020_salesorderitem'),
+        ('eums', '0018_auto_20141003_1440'),
+    ]
+
+    operations = [
+    ]
