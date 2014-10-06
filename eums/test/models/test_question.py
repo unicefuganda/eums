5e2f1be9d75f3a47ee3779753385e6146c6c2245
wanderanimrod <wanderanimrod@gmail.com>
2014-10-06 11:06:17 +0300

    [nim, mukiza] #15 Creates questions in database, with json dump for easy loading

--- eums/test/models/test_question.py
+++ eums/test/models/test_question.py
@@ -12,7 +12,7 @@ class QuestionTest(TestCase):
             self.assertIn(field, fields_in_item)
 
     def test_should_create_a_default_option_for_multiple_choice_question(self):
-        question = Question.objects.create(text='Whats your sex?', type=Question.MULTIPLE_CHOICE, uuid='36464hh34')
+        question = Question.objects.create(text='Whats your gender?', type=Question.MULTIPLE_CHOICE, label='gender')
         uncategorised_option = Option.objects.filter(question=question, text=Option.UNCATEGORISED)
         self.assertEqual(uncategorised_option.count(), 1)
 
