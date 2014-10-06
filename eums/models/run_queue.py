c6aea18a09185b91290351f2573f00bbddd9f50e
James Oloo Onyango <jamesony@thoughtworks.com>
2014-10-06 17:19:13 +0300

    [Remo/James] added enqueue and deque to runqueue

--- eums/models/run_queue.py
+++ eums/models/run_queue.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db.models import Q
 from model_utils.fields import StatusField
 from model_utils import Choices
 
-from eums.models import DistributionPlanLineItem, Consignee
+from eums.models import DistributionPlanLineItem
 
 
 class RunQueue(models.Model):
@@ -14,3 +15,18 @@ class RunQueue(models.Model):
 
     class Meta:
         app_label = 'eums'
+
+    @classmethod
+    def enqueue(cls, node_line_item, start_run_date):
+        queued_run = RunQueue(node_line_item=node_line_item, status=RunQueue.STATUS.not_started,
+                              contact_person_id=node_line_item.distribution_plan_node.consignee.contact_person_id,
+                              start_run_date=start_run_date)
+        queued_run.save()
+
+    @classmethod
+    def deque(cls, contact_person_id):
+        not_started_runs = RunQueue.objects.filter(Q(contact_person_id=contact_person_id) & Q(status='not_started')).\
+            order_by('start_run_date')
+        if len(not_started_runs):
+            return not_started_runs[0]
+        return None
--- eums/test/models/test_run_queue.py
+++ eums/test/models/test_run_queue.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from unittest import TestCase
 
 from eums.models import RunQueue
+from eums.test.factories.RunQueueFactory import RunQueueFactory
+from eums.test.factories.consignee_factory import ConsigneeFactory
+from eums.test.factories.distribution_plan_line_item_factory import DistributionPlanLineItemFactory
+from eums.test.factories.distribution_plan_node_factory import DistributionPlanNodeFactory
 
 
 class RunQueueTest(TestCase):
+    def tearDown(self):
+        RunQueue.objects.all().delete()
+
     def test_should_have_all_expected_fields(self):
         node_line_item_run = RunQueue()
         fields_in_run_queue = [field.attname for field in node_line_item_run._meta.fields]
@@ -12,5 +20,33 @@ class RunQueueTest(TestCase):
             self.assertIn(field, fields_in_run_queue)
 
     def test_can_deque_next_run_for_a_particular_contact_person(self):
-        # RunQueueFactory()
-        pass
+        contact_person_id = 'id'
+        RunQueueFactory(contact_person_id=contact_person_id, status=RunQueue.STATUS.started)
+        RunQueueFactory(contact_person_id=contact_person_id, status=RunQueue.STATUS.not_started,
+                        start_run_date=datetime(2014, 12, 10))
+        run_queue = RunQueueFactory(contact_person_id=contact_person_id, status=RunQueue.STATUS.not_started,
+                                    start_run_date=datetime(2014, 10, 10))
+
+        self.assertEqual(RunQueue.deque(contact_person_id), run_queue)
+
+    def test_deque_returns_none_when_contact_person_no_pending_runs(self):
+        contact_person_id = 'id'
+        RunQueueFactory(contact_person_id=contact_person_id, status=RunQueue.STATUS.started)
+        RunQueueFactory(contact_person_id=contact_person_id, status=RunQueue.STATUS.started,
+                        start_run_date=datetime(2014, 12, 10))
+
+        self.assertEqual(RunQueue.deque(contact_person_id), None)
+
+    def test_queues_run_for_particular_line_item_node(self):
+        contact_person_id = 'id'
+        start_run_date = datetime.now()
+
+        consignee = ConsigneeFactory(contact_person_id=contact_person_id)
+        node = DistributionPlanNodeFactory(consignee=consignee)
+        node_line_item = DistributionPlanLineItemFactory(distribution_plan_node=node)
+
+        RunQueue.enqueue(node_line_item, start_run_date)
+        queued_run = RunQueue.deque(contact_person_id)
+
+        self.assertEqual(queued_run.status, RunQueue.STATUS.not_started)
+        self.assertEqual(queued_run.contact_person_id, contact_person_id)
