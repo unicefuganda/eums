a7948a10a6787eafa8098233911ac98af2de9412
Teejay005 <tunji.sunmonu@gmail.com>
2014-10-08 15:28:53 +0300

    Update staging.sh

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "ssh into staging"
-ssh -t -t staging@196.0.26.51 << EOF
+ssh -t -t -i $HOME/.ssh/id_rsa_staging staging@196.0.26.51 << EOF
 echo "change to root user"
 sudo su
 echo "Go to home"
