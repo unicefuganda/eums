872b7685ebb3097e569addbb6d7acc92484280e0
wanderanimrod <wanderanimrod@gmail.com>
2014-10-10 12:13:43 +0300

    [nim] #45 Updates questions fixture to include changes in the end user flow and questions from the middleman flow

--- eums/fixtures/questions.py
+++ eums/fixtures/questions.py
@@ -1,34 +1,55 @@
 from eums.models import Option
-from eums.models.question import Question, MultipleChoiceQuestion, NumericQuestion, TextQuestion
+from eums.models.question import MultipleChoiceQuestion, NumericQuestion, TextQuestion
 
 
 def seed_questions():
     question_1, _ = MultipleChoiceQuestion.objects.get_or_create(
-        uuids='2ff9fab3-4c12-400e-a2fe-4551fa1ebc18', text='Was item received?',
-        type=Question.MULTIPLE_CHOICE, label='productReceived')
+        uuids=['2ff9fab3-4c12-400e-a2fe-4551fa1ebc18', '93bbd12e-417c-4789-9c42-6dc6959c03be',
+               '53a31c08-896e-43b2-bd1f-e5a527d389b9', 'b8189435-a6b5-42a0-b5b0-91c95484dae1'],
+        text='Was product received?', label='productReceived')
 
     Option.objects.get_or_create(text='Yes', question=question_1)
     Option.objects.get_or_create(text='No', question=question_1)
 
     TextQuestion.objects.get_or_create(
-        uuids='abc9c005-7a7c-44f8-b946-e970a361b6cf', text='What date was it received?', label='dateOfReceipt')
+        uuids=['abc9c005-7a7c-44f8-b946-e970a361b6cf', '884ed6d8-1cef-4878-999d-bce7de85e27c'],
+        text='What date was it received?', label='dateOfReceipt'
+    )
 
     NumericQuestion.objects.get_or_create(
-        uuids='6c1cf92d-59b8-4bd3-815b-783abd3dfad9', text='How much was received?', label='amountReceived')
+        uuids=['69de6032-f4de-412a-9c9e-ed98fb9bca93', '9af2907a-d3a6-41ee-8a12-0b3197d30baf'],
+        text='How much was received?', label='amountReceived'
+    )
 
     question_2, _ = MultipleChoiceQuestion.objects.get_or_create(
-        uuids='7a5c8f57-5c3f-4659-b717-0de556898157, dc27480e-4931-46a8-9bea-ad0dadbec1d8',
-        text='Are you satisfied with the delivery?', label='satisfactionWithDelivery'
+        uuids=['6c1cf92d-59b8-4bd3-815b-783abd3dfad9', 'fe368546-1b9c-4a15-926d-68b7caaa0380'],
+        text='What is the quality of the product?', label='qualityOfProduct'
     )
-    Option.objects.get_or_create(text='Yes', question=question_2)
-    Option.objects.get_or_create(text='No', question=question_2)
+    Option.objects.get_or_create(text='Good', question=question_2)
+    Option.objects.get_or_create(text='Damaged', question=question_2)
+    Option.objects.get_or_create(text='Substandard', question=question_2)
+    Option.objects.get_or_create(text='Expired', question=question_2)
 
     question_3, _ = MultipleChoiceQuestion.objects.get_or_create(
-        uuids='fe368546-1b9c-4a15-926d-68b7caaa0380',
-        text='What is the condition of the items delivered?', type=Question.MULTIPLE_CHOICE,
-        label='deliveryCondition'
+        uuids=['7a5c8f57-5c3f-4659-b717-0de556898157', 'dc27480e-4931-46a8-9bea-ad0dadbec1d8'],
+        text='Are you satisfied with the product?', label='satisfiedWithProduct')
+    Option.objects.get_or_create(text='Yes', question=question_3)
+    Option.objects.get_or_create(text='No', question=question_3)
+
+    question_4, _ = MultipleChoiceQuestion.objects.get_or_create(
+        uuids=['18b2ea96-cb63-40d8-8c26-1985a944ff1c', '84150f15-b18b-4efa-be6b-ad24bc68a08f',
+               '4e46a52d-8ea1-4bc4-824c-f8da74ce7ad0', '269aa1f7-7ca5-46f6-9bc0-6ad3fb7a5629'],
+        text='Have you been informed of the delay?', label='informedOfDelay'
     )
-    Option.objects.get_or_create(text='Expired', question=question_3)
-    Option.objects.get_or_create(text='Damaged', question=question_3)
-    Option.objects.get_or_create(text='Late', question=question_3)
-    Option.objects.get_or_create(text='Substandard', question=question_3)
+    Option.objects.get_or_create(text='Yes', question=question_4)
+    Option.objects.get_or_create(text='No', question=question_4)
+
+    question_5, _ = TextQuestion.objects.get_or_create(
+        uuids=['e9c35020-e751-4611-b222-5573b7040c49', '3f5d290a-067d-4cb9-bb09-ed7c424a6abd'],
+        text='What did the partner say is the revised delivery date?', label='revisedDeliveryDate'
+    )
+
+    question_6, _ = TextQuestion.objects.get_or_create(
+        uuids=['4dd1a813-27d4-4511-82e3-cc470fcd3baa'],
+        text='Feedback about Dissatisfaction', label='feedbackAboutDissatisfaction'
+    )
