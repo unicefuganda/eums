58a08f0fd5f2c1f4e5c19fd4d4e0e4b237248c37
Andrew Mukiza <andrewmukiza@gmail.com>
2014-10-17 10:02:33 +0300

    [Andrew, Tunji] #15 add consignee and amount sent to the json response

--- eums/api/answer/answers_endpoint.py
+++ eums/api/answer/answers_endpoint.py
@@ -11,10 +11,12 @@ class Responses(APIView):
         result = []
         for node in all_nodes:
             node_responses = node.responses()
+            formatted_run_responses = {'consignee': node.consignee.customer_id}
             for item_run, responses in node_responses.iteritems():
-                formatted_run_responses = {'item': item_run.node_line_item.item.description, 'details': []}
+                formatted_run_responses.update({'item': item_run.node_line_item.item.description, 'details': [],
+                                                'amountSent': item_run.node_line_item.targeted_quantity})
                 for response in responses:
-                    question_reponse_map = {'questionLabel': response.question.label, 'answer': response.format()}
-                    formatted_run_responses['details'].append(question_reponse_map)
+                    question_response_map = {'questionLabel': response.question.label, 'answer': response.format()}
+                    formatted_run_responses['details'].append(question_response_map)
                 result.append(formatted_run_responses)
         return Response(json.dumps(result), status=status.HTTP_200_OK)
--- eums/test/api/test_responses_endpoint.py
+++ eums/test/api/test_responses_endpoint.py
@@ -24,7 +24,6 @@ class DistributionPlanEndPointTest(APITestCase):
         yes_option = OptionFactory(text='Yes', question=multichoice_question)
         no_option = OptionFactory(text='No', question=multichoice_question)
 
-        sugar = ItemFactory(description='Sugar')
         salt = ItemFactory(description='Salt')
 
         numeric_question = NumericQuestionFactory(label='amountReceived')
@@ -35,24 +34,16 @@ class DistributionPlanEndPointTest(APITestCase):
                                                              item=item)
         line_item_run = NodeLineItemRunFactory(node_line_item=node_line_item_one, status='completed')
 
-        sugar_item = SalesOrderItemFactory(item=sugar, description='10 bags of sugar')
-        sugar_node_line_item_one = DistributionPlanLineItemFactory(distribution_plan_node=node, targeted_quantity=100,
-                                                                   item=sugar_item)
-        sugar_line_item_run = NodeLineItemRunFactory(node_line_item=sugar_node_line_item_one, status='completed')
-
         multiple_answer_one = MultipleChoiceAnswerFactory(line_item_run=line_item_run, question=multichoice_question,
                                                           value=yes_option)
         numeric_answer_one = NumericAnswerFactory(line_item_run=line_item_run, value=80, question=numeric_question)
 
-        MultipleChoiceAnswerFactory(line_item_run=sugar_line_item_run,
-                                    question=multichoice_question, value=no_option)
-        NumericAnswerFactory(line_item_run=sugar_line_item_run, value=80,
-                             question=numeric_question)
         response = self.client.get("%s/" % ENDPOINT_URL, format='json')
 
         self.assertEqual(response.status_code, 200)
         response_data = json.loads(response.data)
-        expected_data = {u'item': u'10 bags of salt', u'details': [
+
+        expected_data = {u'item': u'10 bags of salt', u'amountSent': 100, u'consignee': u'11', u'details': [
             {u'answer': u'%s' % numeric_answer_one.format(), u'questionLabel': u'%s' % numeric_question.label},
             {u'answer': u'%s' % multiple_answer_one.format(), u'questionLabel': u'%s' % multichoice_question.label}]}
         self.assertDictContainsSubset(expected_data, response_data[0])
