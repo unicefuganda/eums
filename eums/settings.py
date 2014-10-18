8bcea8a98ebee4b6d1c85137d818f8ef903606cb
Kelvin Smith <oneKelvinSmith@gmail.com>
2014-10-18 15:41:08 +0300

    [kelvin] X set rapid pro token to 'token' to allow supervisor to start

--- eums/settings.py
+++ eums/settings.py
@@ -73,7 +73,7 @@ MAX_ALLOWED_REPLY_PERIOD = 7
 CONTACTS_SERVICE_URL = 'http://localhost:8005/api/contacts/'
 
 # RapidPro settings
-token = None
+token = 'token' #TODO figure out a way to use environment variable with supervisor
 RAPIDPRO_API_TOKEN = token or os.environ['RAPIDPRO_API_TOKEN']
 RAPIDPRO_URL = 'https://rapidpro.io/api/v1/'
 RAPIDPRO_URLS = {
