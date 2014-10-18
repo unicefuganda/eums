9b4cf70e56343e8ec9abd22f102bd9bf56a5c9fd
wanderanimrod <wanderanimrod@gmail.com>
2014-10-18 13:26:57 +0300

    [nim, billy] #X reports service can calculate total delivery stats not filtered and filtered by consignee or programme, or both

--- eums/client/app/scripts/home/global-stats.js
+++ eums/client/app/scripts/home/global-stats.js
@@ -2,28 +2,50 @@
 
 angular.module('GlobalStats', ['eums.config'])
     .factory('DistributionReportService', function($http, EumsConfig) {
+
+        var computeTotals = function(reports) {
+            var totals = {received: 0, notReceived: 0, distributed: 0, notDistributed: 0};
+            reports.forEach(function(report) {
+                totals.received += report.total_received_with_quality_issues +
+                    report.total_received_with_quantity_issues +
+                    report.total_received_without_issues;
+
+                totals.distributed += report.total_distributed_with_quality_issues +
+                    report.total_distributed_with_quantity_issues +
+                    report.total_distributed_without_issues;
+
+                totals.notDistributed += report.total_not_distributed;
+
+                totals.notReceived += report.total_not_received;
+            });
+            return totals;
+        };
+
         return {
             getReports: function() {
                 return $http.get(EumsConfig.DISTRIBUTION_REPORT + '/').then(function(response) {
                     return response.data;
                 });
             },
-            getTotals: function(reports) {
-                var totals = {received: 0, notReceived: 0, distributed: 0, notDistributed: 0};
-                reports.forEach(function(report) {
-                    totals.received += report.total_received_with_quality_issues +
-                        report.total_received_with_quantity_issues +
-                        report.total_received_without_issues;
+            getTotals: function(reports, options) {
+                if(options) {
+                    var consignee = options.consignee;
+                    var programme = options.programme;
+                    var filteredReports = reports.filter(function(report) {
+                        if(consignee && programme) {
+                            return report.consignee === consignee && report.programme === programme;
+                        }
+                        else if(consignee) {
+                             return report.consignee === consignee;
+                        }
+                        else if(programme) {
+                             return report.programme === programme;
+                        }
+                    });
 
-                    totals.distributed += report.total_distributed_with_quality_issues +
-                        report.total_distributed_with_quantity_issues +
-                        report.total_distributed_without_issues;
-
-                    totals.notDistributed += report.total_not_distributed;
-
-                    totals.notReceived += report.total_not_received;
-                });
-                return totals;
+                    return computeTotals(filteredReports);
+                }
+                return computeTotals(reports);
             }
         }
     });
--- eums/client/test/spec/services/distribution-report-service-spec.js
+++ eums/client/test/spec/services/distribution-report-service-spec.js
@@ -1,5 +1,48 @@
 describe('Distribution report Service', function() {
     var mockBackend, reportService, config;
+    var consigneeOneId = 1;
+    var consigneeTwoId = 2;
+    var programmeOneId = 1;
+    var programmeTwoId = 2;
+
+    var reports = [
+        {
+            consignee: consigneeOneId,
+            programme: programmeOneId,
+            total_received_with_quality_issues: 1,
+            total_received_with_quantity_issues: 1,
+            total_received_without_issues: 1,
+            total_not_received: 1,
+            total_distributed_with_quality_issues: 1,
+            total_distributed_with_quantity_issues: 1,
+            total_distributed_without_issues: 1,
+            total_not_distributed: 1
+        },
+        {
+            consignee: consigneeTwoId,
+            programme: programmeOneId,
+            total_received_with_quality_issues: 2,
+            total_received_with_quantity_issues: 2,
+            total_received_without_issues: 2,
+            total_not_received: 2,
+            total_distributed_with_quality_issues: 2,
+            total_distributed_with_quantity_issues: 2,
+            total_distributed_without_issues: 2,
+            total_not_distributed: 2
+        },
+        {
+            consignee: consigneeTwoId,
+            programme: programmeTwoId,
+            total_received_with_quality_issues: 2,
+            total_received_with_quantity_issues: 2,
+            total_received_without_issues: 2,
+            total_not_received: 2,
+            total_distributed_with_quality_issues: 2,
+            total_distributed_with_quantity_issues: 2,
+            total_distributed_without_issues: 2,
+            total_not_distributed: 2
+        }
+    ];
 
     beforeEach(function() {
         module('GlobalStats');
@@ -12,7 +55,9 @@ describe('Distribution report Service', function() {
     });
 
     it('should get report from backend', function(done) {
-        var expectedReports = [{consignee: 1, programme: 1, otherDetails: {}}];
+        var expectedReports = [
+            {consignee: 1, programme: 1, otherDetails: {}}
+        ];
 
         mockBackend.whenGET(config.DISTRIBUTION_REPORT + '/').respond(expectedReports);
         reportService.getReports().then(function(reports) {
@@ -23,29 +68,22 @@ describe('Distribution report Service', function() {
     });
 
     it('should get total stats from report', function() {
-        var reports = [
-            {
-                total_received_with_quality_issues: 1,
-                total_received_with_quantity_issues: 1,
-                total_received_without_issues: 1,
-                total_not_received: 1,
-                total_distributed_with_quality_issues: 1,
-                total_distributed_with_quantity_issues: 1,
-                total_distributed_without_issues: 1,
-                total_not_distributed: 1
-            },
-            {
-                total_received_with_quality_issues: 2,
-                total_received_with_quantity_issues: 2,
-                total_received_without_issues: 2,
-                total_not_received: 2,
-                total_distributed_with_quality_issues: 2,
-                total_distributed_with_quantity_issues: 2,
-                total_distributed_without_issues: 2,
-                total_not_distributed: 2
-            }
-        ];
         var totals = reportService.getTotals(reports);
-        expect(totals).toEqual({received: 9, notReceived: 3, distributed: 9, notDistributed: 3});
+        expect(totals).toEqual({ received : 15, notReceived : 5, distributed : 15, notDistributed : 5});
+    });
+
+    it('should get total stats by consignee', function() {
+        var totals = reportService.getTotals(reports, {consignee: consigneeOneId});
+        expect(totals).toEqual({ received : 3, notReceived : 1, distributed : 3, notDistributed : 1});
+    });
+
+    it('should get total stats by programme', function() {
+        var totals = reportService.getTotals(reports, {programme: programmeOneId});
+        expect(totals).toEqual({ received : 9, notReceived : 3, distributed : 9, notDistributed : 3});
+    });
+
+    it('should get total stats by programme and consignee', function() {
+        var totals = reportService.getTotals(reports, {programme: programmeOneId, consignee: consigneeOneId});
+        expect(totals).toEqual({ received : 3, notReceived : 1, distributed : 3, notDistributed : 1});
     });
 });
