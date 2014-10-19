317ae8a194749f889b3a65547c0755df19cb1c65
James Oloo Onyango <jamesony@thoughtworks.com>
2014-10-19 17:36:40 +0300

    [billy, nim] #NO_STORY Added waybill to distribution reporting pages

--- eums/client/app/scripts/manual-reporting/manual-reporting.js
+++ eums/client/app/scripts/manual-reporting/manual-reporting.js
@@ -17,22 +17,45 @@ angular.module('ManualReporting', ['ngTable', 'siTable', 'NewDistributionPlan'])
 
         // Should be in another controller
         $scope.document = DistributionReportingParameters.retrieveVariable('selectedPurchaseOrder');
+        $scope.currentDocumentType = DistributionReportingParameters.retrieveVariable('currentDocumentType');
         $scope.documentItemSelected = undefined;
         $scope.districts = Districts.getAllDistricts().map(function (district) {
             return {id: district, name: district};
         });
+        var purchaseOrders = [];
+        var waybills = [];
 
         $scope.initialize = function () {
             this.sortBy('date');
             this.sort.descending = false;
-            $scope.purchaseOrders = [
-                {id: 1, order_number: 65025072, date: '12/13/2012', description: 'Printer cartridges HP 21'},
-                {id: 2, order_number: 65025073, date: '2/14/2013', description: 'Printer Cartridge HP 22'},
-                {id: 3, order_number: 65025496, date: '2/14/2013', description: 'HP LaserJet Printer - P3015DN'},
-                {id: 4, order_number: 65025623, date: '3/3/2013', description: 'Repair of HP printer 2024dn'},
-                {id: 5, order_number: 65026064, date: '3/3/2013', description: 'Servicing of Fax'},
-                {id: 6, order_number: 65026445, date: '3/25/2013', description: 'HP Toner 11A'}
+            purchaseOrders = [
+                {id: 1, doc_number: 65025072, date: '11/11/2014', programme: 'Keep Chlidren Learning'},
+                {id: 2, doc_number: 65025073, date: '2/10/2013', programme: 'Safe'},
+                {id: 3, doc_number: 65025496, date: '12/03/2013', programme: 'Alive'},
+                {id: 4, doc_number: 65025623, date: '3/3/2013', programme: 'Safe'},
+                {id: 5, doc_number: 65026064, date: '2/3/2014', programme: 'Alive'},
+                {id: 6, doc_number: 65026445, date: '4/21/2014', programme: 'Mothers'}
             ];
+            waybills = [
+                {id: 1, doc_number: 72081598, date: '12/13/2012', programme: 'Alive'},
+                {id: 2, doc_number: 72994735, date: '2/14/2013', programme: 'Safe'},
+                {id: 3, doc_number: 34839344, date: '2/14/2013', programme: 'Alive'},
+                {id: 4, doc_number: 20038445, date: '3/3/2013', programme: 'Alive'},
+                {id: 5, doc_number: 90384434, date: '3/3/2013', programme: 'Safe'},
+                {id: 6, doc_number: 10293800, date: '3/25/2013', programme: 'Mothers'}
+            ];
+            $scope.toggleDocumentType('PO');
+        };
+        
+        $scope.toggleDocumentType = function(type) {
+            $scope.currentDocumentType = type;
+            DistributionReportingParameters.saveVariable('currentDocumentType', type);
+            if(type === 'PO') {
+                $scope.documents = purchaseOrders;
+            }
+            else {
+                $scope.documents = waybills;
+            }
         };
 
         $scope.sortArrowClass = function (criteria) {
@@ -47,8 +70,9 @@ angular.module('ManualReporting', ['ngTable', 'siTable', 'NewDistributionPlan'])
             return output;
         };
 
-        $scope.selectPurchaseOrder = function () {
-            var orderDetails = {id: 1, order_number: 6654353, date: '2014-10-03', description: 'Printer cartridges HP 21', programme: 'Safe programme',
+        $scope.selectDocument = function (document) {
+            var orderDetails = {
+                id: document.id, order_number: document.doc_number, date: document.date, programme: document.programme,
                 items: [
                     {id: 1, description: 'Printer cartridges HP 21', material_code: '213442', quantity: 20, unit: 'kgs',
                         responses: [
@@ -63,7 +87,7 @@ angular.module('ManualReporting', ['ngTable', 'siTable', 'NewDistributionPlan'])
                         ]},
                     {id: 2, description: 'Printer cartridges HP 22', material_code: '2123342', quantity: 100, unit: 'kgs'},
                     {id: 1, description: 'Printer cartridges HP 23', material_code: '21346256', quantity: 500, unit: 'kgs'},
-                    {id: 1, description: 'Printer cartridges HP 24', material_code: '56787562', quantity: 600, unit: 'kgs'},
+                    {id: 1, description: 'Printer cartridges HP 24', material_code: '56787562', quantity: 600, unit: 'kgs'}
                 ]};
             DistributionReportingParameters.saveVariable('selectedPurchaseOrder', orderDetails);
             $location.path('/distribution-reporting/details/');
@@ -87,11 +111,11 @@ angular.module('ManualReporting', ['ngTable', 'siTable', 'NewDistributionPlan'])
             }, 2000);
         };
     })
-    .filter('purchaseOrderFilter', function ($filter) {
-        return  function (purchaseOrders, query) {
-            var results = $filter('filter')(purchaseOrders, {order_number: query});
-            results = _.union(results, $filter('filter')(purchaseOrders, {date: query}));
-            results = _.union(results, $filter('filter')(purchaseOrders, {description: query}));
+    .filter('documentFilter', function ($filter) {
+        return  function (documents, query) {
+            var results = $filter('filter')(documents, {order_number: query});
+            results = _.union(results, $filter('filter')(documents, {date: query}));
+            results = _.union(results, $filter('filter')(documents, {description: query}));
             return results;
         };
     })
