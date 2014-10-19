a1be95a8ec66c01b85400d84d200092fe144c613
Kelvin Smith <oneKelvinSmith@gmail.com>
2014-10-19 14:17:40 +0300

    [kelvin] X add font class for username and clean up user.js indentation

--- eums/client/app/scripts/user/user.js
+++ eums/client/app/scripts/user/user.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('User', ['eums.config']).factory('UserService', function ($http) {
-                 return {
-                     getUserById: function (userId){
-                         return $http.get('/api/user/' + userId + '/').then(function(response){
-                             return {
-                                 id: response.data.id,
-                                 firstName: response.data.first_name,
-                                 lastName: response.data.last_name
-                             };
-                         });
-                     }
-                 };
-             });
+    return {
+        getUserById: function (userId) {
+            return $http.get('/api/user/' + userId + '/').then(function (response) {
+                return {
+                    id: response.data.id,
+                    firstName: response.data.first_name,
+                    lastName: response.data.last_name
+                };
+            });
+        }
+    };
+});
