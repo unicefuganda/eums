b4aafadec6d3d6ce52d958c4534bda28f4977adb
Brain Leke <bleke@thoughtworks.com>
2014-10-20 08:49:57 +0200

    [Brain] #X Added test for new routes and templates in the app

--- eums/client/test/spec/app-spec.js
+++ eums/client/test/spec/app-spec.js
@@ -22,4 +22,28 @@ describe('Route Provider', function () {
         expect(routes.routes['/distribution-planning'].templateUrl).toBe('/static/app/views/distribution-planning/distribution-planning.html');
     });
 
+    it('should know new distribution plan route exists', function () {
+        expect((Object.keys(routes.routes))).toContain('/distribution-plan/new/');
+        expect(routes.routes['/distribution-plan/new/'].controller).toBe('NewDistributionPlanController');
+        expect(routes.routes['/distribution-plan/new/'].templateUrl).toBe('/static/app/views/distribution-planning/new.html');
+    });
+
+    it('should know proceed distribution plan route exists', function () {
+        expect((Object.keys(routes.routes))).toContain('/distribution-plan/proceed/');
+        expect(routes.routes['/distribution-plan/proceed/'].controller).toBe('NewDistributionPlanController');
+        expect(routes.routes['/distribution-plan/proceed/'].templateUrl).toBe('/static/app/views/distribution-planning/select-items.html');
+    });
+
+    it('should know distribution reporting route exists', function () {
+        expect((Object.keys(routes.routes))).toContain('/distribution-reporting');
+        expect(routes.routes['/distribution-reporting'].controller).toBe('ManualReportingController');
+        expect(routes.routes['/distribution-reporting'].templateUrl).toBe('/static/app/views/distribution-reporting/distribution-reporting.html');
+    });
+
+    it('should know distribution reporting details route exists', function () {
+        expect((Object.keys(routes.routes))).toContain('/distribution-reporting/details');
+        expect(routes.routes['/distribution-reporting/details'].controller).toBe('ManualReportingController');
+        expect(routes.routes['/distribution-reporting/details'].templateUrl).toBe('/static/app/views/distribution-reporting/details.html');
+    });
+
 });
