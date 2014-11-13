bd7fd7f6e05658506d823081071b8b57b2b9a264
Jeff Xiong <gigix1980@gmail.com>
2014-11-13 11:58:00 +0300

    template refactoring; added a test for users model

---
+++ eums/test/models/test_users_model.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from unittest import TestCase
+
+from eums.models.users import UserProfile
+
+
+class UserProfileTest(TestCase):
+    def test_user_fields(self):
+        user_profile = UserProfile()
+        fields = [str(item.attname) for item in user_profile._meta.fields]
+        self.assertEqual(4, len(fields))
+        for field in ['id', 'created', 'modified', 'user_id']:
+            self.assertIn(field, fields)
+
+    def test_profile_associated_with_user(self):
+        user = User.objects.create()
+        user_profile = UserProfile.objects.create(user=user)
+        self.failUnless(user_profile.id)
+        self.assertEqual(user, user_profile.user)
