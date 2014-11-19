ea6434f16a2b2f0277f380a263266bbdacd17cc3
Tunji Sunmonu <tunji.sunmonu@gmail.com>
2014-11-19 14:51:23 +0300

    changed sleep period from 3000 to 5000

--- eums/client/test/functional/home-spec.js
+++ eums/client/test/functional/home-spec.js
@@ -15,7 +15,7 @@ describe('Home Page', function () {
     });
 
     it('should get global stats on map', function () {
-        browser.sleep(3000);
+        browser.sleep(10000);
         expect(homePage.mapLocation.getText()).toEqual('UNICEF Delivery Status for UGANDA');
         expect(homePage.numberSent.getText()).toEqual('11');
         expect(homePage.numberDelivered.getText()).toEqual('8');
@@ -24,7 +24,7 @@ describe('Home Page', function () {
 
     it('should click on wakiso district', function () {
         homePage.clickMapLayer('wakiso');
-        browser.sleep(3000);
+        browser.sleep(10000);
         expect(homePage.mapLocation.getText()).toEqual('UNICEF Delivery Status for WAKISO');
         expect(homePage.getMapZoomLevel()).toBe(10);
         expect(homePage.numberSent.getText()).toEqual('2');
@@ -34,7 +34,7 @@ describe('Home Page', function () {
 
     it('when I click on district number of responses should be 10 or less', function () {
         homePage.clickMapLayer('wakiso');
-        browser.sleep(3000);
+        browser.sleep(10000);
         homePage.numberOfResponses.then(function (rows) {
             expect(rows.length).toBeLessThan(6);
         })
@@ -42,22 +42,22 @@ describe('Home Page', function () {
 
     it('should highlight a layer', function () {
         homePage.highLightMapLayer('wakiso');
-        browser.sleep(3000);
+        browser.sleep(10000);
         expect(homePage.getHighlightedLayerName()).toEqual('wakiso');
         expect(homePage.getHighlightedStyle('wakiso')).toEqual({ fillColor: '#FDAE61', fillOpacity: 0.6, weight: 1 });
     });
 
     it('responses panel should have a link to more details', function () {
         homePage.clickMapLayer('wakiso');
-        browser.sleep(5000);
+        browser.sleep(10000);
         expect(homePage.responsesPageLink.getText()).toEqual('View All Responses');
     });
 
     it('should navigate to detail reponses page when page link is clicked', function () {
         homePage.clickMapLayer('wakiso');
-        browser.sleep(5000);
+        browser.sleep(10000);
         responsePage = homePage.goToResponseDetailsPage();
-        browser.sleep(5000);
+        browser.sleep(10000);
         expect(responsePage.header.getText()).toEqual('All responses for WAKISO district');
         responsePage.numberOfResponses.then(function (rows) {
             expect(rows.length).toEqual(2);
@@ -67,9 +67,9 @@ describe('Home Page', function () {
 
     it('should search for "no" product received in Wakiso district', function () {
         homePage.clickMapLayer('wakiso');
-        browser.sleep(5000);
+        browser.sleep(10000);
         responsePage = homePage.goToResponseDetailsPage();
-        browser.sleep(5000);
+        browser.sleep(10000);
         responsePage.searchResponsesFor('no');
         responsePage.numberOfResponses.then(function (rows) {
             expect(rows.length).toEqual(1);
@@ -79,9 +79,9 @@ describe('Home Page', function () {
 
     it('should search for "no" product received in Wakiso district', function () {
         homePage.clickMapLayer('wakiso');
-        browser.sleep(5000);
+        browser.sleep(10000);
         responsePage = homePage.goToResponseDetailsPage();
-        browser.sleep(5000);
+        browser.sleep(10000);
         responsePage.searchResponsesFor('yes');
         responsePage.numberOfResponses.then(function (rows) {
             expect(rows.length).toEqual(1);
