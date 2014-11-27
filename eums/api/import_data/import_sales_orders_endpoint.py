e685d4503b1fd86c8c4cafa22522fefe6304246a
James Oloo Onyango <jamesony@thoughtworks.com>
2014-11-27 14:49:25 +0300

    [james] #X halted work on sales order import

--- eums/api/import_data/import_sales_orders_endpoint.py
+++ eums/api/import_data/import_sales_orders_endpoint.py
@@ -1,10 +1,10 @@
 import os
 from tempfile import NamedTemporaryFile
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import parser_classes
 from rest_framework.parsers import MultiPartParser
-from xlutils.view import View
 from eums.vision.vision_facade import SalesOrderFacade
 
 
@@ -12,15 +12,12 @@ from eums.vision.vision_facade import SalesOrderFacade
 @parser_classes((MultiPartParser,))
 def import_sales_orders(request):
     file_contents = request.FILES['file'].file.getvalue()
-    temp_file = NamedTemporaryFile(mode='w+b', delete=True)
+    temp_file = NamedTemporaryFile(mode='w+b', delete=False, suffix='.xlsx')
     temp_file.write(file_contents)
-    abs_path = os.path.abspath(temp_file.name)
-
-    print abs_path
-    view = View(abs_path)
-    print view[0][1:, :]
-
-    # sales_order_facade = SalesOrderFacade(temp_file.name)
-    # print sales_order_facade.load_order_data()
     temp_file.close()
+
+    sales_order_facade = SalesOrderFacade(temp_file.name)
+    sales_order_facade.import_orders()
+    os.remove(temp_file.name)
+
     return HttpResponse(status=200)
