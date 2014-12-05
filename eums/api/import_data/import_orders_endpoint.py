d9be579a7050721ac43c03175ae6d53b399cd282
James Oloo Onyango <jamesony@thoughtworks.com>
2014-12-05 20:45:16 +0300

    [nim, billy] #X renamed import sales order endpoint to import orders endpoint

--- eums/api/import_data/import_orders_endpoint.py
+++ eums/api/import_data/import_orders_endpoint.py
@@ -5,20 +5,30 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import parser_classes
 from rest_framework.parsers import MultiPartParser
-from eums.vision.vision_facade import SalesOrderFacade
+from eums.vision.vision_facade import SalesOrderFacade, ReleaseOrderFacade
 
 
 @csrf_exempt
 @parser_classes((MultiPartParser,))
 def import_sales_orders(request):
+    return _import_orders(request, SalesOrderFacade)
+
+
+@csrf_exempt
+@parser_classes((MultiPartParser,))
+def import_release_orders(request):
+    return _import_orders(request, ReleaseOrderFacade)
+
+
+def _import_orders(request, order_facade):
     if 'file' in request.FILES:
         file_contents = request.FILES['file'].file.getvalue()
         temp_file = NamedTemporaryFile(mode='w+b', delete=False, suffix='.xlsx')
         temp_file.write(file_contents)
         temp_file.close()
 
-        sales_order_facade = SalesOrderFacade(temp_file.name)
-        sales_order_facade.import_orders()
+        order_facade = order_facade(temp_file.name)
+        order_facade.import_orders()
         os.remove(temp_file.name)
 
     return HttpResponse(status=200)
