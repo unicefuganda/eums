abb735c14de0ba0fc9684a3fad2baa5356ecce88
wanderanimrod <wanderanimrod@gmail.com>
2015-05-08 11:51:06 +0300

    [nim, job] #X Service factory can fetch all and get particular object from arbitrary endpoint

---
+++ eums/client/app/scripts/services/service-factory.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('GenericService', []).factory('ServiceFactory', function($http, $q) {
+    return function(options) {
+        return {
+            all: function() {
+                return $http.get(options.uri).then(function(response) {
+                    return response.data;
+                });
+            },
+            get: function(id) {
+                return $http.get(options.uri + id + '/').then(function(response) {
+                    return response.data;
+                });
+            }
+        };
+    };
+});
+
---
+++ eums/client/test/spec/services/service-factory-spec.js
@@ -0,0 +1,36 @@
+describe('Service Factory', function () {
+    var mockBackend, q, service;
+    const endpointUrl = '/some-endpoint/';
+    const fakeOne = {id: 1, propertyOne: 'one', propertyTwo: 'two'};
+    const fakeTwo = {id: 2, propertyOne: 'one', propertyTwo: 'two'};
+    const fakeObjects = [fakeOne, fakeTwo];
+
+    beforeEach(function () {
+        module('GenericService');
+        inject(function (ServiceFactory, $httpBackend, $q) {
+            q = $q;
+            mockBackend = $httpBackend;
+            service = ServiceFactory({uri: endpointUrl});
+        });
+    });
+
+    it('should get all objects from api endpoint', function(done) {
+        mockBackend.whenGET(endpointUrl).respond(fakeObjects);
+        service.all().then(function(objects) {
+            expect(objects).toEqual(fakeObjects);
+            done();
+        });
+        mockBackend.flush();
+    });
+
+    it('should get object by id', function(done) {
+        mockBackend.whenGET(endpointUrl + fakeOne.id + '/').respond(fakeOne);
+        service.get(fakeOne.id).then(function(object) {
+            expect(object).toEqual(fakeOne);
+            done();
+        });
+        mockBackend.flush();
+    });
+});
+
+
