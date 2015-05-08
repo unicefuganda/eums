9bb6904cb7fa0a0e87904fefa4ed6541c2d4a88a
wanderanimrod <wanderanimrod@gmail.com>
2015-05-08 16:48:17 +0300

    [nim, job] #X Makes consumers of service factory able to catch errors when service api calls fail

--- eums/client/app/scripts/services/service-factory.js
+++ eums/client/app/scripts/services/service-factory.js
@@ -4,29 +4,19 @@ angular.module('GenericService', []).factory('ServiceFactory', function ($http, 
     return function (options) {
         return {
             all: function () {
-                return $http.get(options.uri).then(function (response) {
-                    return response.data;
-                });
+                return $http.get(options.uri);
             },
             get: function (id) {
-                return $http.get(options.uri + id + '/').then(function (response) {
-                    return response.data;
-                });
+                return $http.get(options.uri + id + '/');
             },
             create: function (object) {
-                return $http.post(options.uri, object).then(function (response) {
-                    return response.data;
-                })
+                return $http.post(options.uri, object);
             },
             update: function (object) {
-                return $http.put(options.uri + object.id + '/', object).then(function (response) {
-                    return response;
-                });
+                return $http.put(options.uri + object.id + '/', object);
             },
-            del: function(object) {
-                return $http.delete(options.uri + object.id + '/', object).then(function (response) {
-                    return response;
-                });
+            del: function (object) {
+                return $http.delete(options.uri + object.id + '/', object);
             }
         };
     };
--- eums/client/test/spec/services/service-factory-spec.js
+++ eums/client/test/spec/services/service-factory-spec.js
@@ -16,8 +16,17 @@ describe('Service Factory', function () {
 
     it('should get all objects from api endpoint', function (done) {
         mockBackend.whenGET(endpointUrl).respond(fakeObjects);
-        service.all().then(function (objects) {
+        service.all().success(function (objects) {
             expect(objects).toEqual(fakeObjects);
+            done();
+        });
+        mockBackend.flush();
+    });
+
+    it('should notify when fetching objects does not return 200', function (done) {
+        mockBackend.whenGET(endpointUrl).respond(401);
+        service.all().catch(function (error) {
+            expect(error.status).toBe(401);
             done();
         });
         mockBackend.flush();
@@ -25,8 +34,17 @@ describe('Service Factory', function () {
 
     it('should get object by id', function (done) {
         mockBackend.whenGET(endpointUrl + fakeOne.id + '/').respond(fakeOne);
-        service.get(fakeOne.id).then(function (object) {
+        service.get(fakeOne.id).success(function (object) {
             expect(object).toEqual(fakeOne);
+            done();
+        });
+        mockBackend.flush();
+    });
+
+    it('should notify when get object by id does not return 200', function (done) {
+        mockBackend.whenGET(endpointUrl + fakeOne.id + '/').respond(401);
+        service.get(fakeOne.id).catch(function (error) {
+            expect(error.status).toBe(401);
             done();
         });
         mockBackend.flush();
@@ -34,32 +52,64 @@ describe('Service Factory', function () {
 
     it('should create an object', function (done) {
         mockBackend.whenPOST(endpointUrl).respond(201, fakeOne);
-        service.create(fakeOne).then(function (object) {
+        service.create(fakeOne).success(function (object) {
             expect(object).toEqual(fakeOne);
             done();
         });
         mockBackend.flush();
     });
 
-    it('should update object', function(done) {
-        mockBackend.whenPUT(endpointUrl + fakeOne.id + '/').respond(200);
-        var changedOne = Object.clone(fakeOne);
-        changedOne.propertyOne = 'changed';
-        service.update(changedOne).then(function (response) {
-            expect(response.status).toEqual(200);
+    it('should notify when create does not return 200', function (done) {
+        mockBackend.whenPOST(endpointUrl).respond(401);
+        service.create(fakeOne).catch(function (error) {
+            expect(error.status).toBe(401);
             done();
         });
         mockBackend.flush();
     });
 
-    it('should delete object by id', function(done) {
-        mockBackend.whenDELETE(endpointUrl + fakeTwo.id + '/').respond(200);
-        service.del(fakeTwo).then(function(response) {
-            expect(response.status).toEqual(200);
+    it('should update object', function (done) {
+        mockBackend.whenPUT(endpointUrl + fakeOne.id + '/').respond(200);
+        var changedOne = Object.clone(fakeOne);
+        changedOne.propertyOne = 'changed';
+        service.update(changedOne).success(function (response, status) {
+            expect(status).toEqual(200);
             done();
         });
         mockBackend.flush();
     });
+
+    it('should notify when update object does not return 200', function (done) {
+        mockBackend.whenPUT(endpointUrl + fakeOne.id + '/').respond(401);
+        var changedOne = Object.clone(fakeOne);
+        changedOne.propertyOne = 'changed';
+        service.update(changedOne).catch(function (error) {
+            expect(error.status).toBe(401);
+            done();
+        });
+        mockBackend.flush();
+    });
+
+
+    it('should delete object by id', function (done) {
+        mockBackend.whenDELETE(endpointUrl + fakeTwo.id + '/').respond(200);
+        service.del(fakeTwo).success(function (response, status) {
+            expect(status).toEqual(200);
+            done();
+        });
+        mockBackend.flush();
+    });
+
+    it('should notify when delete does not return 200', function (done) {
+        mockBackend.whenDELETE(endpointUrl + fakeTwo.id + '/').respond(401);
+        service.del(fakeTwo).catch(function (error) {
+            expect(error.status).toBe(401);
+            done();
+        });
+        mockBackend.flush();
+    });
+
+
 });
 
 
