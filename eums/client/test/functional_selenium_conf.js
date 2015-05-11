825534029f6b12e1aac4d5e5ee88afbfe92d6363
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-11 17:18:58 +0300

    [#94162176] Fix to selenium functional testing

--- eums/client/test/functional_selenium_conf.js
+++ eums/client/test/functional_selenium_conf.js
@@ -1,3 +1,22 @@
+exports.config = {
+
+    seleniumServerJar: '../node_modules/selenium-standalone/.selenium/2.43.1/server.jar',
+
+    chromeDriver: '/usr/local/bin/chromedriver',
+
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect: false,
+    specs: ['test/functional/*-spec.js'],
+
+    baseUrl: 'http://localhost:8000',
+
+    framework: 'jasmine',
+
+    jasmineNodeOpts: {
+        showColors: true
+    }
+};
+/*
 var underscore = require("underscore");
 var mainConfig = require("./functional_conf.js");
 var seleniumConfig = underscore.extend(mainConfig.config, {seleniumAddress: 'http://localhost:4444/wd/hub', directConnect: false});
@@ -5,4 +24,4 @@ delete seleniumConfig.config.chromeOnly;
 console.log(seleniumConfig);
 
 exports.config = seleniumConfig;
-
+*/
