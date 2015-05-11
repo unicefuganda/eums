2bf256436cddec66a4246bf92ec6e4779f7c80ae
wanderanimrod <wanderanimrod@gmail.com>
2015-05-11 09:22:32 +0300

    [nim, job] #X Service.all fetches required nested objects

--- eums/client/app/scripts/services/service-factory.js
+++ eums/client/app/scripts/services/service-factory.js
@@ -1,9 +1,35 @@
 'use strict';
 
 angular.module('GenericService', []).factory('ServiceFactory', function ($http, $q) {
+    var buildObject = function (object, buildMap) {
+        var buildOutPromises = [];
+        Object.each(buildMap, function (property, service) {
+            buildOutPromises.push(service.get(object[property]))
+        });
+        return $q.all(buildOutPromises).then(function (responses) {
+            responses.each(function (buildResponse, index) {
+                object[Object.keys(buildMap)[index]] = buildResponse.data;
+            });
+            return object;
+        });
+    };
+
     return function (options) {
         return {
-            all: function () {
+            all: function (opts) {
+                //!opts && (opts = {build: {}});
+                if (opts) {
+                    return $http.get(options.uri).then(function (response) {
+                        var buildPromises = response.data.map(function (flatObject) {
+                            return buildObject(flatObject, opts.build);
+                        });
+                        return $q.all(buildPromises).then(function (builtObjects) {
+                            return builtObjects.map(function (object) {
+                                return object;
+                            });
+                        });
+                    });
+                }
                 return $http.get(options.uri);
             },
             get: function (id) {
--- eums/client/test/spec/services/service-factory-spec.js
+++ eums/client/test/spec/services/service-factory-spec.js
@@ -1,8 +1,11 @@
 describe('Service Factory', function () {
-    var mockBackend, q, service;
-    const endpointUrl = '/some-endpoint/';
-    const fakeOne = {id: 1, propertyOne: 'one', propertyTwo: 'two'};
-    const fakeTwo = {id: 2, propertyOne: 'one', propertyTwo: 'two'};
+    var mockBackend, q, topLevelService, nestedService;
+    const topLevelEndpoint = '/some-endpoint/';
+    const nestedEndpoint = '/nested-endpoint/';
+    const fakeOne = {id: 1, propertyOne: 'one', propertyTwo: 'two', nested: 1};
+    const fakeTwo = {id: 2, propertyOne: 'one', propertyTwo: 'two', nested: 2};
+    const nestedOne = {id: 1, properties: {}};
+    const nestedTwo = {id: 2, properties: []};
     const fakeObjects = [fakeOne, fakeTwo];
 
     beforeEach(function () {
@@ -10,13 +13,14 @@ describe('Service Factory', function () {
         inject(function (ServiceFactory, $httpBackend, $q) {
             q = $q;
             mockBackend = $httpBackend;
-            service = ServiceFactory({uri: endpointUrl});
+            nestedService = ServiceFactory({uri: nestedEndpoint});
+            topLevelService = ServiceFactory({uri: topLevelEndpoint});
         });
     });
 
     it('should get all objects from api endpoint', function (done) {
-        mockBackend.whenGET(endpointUrl).respond(fakeObjects);
-        service.all().success(function (objects) {
+        mockBackend.whenGET(topLevelEndpoint).respond(fakeObjects);
+        topLevelService.all().success(function (objects) {
             expect(objects).toEqual(fakeObjects);
             done();
         });
@@ -24,17 +28,33 @@ describe('Service Factory', function () {
     });
 
     it('should notify when fetching objects does not return 200', function (done) {
-        mockBackend.whenGET(endpointUrl).respond(401);
-        service.all().catch(function (error) {
+        mockBackend.whenGET(topLevelEndpoint).respond(401);
+        topLevelService.all().catch(function (error) {
             expect(error.status).toBe(401);
             done();
         });
         mockBackend.flush();
     });
 
+    it('should build nested objects when fetching all object if needed', function(done) {
+        mockBackend.whenGET(topLevelEndpoint).respond(fakeObjects);
+        mockBackend.whenGET('{1}{2}/'.assign(nestedEndpoint, nestedOne.id)).respond(nestedOne);
+        mockBackend.whenGET('{1}{2}/'.assign(nestedEndpoint, nestedTwo.id)).respond(nestedTwo);
+        var expectedFakeOne = Object.clone(fakeOne);
+        var expectedFakeTwo = Object.clone(fakeTwo);
+        expectedFakeOne.nested = nestedOne;
+        expectedFakeTwo.nested = nestedTwo;
+
+        topLevelService.all({build: {nested: nestedService}}).then(function(objects) {
+            expect(objects).toEqual([expectedFakeOne, expectedFakeTwo]);
+            done();
+        });
+        mockBackend.flush();
+    });
+
     it('should get object by id', function (done) {
-        mockBackend.whenGET(endpointUrl + fakeOne.id + '/').respond(fakeOne);
-        service.get(fakeOne.id).success(function (object) {
+        mockBackend.whenGET(topLevelEndpoint + fakeOne.id + '/').respond(fakeOne);
+        topLevelService.get(fakeOne.id).success(function (object) {
             expect(object).toEqual(fakeOne);
             done();
         });
@@ -42,8 +62,8 @@ describe('Service Factory', function () {
     });
 
     it('should notify when get object by id does not return 200', function (done) {
-        mockBackend.whenGET(endpointUrl + fakeOne.id + '/').respond(401);
-        service.get(fakeOne.id).catch(function (error) {
+        mockBackend.whenGET(topLevelEndpoint + fakeOne.id + '/').respond(401);
+        topLevelService.get(fakeOne.id).catch(function (error) {
             expect(error.status).toBe(401);
             done();
         });
@@ -51,8 +71,8 @@ describe('Service Factory', function () {
     });
 
     it('should create an object', function (done) {
-        mockBackend.whenPOST(endpointUrl).respond(201, fakeOne);
-        service.create(fakeOne).success(function (object) {
+        mockBackend.whenPOST(topLevelEndpoint).respond(201, fakeOne);
+        topLevelService.create(fakeOne).success(function (object) {
             expect(object).toEqual(fakeOne);
             done();
         });
@@ -60,8 +80,8 @@ describe('Service Factory', function () {
     });
 
     it('should notify when create does not return 200', function (done) {
-        mockBackend.whenPOST(endpointUrl).respond(401);
-        service.create(fakeOne).catch(function (error) {
+        mockBackend.whenPOST(topLevelEndpoint).respond(401);
+        topLevelService.create(fakeOne).catch(function (error) {
             expect(error.status).toBe(401);
             done();
         });
@@ -69,10 +89,10 @@ describe('Service Factory', function () {
     });
 
     it('should update object', function (done) {
-        mockBackend.whenPUT(endpointUrl + fakeOne.id + '/').respond(200);
+        mockBackend.whenPUT(topLevelEndpoint + fakeOne.id + '/').respond(200);
         var changedOne = Object.clone(fakeOne);
         changedOne.propertyOne = 'changed';
-        service.update(changedOne).success(function (response, status) {
+        topLevelService.update(changedOne).success(function (response, status) {
             expect(status).toEqual(200);
             done();
         });
@@ -80,10 +100,10 @@ describe('Service Factory', function () {
     });
 
     it('should notify when update object does not return 200', function (done) {
-        mockBackend.whenPUT(endpointUrl + fakeOne.id + '/').respond(401);
+        mockBackend.whenPUT(topLevelEndpoint + fakeOne.id + '/').respond(401);
         var changedOne = Object.clone(fakeOne);
         changedOne.propertyOne = 'changed';
-        service.update(changedOne).catch(function (error) {
+        topLevelService.update(changedOne).catch(function (error) {
             expect(error.status).toBe(401);
             done();
         });
@@ -92,8 +112,8 @@ describe('Service Factory', function () {
 
 
     it('should delete object by id', function (done) {
-        mockBackend.whenDELETE(endpointUrl + fakeTwo.id + '/').respond(200);
-        service.del(fakeTwo).success(function (response, status) {
+        mockBackend.whenDELETE(topLevelEndpoint + fakeTwo.id + '/').respond(200);
+        topLevelService.del(fakeTwo).success(function (response, status) {
             expect(status).toEqual(200);
             done();
         });
@@ -101,8 +121,8 @@ describe('Service Factory', function () {
     });
 
     it('should notify when delete does not return 200', function (done) {
-        mockBackend.whenDELETE(endpointUrl + fakeTwo.id + '/').respond(401);
-        service.del(fakeTwo).catch(function (error) {
+        mockBackend.whenDELETE(topLevelEndpoint + fakeTwo.id + '/').respond(401);
+        topLevelService.del(fakeTwo).catch(function (error) {
             expect(error.status).toBe(401);
             done();
         });
