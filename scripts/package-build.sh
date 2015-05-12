f0c2df82319f5c4f8fa2ce75a488d2d1d8f5ebbb
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-12 13:14:10 +0300

    [#94162176] Fixes to packaging script not picking up GO env variables

--- scripts/package-build.sh
+++ scripts/package-build.sh
@@ -7,5 +7,5 @@ gitRevision=$GO_REVISION
 echo "Packaging for Artifact=$artifactName BuildCounter=$artifactCounter GitRevision=$gitRevision"
 
 mkdir -p /opt/app/staging/$artifactName/$artifactCounter
-docker build -t thoughtworks/bulamu-admin:$artifactCounter . && \
+docker build -t thoughtworks/eums:$artifactCounter .
 docker save -o /opt/app/staging/$artifactName/$artifactCounter/eums_docker_image.tar thoughtworks/eums:$artifactCounter
