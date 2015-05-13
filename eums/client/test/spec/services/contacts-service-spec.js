0b8609ccaca76ffa83d74aa948d85e9448119372
wanderanimrod <wanderanimrod@gmail.com>
2015-05-13 17:11:12 +0300

    [nim, job] #X Contacts service now works with service factory

--- eums/client/test/spec/services/contacts-service-spec.js
+++ eums/client/test/spec/services/contacts-service-spec.js
@@ -47,49 +47,49 @@ describe('Contacts Service', function () {
         mockContactsBackend.flush();
     });
 
-    //it('should get contact from contacts backend by id', function (done) {
-    //    mockContactsBackend.whenGET(config.CONTACT_SERVICE_URL + expectedContact._id + '/').respond(expectedContact);
-    //    contactService.get(expectedContact._id).then(function (contact) {
-    //        expect(contact).toEqual(expectedContact);
-    //        done();
-    //    });
-    //    mockContactsBackend.flush();
-    //});
-    //
-    //it('should search for contact by search string', function (done) {
-    //    var searchString = expectedContact.firstName;
-    //    mockContactsBackend.whenGET(config.CONTACT_SERVICE_URL + '?searchfield=' + searchString).respond(expectedContact);
-    //    contactService.getContactsBySearchQuery(searchString).then(function (contact) {
-    //        expect(contact).toEqual(expectedContact);
-    //        done();
-    //    });
-    //    mockContactsBackend.flush();
-    //});
-    //
-    //it('should load all contacts', function (done) {
-    //    mockContactsBackend.whenGET(config.CONTACT_SERVICE_URL).respond(expectedContacts);
-    //    contactService.all().then(function (contacts) {
-    //        expect(contacts).toEqual(expectedContacts);
-    //        done();
-    //    });
-    //    mockContactsBackend.flush();
-    //});
-    //
-    //it('should delete a contact', function (done) {
-    //    mockContactsBackend.whenDELETE(config.CONTACT_SERVICE_URL + expectedContact._id + '/').respond(null);
-    //    contactService.del(expectedContact).then(function (response) {
-    //        expect(response).toBe(null);
-    //        done();
-    //    });
-    //    mockContactsBackend.flush();
-    //});
-    //
-    //it('should edit an existing contact', function(done) {
-    //    mockContactsBackend.whenPUT(config.CONTACT_SERVICE_URL, expectedContact).respond(expectedContact);
-    //    contactService.update(expectedContact).then(function(response) {
-    //        expect(response).toEqual(expectedContact);
-    //        done();
-    //    });
-    //    mockContactsBackend.flush();
-    //});
+    it('should get contact from contacts backend by id', function (done) {
+        mockContactsBackend.whenGET(config.CONTACT_SERVICE_URL + expectedContact._id + '/').respond(expectedContact);
+        contactService.get(expectedContact._id).then(function (contact) {
+            expect(contact).toEqual(expectedContact);
+            done();
+        });
+        mockContactsBackend.flush();
+    });
+
+    it('should search for contact by search string', function (done) {
+        var searchString = expectedContact.firstName;
+        mockContactsBackend.whenGET(config.CONTACT_SERVICE_URL + '?searchfield=' + searchString).respond(expectedContact);
+        contactService.getContactsBySearchQuery(searchString).then(function (contact) {
+            expect(contact).toEqual(expectedContact);
+            done();
+        });
+        mockContactsBackend.flush();
+    });
+
+    it('should load all contacts', function (done) {
+        mockContactsBackend.whenGET(config.CONTACT_SERVICE_URL).respond(expectedContacts);
+        contactService.all().then(function (contacts) {
+            expect(contacts).toEqual(expectedContacts);
+            done();
+        });
+        mockContactsBackend.flush();
+    });
+
+    it('should delete a contact', function (done) {
+        mockContactsBackend.whenDELETE(config.CONTACT_SERVICE_URL + expectedContact._id + '/').respond(200);
+        contactService.del(expectedContact).then(function (status) {
+            expect(status).toBe(200);
+            done();
+        });
+        mockContactsBackend.flush();
+    });
+
+    it('should edit an existing contact', function(done) {
+        mockContactsBackend.expectPUT(config.CONTACT_SERVICE_URL, expectedContact).respond(expectedContact);
+        contactService.update(expectedContact).then(function(response) {
+            expect(response).toEqual(expectedContact);
+            done();
+        });
+        mockContactsBackend.flush();
+    });
 });
