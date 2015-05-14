d3300bb41a0ec70d95c9cdac83aaba4ad3346331
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-14 16:29:48 +0300

    [#94162176] Fixes to packaging build

--- scripts/packaging/package-build.sh
+++ scripts/packaging/package-build.sh
@@ -9,10 +9,12 @@ echo "Packaging for Artifact=$artifactName BuildCounter=$artifactCounter GitRevi
 #Build and save the image
 mkdir -p /opt/app/staging/$artifactName/$artifactCounter
 sudo docker build -t unicef/$artifactName:$artifactCounter .
-sudo docker save -o $artifactName"_docker_image.tar" unicef/$artifactName:$artifactCounter
+mkdir build
+sudo docker save -o build/$artifactName"_docker_image.tar" unicef/$artifactName:$artifactCounter
 
 #Prepare the script that will install the image
 sed -i -e 's/%IMAGEFILE%/$artifactName_docker_image\.tar/g' scripts/packaging/install-image-eums.sh
 sed -i -e 's/%IMAGENAME%/unicef\/$artifactName/g' scripts/packaging/install-image-eums.sh
 sed -i -e 's/%IMAGEVERSION%/$artifactCounter/g' scripts/packaging/install-image-eums.sh
+cp scripts/packaging/install-image-eums.sh build/scripts/packaging/install-image-eums.shcd
 
