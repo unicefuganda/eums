3b67b1673ae5be31244683c3c2e161b3baf23260
wanderanimrod <wanderanimrod@gmail.com>
2015-05-14 16:58:32 +0300

    [nim, jafari] #X Migrating Answers service to use service factory in the back

--- eums/client/test/spec/services/answer-service-spec.js
+++ eums/client/test/spec/services/answer-service-spec.js
@@ -1,13 +1,12 @@
-describe('Answer Service', function() {
-
+describe('Answer Service', function () {
     var answerService, mockBackend, textAnswerEndpointUrl, numericAnswerEndpointUrl, multipleChoiceAnswerEndpointUrl;
 
-    var answerId = 1, questionId = 1, lineItemRunId = 1;
+    var answerId = 1;
 
-    beforeEach(function() {
+    beforeEach(function () {
         module('Answer');
 
-        inject(function(AnswerService, $httpBackend, EumsConfig) {
+        inject(function (AnswerService, $httpBackend, EumsConfig) {
             mockBackend = $httpBackend;
             textAnswerEndpointUrl = EumsConfig.BACKEND_URLS.TEXT_ANSWERS;
             numericAnswerEndpointUrl = EumsConfig.BACKEND_URLS.NUMERIC_ANSWERS;
@@ -16,105 +15,33 @@ describe('Answer Service', function() {
         });
     });
 
-    describe('Text Answers', function() {
-        var stubTextAnswer = {
-            question: questionId,
-            value: 'Test Item',
-            line_item_run: lineItemRunId
-        };
-
-        var expectedAnswer = {
-            id: answerId,
-            question: questionId,
-            value: 'Test Item',
-            line_item_run: lineItemRunId
-        };
-
-        it('should know how to create a text answer', function (done) {
-            mockBackend.whenPOST(textAnswerEndpointUrl).respond(201, expectedAnswer);
-            answerService.createTextAnswer(stubTextAnswer).then(function (response) {
-                expect(response).toEqual(expectedAnswer);
-                done();
-            });
-            mockBackend.flush();
+    it('Text Answers should know how to update a text answer', function (done) {
+        var answer = {value: 'Edited'};
+        var data = {id: answerId, value: answer.value};
+        mockBackend.expectPATCH(textAnswerEndpointUrl + answerId + '/', data).respond(200);
+        answerService.updateTextAnswer(answerId, answer).then(function () {
+            done();
         });
-
-        it('should know how to update a text answer', function (done) {
-            var updatedTextAnswerField = {value: 'Edited'};
-            mockBackend.whenPATCH(textAnswerEndpointUrl + answerId + '/').respond(expectedAnswer);
-            answerService.updateTextAnswer(answerId, updatedTextAnswerField).then(function (response) {
-                expect(response).toEqual(expectedAnswer);
-                done();
-            });
-            mockBackend.flush();
-        });
+        mockBackend.flush();
     });
 
-    describe('Numeric Answers', function() {
-        var stubNumericAnswer = {
-            question: questionId,
-            value: 1,
-            line_item_run: lineItemRunId
-        };
-
-        var expectedAnswer = {
-            id: answerId,
-            question: questionId,
-            value: 1,
-            line_item_run: lineItemRunId
-        };
-
-        it('should know how to create a numeric answer', function (done) {
-            mockBackend.whenPOST(numericAnswerEndpointUrl).respond(201, expectedAnswer);
-            answerService.createNumericAnswer(stubNumericAnswer).then(function (response) {
-                expect(response).toEqual(expectedAnswer);
-                done();
-            });
-            mockBackend.flush();
+    it('Numeric Answers should know how to update text answer', function (done) {
+        var answer = {value: 3};
+        var data = {id: answerId, value: answer.value};
+        mockBackend.expectPATCH(numericAnswerEndpointUrl + answerId + '/', data).respond(200);
+        answerService.updateNumericAnswer(answerId, answer).then(function () {
+            done();
         });
-
-        it('should know how to update text answer', function (done) {
-            var updatedNumericAnswerField = {value: 3};
-            mockBackend.whenPATCH(numericAnswerEndpointUrl + answerId + '/').respond(expectedAnswer);
-            answerService.updateNumericAnswer(answerId, updatedNumericAnswerField).then(function (response) {
-                expect(response).toEqual(expectedAnswer);
-                done();
-            });
-            mockBackend.flush();
-        });
+        mockBackend.flush();
     });
 
-    describe('Multiple Choice Answers', function() {
-        var stubMultipleChoiceAnswer = {
-            question: questionId,
-            value: 1,
-            line_item_run: lineItemRunId
-        };
-
-        var expectedAnswer = {
-            id: answerId,
-            question: questionId,
-            value: 1,
-            line_item_run: lineItemRunId
-        };
-
-        it('should know how to create a multiple choice answer', function (done) {
-            mockBackend.whenPOST(multipleChoiceAnswerEndpointUrl).respond(201, expectedAnswer);
-            answerService.createMultipleChoiceAnswer(stubMultipleChoiceAnswer).then(function (response) {
-                expect(response).toEqual(expectedAnswer);
-                done();
-            });
-            mockBackend.flush();
+    it('Multiple Choice Answers should know how to update multiple choice answer', function (done) {
+        var answer = {value: 3};
+        var data = {id: answerId, value: answer.value};
+        mockBackend.expectPATCH(multipleChoiceAnswerEndpointUrl + answerId + '/', data).respond(200);
+        answerService.updateMultipleChoiceAnswer(answerId, answer).then(function () {
+            done();
         });
-
-        it('should know how to update multiple choice answer', function (done) {
-            var updatedMultipleChoiceAnswerField = {value: 3};
-            mockBackend.whenPATCH(multipleChoiceAnswerEndpointUrl + answerId + '/').respond(expectedAnswer);
-            answerService.updateMultipleChoiceAnswer(answerId, updatedMultipleChoiceAnswerField).then(function (response) {
-                expect(response).toEqual(expectedAnswer);
-                done();
-            });
-            mockBackend.flush();
-        });
+        mockBackend.flush();
     });
 });
