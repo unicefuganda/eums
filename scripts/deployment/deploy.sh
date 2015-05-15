64bfb2a66a6b630cad523f8e416771af6d287566
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-15 13:51:37 +0300

    [#94162176] Ensure deploment directory exists on target box

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -4,6 +4,7 @@ today=`date +%Y-%m-%d.%H:%M:%S`
 
 #push the image install script and docker-compose.yml and image to the target box
 chmod 600 ${DEPLOY_MACHINE_KEY_FILE}
+ssh -o StrictHostKeyChecking=no -i ${DEPLOY_MACHINE_KEY_FILE} 'mkdir -p ~/deploy_$today/scripts'
 scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/build/deployment ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/scripts
 scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/*.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
 
