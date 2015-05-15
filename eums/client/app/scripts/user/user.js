3daaf641734cfd494c1c242321e444909342e7f1
wanderanimrod <wanderanimrod@gmail.com>
2015-05-15 10:46:35 +0300

    [nim, jafari] #X refactors user service

--- eums/client/app/scripts/user/user.js
+++ eums/client/app/scripts/user/user.js
@@ -2,26 +2,15 @@
 
 angular.module('User', ['eums.config']).factory('UserService', function ($http) {
     return {
-        getUserById: function (userId) {
-            return $http.get('/api/user/' + userId + '/').then(function (response) {
-                return {
-                    id: response.data.id,
-                    firstName: response.data.first_name,
-                    lastName: response.data.last_name
-                };
-            });
-        },
-
         getCurrentUser: function () {
             return $http.get('/api/current-user/').then(function (response) {
                 return response.data;
             });
         },
-
         checkUserPermission: function (permission) {
             return $http.get('/api/permission?permission=' + permission).then(function () {
                 return true;
-            }, function () {
+            }).catch(function () {
                 return false;
             });
         }
