0fb9dba7d0355a4c031223c5535f5207eb1bd043
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-15 12:30:21 +0300

    [#94162176] Removed strict host key checking for SSH and SCP

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -3,10 +3,10 @@
 today=`date +%Y-%m-%d.%H:%M:%S`
 
 #push the image install script and docker-compose.yml and image to the target box
-scp -r -i ${DEPLOY_MACHINE_KEY_FILE} scripts ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
-scp -r -i ${DEPLOY_MACHINE_KEY_FILE} *.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
-scp -r -i ${DEPLOY_MACHINE_KEY_FILE} docker-compose.yml ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
+scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} scripts ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
+scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} *.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
+scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} docker-compose.yml ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
 
 #run the deployment script via ssh on the server
-ssh -i ${DEPLOY_MACHINE_KEY_FILE} "/deploy_$today/scripts/deployment/install-image.sh ~/deploy_$today"
+ssh -o StrictHostKeyChecking=no -i ${DEPLOY_MACHINE_KEY_FILE} "/deploy_$today/scripts/deployment/install-image.sh ~/deploy_$today"
 
