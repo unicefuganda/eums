16a5a6a7b61084ef77eea663e9eeea00ae7979b4
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-15 13:14:51 +0300

    [#94162176] Fixed deployment path issues

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -3,9 +3,9 @@
 today=`date +%Y-%m-%d.%H:%M:%S`
 
 #push the image install script and docker-compose.yml and image to the target box
-scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} scripts ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
-scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} *.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
-scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} docker-compose.yml ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
+scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/build/deployment ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/scripts
+scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/*.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
+scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/docker-compose.yml ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
 
 #run the deployment script via ssh on the server
 ssh -o StrictHostKeyChecking=no -i ${DEPLOY_MACHINE_KEY_FILE} "/deploy_$today/scripts/deployment/install-image.sh ~/deploy_$today"
