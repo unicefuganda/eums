ed4447895c14ed9937251b32a18ebdea9ab4dec6
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-15 13:21:56 +0300

    [#94162176] Restricted keyfile to facilitate deploy

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -3,6 +3,7 @@
 today=`date +%Y-%m-%d.%H:%M:%S`
 
 #push the image install script and docker-compose.yml and image to the target box
+chmod 600 ${DEPLOY_MACHINE_KEY_FILE}
 scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/build/deployment ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/scripts
 scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/*.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
 scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} eums/docker-compose.yml ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
