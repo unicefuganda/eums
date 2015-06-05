9434504b1f2524a4c4ee5e67b6531dd24db9d872
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-05 14:14:36 +0300

    [94162176] fixes to the functional test pipeline

--- scripts/testing/run_functional_tests.sh
+++ scripts/testing/run_functional_tests.sh
@@ -5,6 +5,8 @@ set -e
 virtualenv eums_env
 source eums_env/bin/activate
 pip install -r requirements.txt
+dropdb eums_test
+createdb eums_test
 cd eums/client
 sudo npm install -g grunt-cli
 npm install
