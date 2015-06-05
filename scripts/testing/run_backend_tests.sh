88c24be16ebe4abce9bca49fffed5ae20fc8fdb3
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-05 13:27:11 +0300

    [#94161964] Abe Job Fix to go build

--- scripts/testing/run_backend_tests.sh
+++ scripts/testing/run_backend_tests.sh
@@ -4,6 +4,8 @@ set -e
 
 pip install -r requirements.txt
 pip install python-coveralls
+dropdb app_test
+createdb app_test
 python manage.py migrate --settings=eums.snap_settings
 echo repo_token: $COVERALLS_REPO_TOKEN >> .coveralls.yml
 coverage run --source=eums ./manage.py test --settings=eums.snap_settings -v 2
