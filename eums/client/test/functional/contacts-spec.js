ae303731314f413c150e0b91b2e369b1d8c670ba
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-06 14:14:32 +0300

    [#94162176] Fixes to deployment stage

--- eums/client/test/functional/contacts-spec.js
+++ eums/client/test/functional/contacts-spec.js
@@ -3,6 +3,48 @@
 describe('contacts page', function () {
     var loginPage;
 
+    function waitForPromiseTest(promiseFn, testFn) {
+        browser.wait(function () {
+            var deferred = protractor.promise.defer();
+            promiseFn().then(function (data) {
+                deferred.fulfill(testFn(data));
+            });
+            return deferred.promise;
+        });
+    }
+
+
+
+    function waitForPredicate(predicate, target){
+        //browser.wait(function () {
+        //    return elm.isPresent();
+        //},10000);
+        //browser.wait(function () {
+        //    return elm.isDisplayed();
+        //},10000);
+
+        browser.wait(function () {
+            return predicate().then(function(value){return value;});
+        }.bind(this), 5000).then(function(){
+            console.log("predicate set. calling target ");
+            target();
+        }.bind(this), function(err){
+            console.log("Timeout occured Abe");
+        });
+
+
+        //browser.wait(function() {
+        //    var deferred = protractor.promise.defer();
+        //    element(by.id('some-element')).isPresent()
+        //        .then(function (isPresent) {
+        //            deferred.fulfill(!isPresent);
+        //        });
+        //    return deferred.promise;
+        //});
+    }
+
+
+
     beforeEach(function () {
         loginPage = require('./pages/login-page');
         browser.ignoreSynchronization = true;
@@ -15,12 +57,21 @@ describe('contacts page', function () {
         loginPage.logout();
     });
 
-    it('should go to the contacts page', function () {
+    iit('should go to the contacts page', function () {
         //FIXME need to remove these arbitrary waits. They make tests either flaky or unnecessarily long
-        browser.sleep(5000);
+        waitForPredicate(element(by.id('admin-nav')).isElementPresent, function(){console.log("target called!")})
+
+        waitForPromiseTest(element(by.id('admin-nav')).isPresent,
+            function (isPresent) {
+                return !isPresent;
+            }).then(console.log("Duude is present"));
+
+        //browser.sleep(5000);
         element(by.id('admin-nav')).click();
+        waitForPredicate(element(by.id('contact-nav')).isElementPresent, function(){console.log("target2 called!")})
         element(by.id('contact-nav')).click();
-        browser.sleep(5000);
+        //browser.sleep(5000);
+        waitForPredicate(element(by.css('.page-header')).isElementPresent, function(){console.log("target3 called!")})
         expect(element(by.css('.page-header')).getText()).toEqual('Contacts');
         expect(element(by.id('add-contact')).getText()).toEqual('Add Contact');
     });
--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -2,12 +2,12 @@
 
 today=`date +%Y-%m-%d.%H:%M:%S`
 
-#push the image install script and docker-compose.yml and image to the target box
-chmod 600 ${DEPLOY_MACHINE_KEY_FILE}
-ssh -o StrictHostKeyChecking=no -i ${DEPLOY_MACHINE_KEY_FILE} ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ~/deploy_$today/scripts"
-scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} build/deployment ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/scripts
-scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} build/*.sh ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/scripts
-scp -o StrictHostKeyChecking=no -r -i ${DEPLOY_MACHINE_KEY_FILE} build/*.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
+sshpass -p '${DEPLOY_USER_PASSWORD}' ssh -o StrictHostKeyChecking=no ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ~/deploy_$today/scripts"
+sshpass -p '${DEPLOY_USER_PASSWORD}' scp build/deployment ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/scripts
+sshpass -p '${DEPLOY_USER_PASSWORD}' scp build/*.sh ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/scripts
+sshpass -p '${DEPLOY_USER_PASSWORD}' scp build/*.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~/deploy_$today/
+
+
 
 #run the deployment script via ssh on the server
-ssh -o StrictHostKeyChecking=no -i ${DEPLOY_MACHINE_KEY_FILE} root@${DEPLOY_HOST} "cd /home/${DEPLOY_USER}/deploy_$today && chmod a+x scripts/*.sh && scripts/deployment/install.sh"
+sshpass -p '${DEPLOY_USER_PASSWORD}' ssh -o StrictHostKeyChecking=no ${DEPLOY_USER}@${DEPLOY_HOST} "cd /home/${DEPLOY_USER}/deploy_$today && chmod a+x scripts/*.sh && scripts/deployment/install-image-eums.sh"
