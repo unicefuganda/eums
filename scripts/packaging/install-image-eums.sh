2780e0586f8e43b438919b340b9a9f94e23546cc
Jafari <jafaris@thoughtworks.com>
2015-06-08 12:54:55 +0300

    jafari, @francikas #95687036: image build and deployment change

--- scripts/packaging/install-image-eums.sh
+++ scripts/packaging/install-image-eums.sh
@@ -17,4 +17,4 @@ docker run -p ${DEPLOY_MACHINE_SSH_PORT}:22 -p ${DEPLOY_MACHINE_HTTP_PORT}:80 -d
 echo "Done!"
 
 echo "Editing host name"
-sshpass -p 'password' ssh -p ${DEPLOY_MACHINE_SSH_PORT} root@0.0.0.0 'sed -i -e "s/%EUMS_HOST_NAME%/${EUMS_HOST_NAME}/g" /etc/nginx/sites-available/eums.nginx.conf && ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled && supervisorctl restart nginx'
+sshpass -p 'password' ssh -p ${DEPLOY_MACHINE_SSH_PORT} -o StrictHostKeyChecking=no root@0.0.0.0 'sed -i -e "s/%EUMS_CONTAINER_HOST_NAME%/${EUMS_CONTAINER_HOST_NAME}/g" /etc/nginx/sites-available/eums.nginx.conf && ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled && supervisorctl restart nginx'
