c3c3d07f690276df6b99a0868491d47752a4ad53
Tom Oketch <tc.oketch@gmail.com>
2015-06-11 22:30:31 +0300

    @tomclement: Update feature test protractor conf options
    ✓ Add sane timeouts for protractor actions
    ✓ Turn on jasmine-spec-reporter stacktraces
    ✓ Show protractor spec durations

--- eums/client/test/functional_conf.js
+++ eums/client/test/functional_conf.js
@@ -1,6 +1,7 @@
 var SpecReporter = require('jasmine-spec-reporter');
 
 exports.config = {
+    allScriptsTimeout: 15000,
 
     seleniumServerJar: '../node_modules/selenium-standalone/.selenium/2.43.1/server.jar',
 
@@ -15,12 +16,15 @@ exports.config = {
     framework: 'jasmine',
 
     jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000,
         print: function () {}
     },
 
     onPrepare: function () {
         jasmine.getEnv().addReporter(new SpecReporter({
-            displayFailuresSummary: true
+            displayFailuresSummary: true,
+            displaySpecDuration: true,
+            displayStacktrace: 'summary'
         }));
     }
 };
