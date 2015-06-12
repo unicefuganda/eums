64e744e62fb3003f9982abce4843cf35ba8af25a
Tom Oketch <tc.oketch@gmail.com>
2015-06-12 10:29:33 +0300

    @tomclement: Use same options for protractor:headless_selenium as protractor:headless

--- eums/client/test/functional_selenium_conf.js
+++ eums/client/test/functional_selenium_conf.js
@@ -1,11 +1,16 @@
+var SpecReporter = require('jasmine-spec-reporter');
+
 exports.config = {
+    allScriptsTimeout: 15000,
 
     seleniumServerJar: '../node_modules/selenium-standalone/.selenium/2.43.1/server.jar',
 
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+
     chromeDriver: '/usr/local/bin/chromedriver',
 
-    seleniumAddress: 'http://localhost:4444/wd/hub',
     directConnect: false,
+
     specs: ['test/functional/*-spec.js'],
 
     baseUrl: process.env.EUMS_HOST_URL,
@@ -13,15 +18,15 @@ exports.config = {
     framework: 'jasmine',
 
     jasmineNodeOpts: {
-        showColors: true
+        defaultTimeoutInterval: 30000,
+        print: function () {}
+    },
+
+    onPrepare: function () {
+        jasmine.getEnv().addReporter(new SpecReporter({
+            displayFailuresSummary: true,
+            displaySpecDuration: true,
+            displayStacktrace: 'summary'
+        }));
     }
 };
-/*
-var underscore = require("underscore");
-var mainConfig = require("./functional_conf.js");
-var seleniumConfig = underscore.extend(mainConfig.config, {seleniumAddress: 'http://localhost:4444/wd/hub', directConnect: false});
-delete seleniumConfig.config.chromeOnly;
-console.log(seleniumConfig);
-
-exports.config = seleniumConfig;
-*/
