d972bd9178b91ea73515deb8c94d134ee4b2349f
Jafari <jafaris@thoughtworks.com>
2015-06-12 15:26:25 +0300

    jafari #95687036: correct location of nginx cong file and provide right scp flag

--- scripts/packaging/install-image-eums.sh
+++ scripts/packaging/install-image-eums.sh
@@ -40,10 +40,10 @@ echo "Done!"
 sleep 10s
 
 echo "Editing host name ..."
-sed -i -e "s/%EUMS_CONTAINER_HOST_NAME%/$EUMS_CONTAINER_HOST_NAME/g" eums.nginx.config
+sed -i -e "s/%EUMS_CONTAINER_HOST_NAME%/$EUMS_CONTAINER_HOST_NAME/g" scripts/eums.nginx.config
 
 echo "copying config file ..."
-sshpass -p 'password' scp -o StrictHostKeyChecking=no -p $DEPLOY_MACHINE_SSH_PORT eums.nginx.config root@0.0.0.0:/etc/nginx/sites-available/eums
+sshpass -p 'password' scp -o StrictHostKeyChecking=no -P $DEPLOY_MACHINE_SSH_PORT scripts/eums.nginx.config root@0.0.0.0:/etc/nginx/sites-available/eums
 
 echo "Creating ln and restarting nginx ..."
 sshpass -p 'password' ssh -o StrictHostKeyChecking=no -p $DEPLOY_MACHINE_SSH_PORT root@0.0.0.0 'ln -sf /etc/nginx/sites-available/eums /etc/nginx/sites-enabled/eums && service nginx restart'
