5ee80b868150ec5de0e82069ab01cb4b4c4290c3
jobasiimwe <jobasiimwe@gmail.com>
2015-06-18 18:05:01 +0300

    Job #96992360 added tests for programme and programme_name feilds

--- eums/test/api/test_purchase_order_endpoint.py
+++ eums/test/api/test_purchase_order_endpoint.py
@@ -1,3 +1,4 @@
+from eums.models import PurchaseOrderItem, PurchaseOrder, Item
 from eums.test.api.api_test_helpers import create_purchase_order, create_release_order
 from eums.test.api.authenticated_api_test_case import AuthenticatedAPITestCase
 from eums.test.config import BACKEND_URL
@@ -7,6 +8,11 @@ ENDPOINT_URL = BACKEND_URL + 'purchase-order/'
 
 
 class PurchaseOrderEndPointTest(AuthenticatedAPITestCase):
+
+    def tearDown(self):
+        PurchaseOrderItem.objects.all().delete()
+        Item.objects.all().delete()
+        PurchaseOrder.objects.all().delete()
 
     def test_should_get_purchase_orders_without_release_orders(self):
         created_purchase_order = create_purchase_order(self)
@@ -22,3 +28,11 @@ class PurchaseOrderEndPointTest(AuthenticatedAPITestCase):
         response = self.client.get(ENDPOINT_URL)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
+
+    def test_should_get_purchase_orders(self):
+        created_purchase_order = create_purchase_order(self)
+        get_response = self.client.get(ENDPOINT_URL)
+        self.assertEqual(get_response.status_code, 200)
+        self.assertDictContainsSubset(created_purchase_order, get_response.data[0])
+        self.assertEqual(get_response.data[0]['programme'], created_purchase_order['programme'])
+        self.assertEqual(get_response.data[0]['programme_name'], created_purchase_order['programme_name'])
