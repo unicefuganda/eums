7fb116e9eabfa86cf687ff25f4175a80f6dd6cba
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-18 08:59:09 +0300

    [#96260050] Cleanup and refactor patches to the change

--- eums/client/app/scripts/import-data/import-data.js
+++ eums/client/app/scripts/import-data/import-data.js
@@ -2,35 +2,28 @@
 
 angular.module('ImportData', ['eums.config', 'angularFileUpload'])
     .controller('ImportDataController', function ($scope, FileUploader, EumsConfig) {
-        $scope.errorMessage = '';
-        $scope.salesOrdersUploader = new FileUploader({
-            url: EumsConfig.BACKEND_URLS.IMPORT_SALES_ORDERS
-        });
 
-        $scope.salesOrdersUploader.onErrorItem = function(){
-            $scope.errorMessage = 'Failed to submit file to backend. Please ensure its not too big';
-        };
-        $scope.salesOrdersUploader.onSuccessItem = function(item, response){
-            $scope.errorMessage = response.error;
-        };
-        $scope.releaseOrdersUploader = new FileUploader({
-            url: EumsConfig.BACKEND_URLS.IMPORT_RELEASE_ORDERS
-        });
-        $scope.releaseOrdersUploader.onErrorItem = function(){
-            $scope.errorMessage = 'Failed to submit file to backend. Please ensure its not too big';
-        };
-        $scope.releaseOrdersUploader.onSuccessItem = function(response){
-            $scope.errorMessage = response.error;
-        };
+        //TODO: Refactor this functionality into a directive to avoid duplicate html in template
+        function getUploader(apiUrl, errProp) {
+            $scope[errProp] = undefined;
+            var theUploader = new FileUploader({
+                url: apiUrl
+            });
+            theUploader.onErrorItem = function () {
+                $scope[errProp] = "Failed to submit file to backend. Please ensure its not too big";
+            };
+            theUploader.onSuccessItem = function (item, response, status, headers) {
+                $scope[errProp] = response.error;
+            };
+            theUploader.onAfterAddingFile = function (item, response, status, headers) {
+                $scope[errProp] = undefined;
+            };
+            return theUploader;
+        }
 
-        $scope.purchaseOrdersUploader = new FileUploader({
-            url: EumsConfig.BACKEND_URLS.IMPORT_PURCHASE_ORDERS
-        });
-        $scope.purchaseOrdersUploader.onErrorItem = function(){
-            $scope.errorMessage = 'Failed to submit file to backend. Please ensure its not too big';
-        };
-        $scope.purchaseOrdersUploader.onSuccessItem = function(item, response){
-            $scope.errorMessage = response.error;
-        };
+        $scope.salesOrdersUploader = getUploader(EumsConfig.BACKEND_URLS.IMPORT_SALES_ORDERS, 'errorMessageSO');
+        $scope.releaseOrdersUploader = getUploader(EumsConfig.BACKEND_URLS.IMPORT_RELEASE_ORDERS, 'errorMessageRO');
+        $scope.purchaseOrdersUploader = getUploader(EumsConfig.BACKEND_URLS.IMPORT_PURCHASE_ORDERS, 'errorMessagePO');
+
 
     });
