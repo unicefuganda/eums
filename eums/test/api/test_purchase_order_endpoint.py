86c913cec21bf82166f400d29f30621d812530ce
wanderanimrod <wanderanimrod@gmail.com>
2015-06-21 16:57:52 +0300

    [nim] #96992360 Adds test for purchase order endpoint

--- eums/test/api/test_purchase_order_endpoint.py
+++ eums/test/api/test_purchase_order_endpoint.py
@@ -1,7 +1,9 @@
 from eums.test.api.api_test_helpers import create_purchase_order, create_release_order
 from eums.test.api.authenticated_api_test_case import AuthenticatedAPITestCase
 from eums.test.config import BACKEND_URL
-
+from eums.test.factories.programme_factory import ProgrammeFactory
+from eums.test.factories.purchase_order_factory import PurchaseOrderFactory
+from eums.test.factories.sales_order_factory import SalesOrderFactory
 
 ENDPOINT_URL = BACKEND_URL + 'purchase-order/'
 
@@ -22,3 +24,9 @@ class PurchaseOrderEndPointTest(AuthenticatedAPITestCase):
         response = self.client.get(ENDPOINT_URL)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
+
+    def test_fetched_purchase_orders_should_have_programme_name_and_programme_id(self):
+        programme = ProgrammeFactory()
+        purchase_order = PurchaseOrderFactory(sales_order=SalesOrderFactory(programme=programme))
+        response = self.client.get('%s%s/' % (ENDPOINT_URL, purchase_order.id))
+        self.assertDictContainsSubset({'programme': programme.id, 'programme_name': programme.name}, response.data)
