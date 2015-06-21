6fbdc4be7b27e4ccb668e812c091be583e9374c1
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-21 09:51:15 +0300

    [#94162176] Fixed falkey test failing when order of object members is changed

--- eums/test/api/test_distribution_report_endpoint.py
+++ eums/test/api/test_distribution_report_endpoint.py
@@ -17,5 +17,12 @@ class DistributionReportEndPointTest(AuthenticatedAPITestCase):
                               'total_received': 1, 'id': 1, 'total_not_received': 1, 'total_distributed': 1}]
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(expected_response, response.data)
+        self.assertEqual(len(response.data), 1)
+        print expected_response[0]
+        self.assertEqual(expected_response[0]['consignee'], response.data[0]['consignee'])
+        self.assertEqual(expected_response[0]['programme'], response.data[0]['programme'])
+        self.assertEqual(expected_response[0]['total_received'], response.data[0]['total_received'])
+        self.assertEqual(expected_response[0]['id'], response.data[0]['id'])
+        self.assertEqual(expected_response[0]['total_not_received'], response.data[0]['total_not_received'])
+        self.assertEqual(expected_response[0]['total_distributed'], response.data[0]['total_distributed'])
 
