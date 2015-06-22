0fdb6fa9b3370b24118d3f0b565d89550cf0f1f6
wanderanimrod <wanderanimrod@gmail.com>
2015-06-22 21:17:20 +0300

    [nim] #96992360 WIP. Fixes jshint error

--- eums/client/app/scripts/reported-by-ip/ip-purchase-orders-controller.js
+++ eums/client/app/scripts/reported-by-ip/ip-purchase-orders-controller.js
@@ -51,5 +51,5 @@ angular.module('NewIpReport', ['PurchaseOrder', 'User', 'DistributionPlanNode'])
                     $scope.planNode = node;
                 });
             });
-        }
+        };
     });
