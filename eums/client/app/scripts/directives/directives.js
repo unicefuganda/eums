e50de88fca85655374a6830d3ddb7cb5417bf521
jobasiimwe <jobasiimwe@gmail.com>
2015-06-22 11:42:18 +0300

    [job, nim] #96992360 Moves directives into Directives module. Can't modify NewDistributionPlan module

--- eums/client/app/scripts/directives/directives.js
+++ eums/client/app/scripts/directives/directives.js
@@ -11,5 +11,120 @@ angular.module('Directives', [])
             },
             templateUrl: '/static/app/views/distribution-planning/partials/view-sales-orders.html'
         };
-    }]);
+    }])
+    .directive('searchFromList', function ($timeout) {
+        return {
+            restrict: 'A',
+            scope: false,
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+                var list = JSON.parse(attrs.list);
+
+                element.select2({
+                    width: '100%',
+                    query: function (query) {
+                        var data = {results: []};
+                        var matches = list.filter(function (item) {
+                            return item.name.toLowerCase().indexOf(query.term.toLowerCase()) >= 0;
+                        });
+                        data.results = matches.map(function (match) {
+                            return {
+                                id: match.id,
+                                text: match.name
+                            };
+                        });
+                        query.callback(data);
+                    },
+                    initSelection: function (element, callback) {
+                        $timeout(function () {
+                            var matchingItem = list.filter(function (item) {
+                                return item.id === ngModel.$modelValue;
+                            })[0];
+                            if (matchingItem) {
+                                callback({id: matchingItem.id, text: matchingItem.name});
+                            }
+                        });
+                    }
+                });
+
+                element.change(function () {
+                    ngModel.$setViewValue(element.select2('data').id);
+                    scope.$apply();
+                });
+            }
+        };
+    })
+    .directive('searchContacts', function (ContactService, $timeout) {
+        function formatResponse(data) {
+            return data.map(function (contact) {
+                return {
+                    id: contact._id,
+                    text: contact.firstName + ' ' + contact.lastName
+                };
+            });
+        }
+
+        return {
+            restrict: 'A',
+            scope: true,
+            require: 'ngModel',
+            link: function (scope, element, _, ngModel) {
+
+                element.select2({
+                    minimumInputLength: 1,
+                    width: '150px',
+                    query: function (query) {
+                        var data = {results: []};
+                        ContactService.search(query.term).then(function (foundContacts) {
+                            data.results = formatResponse(foundContacts);
+                            query.callback(data);
+                        });
+                    },
+                    initSelection: function (element, callback) {
+                        $timeout(function () {
+                            var modelValue = ngModel.$modelValue;
+                            if (modelValue) {
+                                ContactService.get(modelValue).then(function (contact) {
+                                    if (contact._id) {
+                                        callback({
+                                            id: contact._id,
+                                            text: contact.firstName + ' ' + contact.lastName
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+
+                element.change(function () {
+                    ngModel.$setViewValue(element.select2('data').id);
+                    scope.$apply();
+                });
+            }
+        };
+    })
+    .directive('onlyDigits', function () {
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            link: function (scope, element, attr, ngModelCtrl) {
+                function inputValue(val) {
+                    if (val) {
+                        var digits = val.toString().replace(/[^0-9]/g, '');
+
+                        if (digits.toString() !== val.toString()) {
+                            ngModelCtrl.$setViewValue(digits);
+                            ngModelCtrl.$render();
+                        }
+                        return parseInt(digits, 10);
+                    }
+                    return undefined;
+                }
+
+                ngModelCtrl.$parsers.push(inputValue);
+            }
+        };
+    });
+
 
