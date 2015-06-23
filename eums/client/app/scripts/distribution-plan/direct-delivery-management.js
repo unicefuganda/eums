30d91c18584ebb936598b6ac257bc81c481f6824
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-23 16:18:50 +0300

    [Abe, Francis] #95500096 - Display purchase order numberon Direct Delivery page

--- eums/client/app/scripts/distribution-plan/direct-delivery-management.js
+++ eums/client/app/scripts/distribution-plan/direct-delivery-management.js
@@ -2,7 +2,12 @@
 
 
 angular.module('DirectDeliveryManagement', ['eums.config', 'DistributionPlanNode', 'ngTable', 'siTable', 'Programme', 'PurchaseOrder', 'User', 'Directives'])
-    .controller('DirectDeliveryManagementController', function ($scope, $location, DistributionPlanService, ProgrammeService, PurchaseOrderService, UserService, $sorter) {
+    .controller('DirectDeliveryManagementController', function ($scope, $location, DistributionPlanService, ProgrammeService, PurchaseOrderService, $routeParams) {
+
+        PurchaseOrderService.get($routeParams.purchaseOrderId, ['purchaseorderitem_set.item.unit']).then(function (purchaseOrder) {
+            $scope.selectedPurchaseOrder = purchaseOrder;
+            $scope.purchaseOrderItems = purchaseOrder.purchaseorderitemSet;
+        });
 
         $scope.showSingleIpMode = function() {
             $scope.inSingleIpMode = true;
