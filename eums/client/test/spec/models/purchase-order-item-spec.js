0868b75d34785e312415b70f4c0dba6b83369255
wanderanimrod <wanderanimrod@gmail.com>
2015-06-23 19:34:41 +0300

    [nim] #96992360 Refactor

--- eums/client/test/spec/models/purchase-order-item-spec.js
+++ eums/client/test/spec/models/purchase-order-item-spec.js
@@ -34,9 +34,9 @@ describe('Purchase Order Item Model', function () {
         expect(purchaseOrderItem.distributionplannodeSet).toEqual([1, 2]);
     });
 
-    it('should calculate quantity left from a list of delivery nodes', function () {
+    it('should calculate quantity left from a list of delivery nodes filtering out NaNs', function () {
         var purchaseOrderItem = new PurchaseOrderItemModel(itemJson);
-        var nodes = [{targetedQuantity: 10}, {targetedQuantity: 30}];
+        var nodes = [{targetedQuantity: 10}, {targetedQuantity: 30}, {targetedQuantity: NaN}];
         expect(purchaseOrderItem.quantityLeft(nodes)).toBe(60);
     });
 });
