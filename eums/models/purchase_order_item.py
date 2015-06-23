fc93c35006c91c3aed65c1dbbbb8f26083fc3568
wanderanimrod <wanderanimrod@gmail.com>
2015-06-23 12:20:35 +0300

    [nim] #X Fixes equals method on Purchase order item to reject non-purchase order item objects so the DJRF works

--- eums/models/purchase_order_item.py
+++ eums/models/purchase_order_item.py
@@ -16,6 +16,8 @@ class PurchaseOrderItem(OrderItem):
         return '%s %s %s.' % (self.purchase_order.order_number, str(self.item_number), self.sales_order_item.description)
 
     def __eq__(self, other):
+        if not isinstance(other, PurchaseOrderItem):
+            return False
         return self.purchase_order.order_number == other.purchase_order.order_number \
                and self.item_number == other.item_number \
                and self.sales_order_item.id == other.sales_order_item.id \
