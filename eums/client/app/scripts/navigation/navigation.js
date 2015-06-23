20de87ddb05b9945c2ec2b100978eba1e9ee1e07
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-06-23 16:18:50 +0300

    [Abe, Francis] #95500096 - Display purchase order numberon Direct Delivery page

--- eums/client/app/scripts/navigation/navigation.js
+++ eums/client/app/scripts/navigation/navigation.js
@@ -4,6 +4,13 @@ angular.module('NavigationTabs', [])
     .controller('NavigationTabsController', function($scope, $location) {
         $scope.isActive = function(viewLocation) {
             var allLocations = viewLocation.split(',');
-            return allLocations.indexOf($location.path()) !== -1;
+            for(var enabledLocation in allLocations){
+                if(allLocations[enabledLocation] === "/"){
+                    return $location.path() === "/";
+                }
+                else if ($location.path().indexOf(allLocations[enabledLocation]) >= 0)
+                    return true;
+            }
+            return false;
         };
     });
