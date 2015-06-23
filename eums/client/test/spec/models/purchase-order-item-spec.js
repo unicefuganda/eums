3600789fa5996d4df80e810012694fcaf66c4068
wanderanimrod <wanderanimrod@gmail.com>
2015-06-23 19:00:10 +0300

    [nim] #96992360 adds test for purchase order item model

---
+++ eums/client/test/spec/models/purchase-order-item-spec.js
@@ -0,0 +1,42 @@
+describe('Purchase Order Item Model', function () {
+    var PurchaseOrderItemModel;
+    var itemJson = {
+        id: 1, purchaseOrder: 10, quantity: 100, value: 11, salesOrderItem: 12,
+        item: 89, distributionplannodeSet: [1, 2]
+    };
+
+    beforeEach(function () {
+        module('PurchaseOrder');
+        inject(function (PurchaseOrderItem) {
+            PurchaseOrderItemModel = PurchaseOrderItem;
+        });
+    });
+
+    it('should construct empty object if nothing is passed to constructor', function () {
+        var purchaseOrderItem = new PurchaseOrderItemModel();
+        expect(purchaseOrderItem.id).toBeUndefined();
+        expect(purchaseOrderItem.purchaseOrder).toBeUndefined();
+        expect(purchaseOrderItem.quantity).toEqual(0);
+        expect(purchaseOrderItem.value).toEqual(0);
+        expect(purchaseOrderItem.salesOrderItem).toBeUndefined();
+        expect(purchaseOrderItem.item).toBeUndefined();
+        expect(purchaseOrderItem.distributionplannodeSet).toEqual([]);
+    });
+
+    it('should construct item from json', function () {
+        var purchaseOrderItem = new PurchaseOrderItemModel(itemJson);
+        expect(purchaseOrderItem.id).toBe(1);
+        expect(purchaseOrderItem.purchaseOrder).toBe(10);
+        expect(purchaseOrderItem.quantity).toEqual(100);
+        expect(purchaseOrderItem.value).toEqual(11);
+        expect(purchaseOrderItem.salesOrderItem).toEqual(12);
+        expect(purchaseOrderItem.item).toEqual(89);
+        expect(purchaseOrderItem.distributionplannodeSet).toEqual([1, 2]);
+    });
+
+    it('should calculate quantity left from a list of delivery nodes', function () {
+        var purchaseOrderItem = new PurchaseOrderItemModel(itemJson);
+        var nodes = [{targetedQuantity: 10}, {targetedQuantity: 30}];
+        expect(purchaseOrderItem.quantityLeft(nodes)).toBe(60);
+    });
+});
