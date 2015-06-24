4b0ff1c7a84fad15cf87736b70ba20a0bded61b7
jobasiimwe <jobasiimwe@gmail.com>
2015-06-24 17:09:37 +0300

    [job, jaf] #95500128 WIP introducing separate controller and view for warehouse delivery

--- eums/client/app/scripts/app.js
+++ eums/client/app/scripts/app.js
@@ -3,7 +3,7 @@
 angular.module('eums', ['ngRoute', 'Home', 'DistributionPlan', 'DirectDelivery', 'ReportedByIP', 'WarehouseDelivery', 'NewDistributionPlan',
     'NavigationTabs', 'eums.service-factory', 'gs.to-snake-case', 'gs.to-camel-case', 'ngTable', 'siTable', 'ui.bootstrap', 'eums.map', 'eums.ip',
     'ManualReporting', 'ManualReportingDetails', 'DatePicker', 'StockReport', 'ngToast', 'cgBusy', 'Responses', 'User', 'Contact',
-    'ImportData', 'EndUserResponses', 'Directives', 'NewIpReport'])
+    'ImportData', 'EndUserResponses', 'Directives', 'NewIpReport', 'WarehouseDeliveryPlan'])
     .config(function ($routeProvider, $httpProvider) {
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
@@ -109,8 +109,8 @@ angular.module('eums', ['ngRoute', 'Home', 'DistributionPlan', 'DirectDelivery',
                 }
             })
             .when('/warehouse-delivery/new/:releaseOrderId', {
-                templateUrl: '/static/app/views/distribution-planning/new.html',
-                controller: 'NewDistributionPlanController',
+                templateUrl: '/static/app/views/distribution-planning/warehouse-delivery-plan.html',
+                controller: 'WarehouseDeliveryPlanController',
                 resolve: {
                     permission: function (UserService) {
                         return UserService.checkUserPermission('auth.can_view_distribution_plans');
