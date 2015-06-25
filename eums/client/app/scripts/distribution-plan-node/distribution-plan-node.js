70b7eed978584f7434f42ff699c16b6850e3d9cc
jobasiimwe <jobasiimwe@gmail.com>
2015-06-25 08:55:59 +0300

    Job, #95500128 adds loading empty distribution plan and changes warehouse delivery page header

--- eums/client/app/scripts/distribution-plan-node/distribution-plan-node.js
+++ eums/client/app/scripts/distribution-plan-node/distribution-plan-node.js
@@ -2,13 +2,6 @@
 
 angular.module('DistributionPlanNode', ['eums.config', 'Contact', 'Consignee', 'eums.service-factory'])
     .factory('DistributionPlanNodeService', function ($http, $q, EumsConfig, ContactService, ConsigneeService, ServiceFactory) {
-        var getPlanNodeDetails = function (planNodeId) {
-            var fieldsToBuild = ['consignee', 'contact_person_id', 'children'];
-            return this.get(planNodeId, fieldsToBuild).then(function (planNode) {
-                planNode.contactPerson = planNode.contactPersonId;
-                return planNode;
-            });
-        };
         return ServiceFactory.create({
             uri: EumsConfig.BACKEND_URLS.DISTRIBUTION_PLAN_NODE,
             propertyServiceMap: {consignee: ConsigneeService, contact_person_id: ContactService, children: 'self'},
@@ -18,35 +11,11 @@ angular.module('DistributionPlanNode', ['eums.config', 'Contact', 'Consignee', '
                         return response.data;
                     });
                 },
-                getPlanNodeDetails: getPlanNodeDetails,
-                getTopLevelDistributionPlanNodes: function (orderItem) {
-                    var allDistributionPlanNodes = orderItem.distributionplannodeSet;
-
-                    var planNodePromises = [], planNodes = [];
-
-                    if(allDistributionPlanNodes === undefined){
-                        return [];
-                    }
-
-                    allDistributionPlanNodes.forEach(function (node) {
-                        var planNodePromise = getPlanNodeDetails(node.id).then(function (planNodeResponse) {
-                            return planNodeResponse;
-                        });
-                        planNodePromises.push(planNodePromise);
-                    });
-
-                    planNodePromises.forEach(function (promise) {
-                        promise.then(function (plaNode) {
-                            planNodes.push(plaNode);
-                        });
-                    });
-
-                    function hasNoParent(planNode) {
-                        return !planNode.parent;
-                    }
-
-                    return $q.all(planNodePromises).then(function () {
-                        return planNodes.filter(hasNoParent);
+                getPlanNodeDetails: function (planNodeId) {
+                    var fieldsToBuild = ['consignee', 'contact_person_id', 'children'];
+                    return this.get(planNodeId, fieldsToBuild).then(function (planNode) {
+                        planNode.contactPerson = planNode.contactPersonId;
+                        return planNode;
                     });
                 }
             }
--- eums/client/app/scripts/purchase-order-item/purchase-order-item.js
+++ eums/client/app/scripts/purchase-order-item/purchase-order-item.js
@@ -8,35 +8,6 @@ angular.module('PurchaseOrderItem', ['eums.config', 'eums.service-factory', 'Dis
                 distributionplannode_set: DistributionPlanNodeService,
                 item: ItemService
             },
-            methods: {
-                getTopLevelDistributionPlanNodes: function (purchaseOrderItem) {
-                    var allDistributionPlanNodes = purchaseOrderItem.distributionplannodeSet;
-
-                    var planNodePromises = [],
-                        planNodes = [];
-
-                    allDistributionPlanNodes.forEach(function (node) {
-                        var planNodePromise = DistributionPlanNodeService.getPlanNodeDetails(node.id).then(function (planNodeResponse) {
-                            return planNodeResponse;
-                        });
-                        planNodePromises.push(planNodePromise);
-                    });
-
-                    planNodePromises.forEach(function (promise) {
-                        promise.then(function (plaNode) {
-                            planNodes.push(plaNode);
-                        });
-                    });
-
-                    function hasNoParent(planNode) {
-                        return !planNode.parent;
-                    }
-
-                    return $q.all(planNodePromises).then(function () {
-                        return planNodes.filter(hasNoParent);
-                    });
-
-                }
-            }
+            methods: {}
         });
     });
