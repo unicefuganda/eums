56fbc90074346ae50860eb6d590b8622185c92c8
jobasiimwe <jobasiimwe@gmail.com>
2015-06-25 08:57:37 +0300

    Job , #95500128 removes unnecessary method from service

--- eums/client/app/scripts/release-order-item/release-order-item.js
+++ eums/client/app/scripts/release-order-item/release-order-item.js
@@ -8,35 +8,6 @@ angular.module('ReleaseOrderItem', ['eums.config', 'eums.service-factory', 'Item
                 item: ItemService,
                 distributionplannode_set: DistributionPlanNodeService
             },
-            methods: {
-                getTopLevelDistributionPlanNodes: function (releaseOrderItem) {
-                    var allDistributionPlanNodes = releaseOrderItem.distributionplannodeSet;
-
-                    var planNodePromises = [],
-                        planNodes = [];
-
-                    allDistributionPlanNodes.forEach(function (node) {
-                        var planNodePromise = DistributionPlanNodeService.getPlanNodeDetails(node.id).then(function (planNodeResponse) {
-                            return planNodeResponse;
-                        });
-                        planNodePromises.push(planNodePromise);
-                    });
-
-                    planNodePromises.forEach(function (promise) {
-                        promise.then(function (plaNode) {
-                            planNodes.push(plaNode);
-                        });
-                    });
-
-                    function hasNoParent(planNode) {
-                        return !planNode.parent;
-                    }
-
-                    return $q.all(planNodePromises).then(function () {
-                        return planNodes.filter(hasNoParent);
-                    });
-
-                }
-            }
+            methods: {}
         });
     });
