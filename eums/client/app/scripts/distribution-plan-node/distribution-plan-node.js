f5381d5894571ee113b9c9a11e206fbeff6b9dcc
jobasiimwe <jobasiimwe@gmail.com>
2015-06-25 08:55:59 +0300

    Job, #95500128 adds loading empty distribution plan and changes warehouse delivery page header

--- eums/client/app/scripts/distribution-plan-node/distribution-plan-node.js
+++ eums/client/app/scripts/distribution-plan-node/distribution-plan-node.js
@@ -82,35 +82,11 @@ angular.module('DistributionPlanNode', ['eums.config', 'Contact', 'Consignee', '
                         return response.data;
                     });
                 },
-                getPlanNodeDetails: getPlanNodeDetails,
-                getTopLevelDistributionPlanNodes: function (orderItem) {
-                    var allDistributionPlanNodes = orderItem.distributionplannodeSet;
-
-                    var planNodePromises = [], planNodes = [];
-
-                    if(allDistributionPlanNodes === undefined){
-                        return [];
-                    }
-
-                    allDistributionPlanNodes.forEach(function (node) {
-                        var planNodePromise = getPlanNodeDetails(node.id).then(function (planNodeResponse) {
-                            return planNodeResponse;
-                        });
-                        planNodePromises.push(planNodePromise);
-                    });
-
-                    planNodePromises.forEach(function (promise) {
-                        promise.then(function (plaNode) {
-                            planNodes.push(plaNode);
-                        });
-                    });
-
-                    function hasNoParent(planNode) {
-                        return !planNode.parent;
-                    }
-
-                    return $q.all(planNodePromises).then(function () {
-                        return planNodes.filter(hasNoParent);
+                getPlanNodeDetails: function (planNodeId) {
+                    var fieldsToBuild = ['consignee', 'contact_person_id', 'children'];
+                    return this.get(planNodeId, fieldsToBuild).then(function (planNode) {
+                        planNode.contactPerson = planNode.contactPersonId;
+                        return planNode;
                     });
                 },
                 getNodesByDelivery: function (deliveryId) {
