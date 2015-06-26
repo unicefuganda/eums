191f9dec453198e2f1d749af738dae4da278010c
Jafari <jafaris@thoughtworks.com>
2015-06-26 17:40:49 +0300

    [Job Jaf] #95500128 adds saving and updating delivery and delivery nodes

--- eums/client/app/scripts/release-order/release-order.js
+++ eums/client/app/scripts/release-order/release-order.js
@@ -1,13 +1,16 @@
 'use strict';
 
-angular.module('ReleaseOrder', ['eums.config', 'eums.service-factory', 'ReleaseOrderItem', 'SalesOrder', 'Consignee'])
-    .factory('ReleaseOrderService', function ($http, EumsConfig, ServiceFactory, ReleaseOrderItemService, SalesOrderService, ConsigneeService) {
+angular.module('ReleaseOrder', ['eums.config', 'eums.service-factory', 'ReleaseOrderItem', 'SalesOrder', 'Consignee',
+    'DistributionPlan'])
+    .factory('ReleaseOrderService', function ($http, EumsConfig, ServiceFactory, ReleaseOrderItemService,
+                                              SalesOrderService, ConsigneeService, DistributionPlanService) {
         return ServiceFactory.create({
             uri: EumsConfig.BACKEND_URLS.RELEASE_ORDER,
             propertyServiceMap: {
                 consignee: ConsigneeService,
                 sales_order: SalesOrderService,
-                items: ReleaseOrderItemService
+                items: ReleaseOrderItemService,
+                delivery: DistributionPlanService
             },
             methods: {}
         });
