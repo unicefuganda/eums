e93ac4830c66fe8046d54454ab0db1348a540959
wanderanimrod <wanderanimrod@gmail.com>
2015-06-26 18:32:56 +0300

    [nim] #96992360 Reported by IP only fetches purchase order items that were sent to the logged in consignee

--- eums/api/purchase_order_item/purchase_order_item_endpoint.py
+++ eums/api/purchase_order_item/purchase_order_item_endpoint.py
@@ -1,8 +1,9 @@
+from rest_framework import filters
 from rest_framework import serializers
 from rest_framework.routers import DefaultRouter
 from rest_framework.viewsets import ModelViewSet
 
-from eums.models import PurchaseOrderItem
+from eums.models import PurchaseOrderItem, DistributionPlanNode
 
 
 class PurchaseOrderItemSerialiser(serializers.ModelSerializer):
@@ -15,7 +16,15 @@ class PurchaseOrderItemSerialiser(serializers.ModelSerializer):
 class PurchaseOrderItemViewSet(ModelViewSet):
     queryset = PurchaseOrderItem.objects.all()
     serializer_class = PurchaseOrderItemSerialiser
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('purchase_order',)
 
+    def get_queryset(self):
+        consignee = self.request.GET.get('consignee', None)
+        if consignee:
+            po_item_ids = DistributionPlanNode.objects.filter(consignee_id=int(consignee)).values_list('item_id')
+            return self.queryset.filter(id__in=po_item_ids)
+        return self.queryset
 
 purchaseOrderItemRouter = DefaultRouter()
 purchaseOrderItemRouter.register(r'purchase-order-item', PurchaseOrderItemViewSet)
