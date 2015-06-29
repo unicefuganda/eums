d6509ed531d4a228c8612c9635c6438d852df5ff
Tom Oketch <tc.oketch@gmail.com>
2015-06-29 22:27:26 +0300

    [@tomclement #96259738] Add e2e test for importing consignees from spreadsheet

---
+++ eums/client/test/functional/import-data-spec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var feedbackReportPage = require('./pages/feedback-report-page.js');
+var loginPage = require('./pages/login-page.js');
+var importDataPage = require('./pages/import-data-page.js');
+
+describe('Vision Data Imports', function () {
+
+    it('should show consignees imported from vision on the feedback report page', function () {
+        loginPage.visit();
+        loginPage.loginAs('admin', 'admin');
+
+        importDataPage.visit();
+
+        var path = require('path');
+        var fileToUpload = './files/consignees.xlsx',
+            absolutePath = path.resolve(__dirname, fileToUpload);
+
+        element(by.css('#consigneesDiv input')).sendKeys(absolutePath);
+        element(by.css('#consigneesDiv button')).click();
+
+        var EC = protractor.ExpectedConditions;
+        var successTick = element(by.css('#consigneesDiv .glyphicon.glyphicon-ok'));
+
+        var uploadIsComplete = (EC.visibilityOf(successTick));
+        browser.wait(uploadIsComplete, 15000, "Timeout exceeded while importing data");
+
+        feedbackReportPage.visit();
+        feedbackReportPage.consigneeSelect.click();
+
+        expect(feedbackReportPage.consigneeResults).toContain('name of test consignee');
+    });
+});
