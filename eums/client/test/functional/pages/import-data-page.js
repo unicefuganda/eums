d1ef6ddde5571e58d3fbf73668333f22f80a3fec
Tom Oketch <tc.oketch@gmail.com>
2015-07-01 15:26:38 +0300

    [@tomclement #96259738] Refactor e2e tests for importing programmes & consignees

--- eums/client/test/functional/pages/import-data-page.js
+++ eums/client/test/functional/pages/import-data-page.js
@@ -4,6 +4,31 @@ var ImportDataPage = function () {
     this.visit = function () {
         browser.get(this.url);
     };
+
+    this.uploadConsignees = function(fileToUpload) {
+        uploadUsingThisDiv('consigneesDiv', fileToUpload);
+    };
+
+    this.uploadProgrammes = function(fileToUpload) {
+        uploadUsingThisDiv('programmesDiv', fileToUpload);
+    };
 };
 
 module.exports = new ImportDataPage;
+
+
+function uploadUsingThisDiv (divID, fileToUpload) {
+    itemDiv = divID;
+
+    var path = require('path');
+    absolutePath = path.resolve(__dirname, fileToUpload);
+
+    element(by.css('#' + itemDiv + ' input')).sendKeys(absolutePath);
+    element(by.css('#' + itemDiv + ' button')).click();
+
+    var EC = protractor.ExpectedConditions;
+    var successTick = element(by.css('#' + itemDiv + ' .glyphicon.glyphicon-ok'));
+
+    var uploadIsComplete = (EC.visibilityOf(successTick));
+    browser.wait(uploadIsComplete, 15000, "Timeout exceeded while importing data");
+}
