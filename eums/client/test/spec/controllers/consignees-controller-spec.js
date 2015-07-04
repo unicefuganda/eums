2e42afd5305946ca633cc4b2b2654e3f49f47753
wanderanimrod <wanderanimrod@gmail.com>
2015-07-04 22:31:31 +0300

    [nim] #X Edit consignee component WIP

--- eums/client/test/spec/controllers/consignees-controller-spec.js
+++ eums/client/test/spec/controllers/consignees-controller-spec.js
@@ -23,4 +23,11 @@ describe('Consignees Controller', function () {
         expect(mockConsigneeService.all).toHaveBeenCalled();
         expect(scope.consignees).toEqual(consignees);
     });
+
+    it('should fire off an event to edit consignee when edit button is clicked', function() {
+        var consignee = {};
+        var broadcast = spyOn(scope, '$broadcast');
+        scope.edit(consignee);
+        expect(broadcast).toHaveBeenCalledWith('edit-consignee', consignee);
+    });
 });
