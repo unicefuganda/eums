8b2ea3177031436bc97a8f00d3e9abe81bc455d8
Tom Oketch <tc.oketch@gmail.com>
2015-07-21 10:24:34 +0300

    [@tomclement #96259916] Add e2e test to verify that sub-consignees can be added directly while reporting

--- eums/client/test/functional/direct-delivery-spec.js
+++ eums/client/test/functional/direct-delivery-spec.js
@@ -3,26 +3,23 @@
 var loginPage = require('./pages/login-page.js');
 var directDeliveryPage = require('./pages/direct-delivery-page.js');
 var ipDirectDeliveryPage = require('./pages/ip-direct-delivery-page.js');
-var header = require('./pages/header.js');
+var consigneesPage = require('./pages/consignees-page.js');
 
 describe('Direct Delivery', function () {
+
+    var PURCHASE_ORDER_NUMBER = '81026395';
 
     it('Admin should be able to create direct deliveries to multiple IPs', function () {
 
         loginPage.visit();
-
         loginPage.loginAs('admin', 'admin');
-
         directDeliveryPage.visit();
-
-        var PURCHASE_ORDER_NUMBER = '81026395';
 
         directDeliveryPage.searchForThisPurchaseOrder(PURCHASE_ORDER_NUMBER);
         expect(directDeliveryPage.firstPurchaseOrderAttributes).toContain('text-danger');
 
         directDeliveryPage.selectPurchaseOrderByNumber(PURCHASE_ORDER_NUMBER);
         directDeliveryPage.selectMultipleIP();
-
         directDeliveryPage.selectItem('How Business Affects Us');
 
         directDeliveryPage.addConsignee();
@@ -32,19 +29,35 @@ describe('Direct Delivery', function () {
         directDeliveryPage.setContact('John');
         directDeliveryPage. setDistrict('Wakiso');
         directDeliveryPage.saveDelivery();
-
         directDeliveryPage.confirmDelivery();
 
         directDeliveryPage.visit();
         directDeliveryPage.searchForThisPurchaseOrder(PURCHASE_ORDER_NUMBER);
         expect(directDeliveryPage.firstPurchaseOrderAttributes).toContain('text-warning');
+    });
 
-        header.logout();
+    it('IP should be able to see direct delivery created by admin and add consignees while reporting', function () {
         loginPage.visit();
         loginPage.loginAs('wakiso', 'wakiso');
 
         ipDirectDeliveryPage.visit();
         ipDirectDeliveryPage.searchForThisPurchaseOrder(PURCHASE_ORDER_NUMBER);
         expect(ipDirectDeliveryPage.purchaseOrderCount).toEqual(1);
+
+        ipDirectDeliveryPage.selectPurchaseOrderByNumber(PURCHASE_ORDER_NUMBER);
+        ipDirectDeliveryPage.selectItem('How Business Affects Us');
+        ipDirectDeliveryPage.addNewConsignee();
+
+        ipDirectDeliveryPage.setConsigneeName('Fresh Consignee');
+        ipDirectDeliveryPage.setConsigneeLocation('Fresh Location');
+        ipDirectDeliveryPage.setConsigneeRemarks('Fresh Remarks');
+        ipDirectDeliveryPage.saveConsignee();
+
+        consigneesPage.visit();
+        consigneesPage.searchFor('Fresh Consignee');
+        expect(consigneesPage.consigneeNames).toContain('Fresh Consignee');
+        expect(consigneesPage.consigneeLocations).toContain('Fresh Location');
+        expect(consigneesPage.consigneeIDs).toContain('');
+        expect(consigneesPage.consigneeRemarks).toContain('Fresh Remarks');
     });
 });
--- eums/client/test/functional/pages/ip-direct-delivery-page.js
+++ eums/client/test/functional/pages/ip-direct-delivery-page.js
@@ -13,8 +13,50 @@ IpDirectDeliveryPage.prototype = Object.create({}, {
 
     searchForThisPurchaseOrder: { value: function (searchTerm) {
         this.searchBar.clear().sendKeys(searchTerm);
-    }}
+    }},
+    selectPurchaseOrderByNumber: { value: function (text) { element(by.linkText(text)).click(); }},
 
+    selectItem: { value: function (item) {
+        waitForLoadingToComplete();
+        element(by.css('#select-purchase-order')).click();
+
+        waitForLoadingToComplete();
+        element(by.css("#select-purchase-order option[label='" + item + "']")).click();
+    }},
+
+    addNewConsignee: { value: function () {
+        waitForLoadingToComplete();
+        element(by.id('addNewConsignee')).click();
+
+        var EC = protractor.ExpectedConditions;
+        var addConsigneeModal = element(by.id('add-consignee-modal'));
+        var modalHasLoaded = EC.visibilityOf(addConsigneeModal);
+        browser.wait(modalHasLoaded, 5000, "Timeout exceeded while loading modal");
+    }},
+    setConsigneeName: { value: function (input) {
+        element(by.model('consignee.name')).clear().sendKeys(input);
+    }},
+    setConsigneeLocation: { value: function (input) {
+        element(by.model('consignee.location')).clear().sendKeys(input);
+    }},
+    setConsigneeRemarks: { value: function (input) {
+        element(by.model('consignee.remarks')).clear().sendKeys(input);
+    }},
+    saveConsignee: { value: function () {
+        var EC = protractor.ExpectedConditions;
+        var addConsigneeModal = element(by.id('add-consignee-modal'));
+        var modalhasFaded = EC.invisibilityOf(addConsigneeModal);
+
+        element(by.id('saveConsigneeBtn')).click();
+        browser.wait(modalhasFaded, 5000, "Timeout exceeded while waiting for modal to fade");
+    }}
 });
 
 module.exports = new IpDirectDeliveryPage;
+
+function waitForLoadingToComplete () {
+    var EC = protractor.ExpectedConditions;
+    var fadingModal = element(by.css('.modal-backdrop.fade'));
+    var itemsHaveLoaded = EC.stalenessOf(fadingModal);
+    browser.wait(itemsHaveLoaded, 5000, "Timeout exceeded while loading item");
+}
