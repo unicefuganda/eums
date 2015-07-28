4e4f75c167117aba437ec0193f58f19989efd328
wanderanimrod <wanderanimrod@gmail.com>
2015-07-28 17:05:21 +0300

    [nim] [#X] Adds unit value to release order item so that fetching distribution plans succeeds

--- eums/models/release_order_item.py
+++ eums/models/release_order_item.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import models
 from eums.models import PurchaseOrderItem
 from eums.models.order_item import OrderItem
@@ -7,6 +8,9 @@ class ReleaseOrderItem(OrderItem):
     release_order = models.ForeignKey('ReleaseOrder', related_name='items')
     purchase_order_item = models.ForeignKey(PurchaseOrderItem)
     value = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def unit_value(self):
+        return self.value / Decimal(self.quantity)
 
     class Meta:
         app_label = 'eums'
