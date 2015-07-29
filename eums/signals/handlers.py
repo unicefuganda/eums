843de98f2b3c04f0c62bc690675e7d0854e57883
francikas <francikas@gmail.com>
2015-07-29 15:11:24 +0300

    Revert "[#96260566] francis/faris - Added post_save handler for delivery; Updated node post_save handler not to schedule run for top level nodes"
    This reverts commit c6e54fad0d4b7c4215b380e40afc7d64da9b47a8.

--- eums/signals/handlers.py
+++ eums/signals/handlers.py
@@ -1,18 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from eums.models import DistributionPlanNode, DistributionPlan
+from eums.models import DistributionPlanNode
 from eums.services.flow_scheduler import schedule_run_for
 
 
 @receiver(post_save, sender=DistributionPlanNode)
-def on_post_save_node(sender, **kwargs):
+def on_post_save_line_item(sender, **kwargs):
     node = kwargs['instance']
-    if node.track and node.parent and node.latest_run() is None:
+    if node.track and node.latest_run() is None:
         schedule_run_for(node)
 
-@receiver(post_save, sender=DistributionPlan)
-def on_post_save_delivery(sender, **kwargs):
-    delivery = kwargs['instance']
-    if delivery.track and delivery.latest_run() is None:
-        schedule_run_for(delivery)
