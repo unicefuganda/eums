6b7b149e29f1d5e810b5ca20c60c40cd469cce0d
francikas <francikas@gmail.com>
2015-07-29 14:40:46 +0300

    [#96260566] francis/faris - Adds test for node  post_save handler

--- eums/test/models/test_distribution_plan_node.py
+++ eums/test/models/test_distribution_plan_node.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
-
+from mock import MagicMock
 from eums.models import DistributionPlanNode, SalesOrderItem, PurchaseOrderItem, ReleaseOrderItem, \
-    ReleaseOrder, \
-    PurchaseOrder, SalesOrder
+    ReleaseOrder, PurchaseOrder, SalesOrder
+from eums.services import flow_scheduler
+from eums.test.factories.distribution_plan_factory import DistributionPlanFactory
+
 from eums.test.factories.distribution_plan_node_factory import DistributionPlanNodeFactory as NodeFactory, \
     DistributionPlanNodeFactory
 from eums.test.factories.purchase_order_item_factory import PurchaseOrderItemFactory
@@ -44,3 +46,11 @@ class DistributionPlanNodeTest(TestCase):
         self.assertEqual(DistributionPlanNode.objects.get(item=sales_order_item), node_with_so_item)
         self.assertEqual(DistributionPlanNode.objects.get(item=purchase_order_item), node_with_po_item)
         self.assertEqual(DistributionPlanNode.objects.get(item=release_order_item), node_with_ro_item)
+
+    def test_should_schedule_run_when_node_saved_and_is_tracked_and_has_parent(self):
+        flow_scheduler.schedule_run_for = MagicMock(return_value=None)
+        from eums.signals import handlers
+
+        node = NodeFactory(track=True, parent=NodeFactory())
+
+        flow_scheduler.schedule_run_for.assert_called_with(node)
