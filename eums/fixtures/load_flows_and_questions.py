e9ad9c124480a6705f870a8477b3a41cd90ae47c
francikas <francikas@gmail.com>
2015-07-30 16:30:48 +0300

    [#96260566] Updated database deployment script to run seed script correctly

--- eums/fixtures/load_flows_and_questions.py
+++ eums/fixtures/load_flows_and_questions.py
@@ -1,5 +1,5 @@
 from eums.fixtures.flows import seed_flows
-from eums.fixtures.questions import seed_questions
-
 seed_flows()
+
+from eums.fixtures.questions import seed_questions
 seed_questions()
