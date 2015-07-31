61126e0fb7e9284edb58c320b558f36a70fd1d23
Chris George <cgeorge@thoughtworks.com>
2015-07-31 17:48:49 +0300

    [#96260288] chris - moving permissions into new folder

--- eums/api/consignee/__init__.py
+++
@@ -1,3 +0,0 @@
-from vision_imported_permission import VisionImportedPermission
-from delivery_attached_permission import DeliveryAttachedPermission
-from created_by_permission import CreatedByPermission
---
+++ eums/permissions/__init__.py
@@ -0,0 +1,3 @@
+from vision_imported_permission import VisionImportedPermission
+from delivery_attached_permission import DeliveryAttachedPermission
+from created_by_permission import CreatedByPermission
