9eb5afd51ac2fc787b9dfbc09c4a6caeb0dd1210
Jafari <jafaris@thoughtworks.com>
2015-08-03 17:03:53 +0300

    [#96260288] jafari/faris: tests that implementing partner viewers and editors can not edit and add users

--- eums/test/api/authorization/test_user_endpoint.py
+++ eums/test/api/authorization/test_user_endpoint.py
@@ -125,6 +125,76 @@ class UserEndpointTest(APITestCase):
 
         self.assertEqual(response.status_code, 403)
 
+    # Implementing partner editor
+
+    def test_should_not_allow_implementing_partner_editors_to_create_users(self):
+        self._login_as('Implementing Partner_editor')
+        response = self.client.post(ENDPOINT_URL,
+                                    {'username': 'name', 'password': 'password',
+                                     'email': 'email@email.email',
+                                     'first_name': 'f name', 'last_name': 'l name'
+                                     }
+                                    )
+
+        self.assertEqual(response.status_code, 403)
+
+    def _test_should_not_allow_implementing_partner_editors_to_view_users(self):
+        user = UserFactory()
+        self._login_as('Implementing Partner_editor')
+
+        response = self.client.get(ENDPOINT_URL + str(user.id) + '/')
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_should_not_allow_implementing_partner_editors_to_edit_users(self):
+        user = UserFactory()
+        self._login_as('Implementing Partner_editor')
+
+        response = self.client.put(ENDPOINT_URL + str(user.id) + '/', {
+            'id': str(user.id),
+            'username': 'newName',
+            'email': 'email@email.email',
+            'first_name': 'f name',
+            'last_name': 'l name'
+        })
+
+        self.assertEqual(response.status_code, 403)
+
+    # Implementing partner viewer
+
+    def test_should_not_allow_implementing_partner_viewers_to_create_users(self):
+        self._login_as('Implementing Partner_viewer')
+        response = self.client.post(ENDPOINT_URL,
+                                    {'username': 'name', 'password': 'password',
+                                     'email': 'email@email.email',
+                                     'first_name': 'f name', 'last_name': 'l name'
+                                     }
+                                    )
+
+        self.assertEqual(response.status_code, 403)
+
+    def _test_should_not_allow_implementing_partner_viewers_to_view_users(self):
+        user = UserFactory()
+        self._login_as('Implementing Partner_viewer')
+
+        response = self.client.get(ENDPOINT_URL + str(user.id) + '/')
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_should_not_allow_implementing_partner_viewers_to_edit_users(self):
+        user = UserFactory()
+        self._login_as('Implementing Partner_viewer')
+
+        response = self.client.put(ENDPOINT_URL + str(user.id) + '/', {
+            'id': str(user.id),
+            'username': 'newName',
+            'email': 'email@email.email',
+            'first_name': 'f name',
+            'last_name': 'l name'
+        })
+
+        self.assertEqual(response.status_code, 403)
+
     # Helper methods
 
     def _login_as(self, group_name):
