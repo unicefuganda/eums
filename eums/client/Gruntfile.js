bba292f9b36b89b28a8f3cb64256ecf6c2bb2cd6
wanderanimrod <wanderanimrod@gmail.com>
2015-08-04 19:23:35 +0300

    [nim, tom, job] [#99881574] WIP. Using new python fixture file to load FT fixtures

--- eums/client/Gruntfile.js
+++ eums/client/Gruntfile.js
@@ -204,7 +204,7 @@ module.exports = function (grunt) {
                 }
             },
             mapData: {
-                command: 'python manage.py loaddata eums/client/test/functional/fixtures/mapdata.json --settings=eums.test_settings',
+                command: 'python manage.py shell < eums/client/test/functional/fixtures/mapdata_code.py --settings=eums.test_settings',
                 options: {
                     stderr: false,
                     execOptions: {
