6de92254fe71f7b116636f236437b59dde6b292f
Faris Mohammed <farismosman@gmail.com>
2015-08-06 16:20:13 +0300

    #96260176 job/faris gets user permissions and sets can confirm on scope

--- eums/client/app/scripts/delivery/ip-delivery/ip-delivery.js
+++ eums/client/app/scripts/delivery/ip-delivery/ip-delivery.js
@@ -1,15 +1,36 @@
 'use strict';
 
-angular.module('IpDelivery', ['eums.config', 'ngTable', 'siTable', 'Loader'])
-    .controller('IpDeliveryController', function ($scope, DeliveryService, LoaderService) {
+angular.module('IpDelivery', ['eums.config', 'ngTable', 'siTable', 'Loader', 'User'])
+    .controller('IpDeliveryController', function ($scope, DeliveryService, LoaderService, UserService) {
         LoaderService.showLoader();
 
         $scope.deliveries = [];
 
-        DeliveryService.all().then(function(deliveries){
+        DeliveryService.all().then(function (deliveries) {
             $scope.deliveries = deliveries;
             LoaderService.hideLoader();
         });
+
+        UserService.retrieveUserPermissions().then(function (userPermissions) {
+            $scope.canConfirm = _isSubarray(userPermissions, [
+                "auth.can_view_distribution_plans",
+                "auth.can_add_textanswer",
+                "auth.change_textanswer",
+                "auth.add_nimericanswer",
+                "auth.change_nimericanswer"
+            ]);
+        });
+
+        function _isSubarray(mainArray, testArray) {
+            var found = [];
+            testArray.forEach(function (element) {
+                if (mainArray.indexOf(element) != -1) {
+                    found.add(element)
+                }
+            });
+
+            return found.length === testArray.length;
+        }
     });
 
 
--- eums/client/test/spec/controllers/ip-delivery-controller-spec.js
+++ eums/client/test/spec/controllers/ip-delivery-controller-spec.js
@@ -1,5 +1,5 @@
 describe('IP Delivery Controller', function () {
-    var mockDeliveryService, scope, location, mockLoaderService, q;
+    var mockDeliveryService, scope, location, mockLoaderService, q, mockUserService, controller;
 
     var firstDelivery = {
         id: 1,
@@ -17,6 +17,14 @@ describe('IP Delivery Controller', function () {
         delivery_date: '2015-03-15',
         remark: 'no remarks'
     };
+    var ipEditorPermissions = [
+        "auth.can_view_distribution_plans",
+        "auth.can_add_textanswer",
+        "auth.change_textanswer",
+        "auth.add_nimericanswer",
+        "auth.change_nimericanswer"
+    ];
+    var ipViewerPermissions = ["auth.can_view_distribution_plans"];
     var deliveries = [firstDelivery, secondDelivery];
     var emptyFunction = function () {
     };
@@ -28,29 +36,38 @@ describe('IP Delivery Controller', function () {
         else return mockLoaderService;
     };
 
+    function initializeController(userService) {
+        controller('IpDeliveryController', {
+            $scope: scope,
+            DeliveryService: mockDeliveryService,
+            LoaderService: mockLoaderService,
+            UserService: userService || mockUserService
+        });
+    }
+
     beforeEach(function () {
+
         module('IpDelivery');
 
         mockDeliveryService = jasmine.createSpyObj('mockDeliveryService', ['all']);
 
-        inject(function ($controller, $rootScope, $location, $q, LoaderService) {
+        inject(function ($controller, $rootScope, $location, $q, LoaderService, UserService) {
+            controller = $controller;
             scope = $rootScope.$new();
             location = $location;
             q = $q;
             mockLoaderService = LoaderService;
+            mockUserService = UserService;
 
-            mockDeliveryService.all.and.returnValue($q.when(deliveries));
+            mockDeliveryService.all.and.returnValue(q.when(deliveries));
 
             spyOn(angular, 'element').and.callFake(jqueryFake);
             spyOn(mockModal, 'modal');
             spyOn(mockLoaderService, 'showLoader');
             spyOn(mockLoaderService, 'hideLoader');
+            spyOn(mockUserService, 'retrieveUserPermissions');
 
-            $controller('IpDeliveryController', {
-                $scope: scope,
-                DeliveryService: mockDeliveryService,
-                LoaderService: mockLoaderService
-            });
+            mockUserService.retrieveUserPermissions.and.returnValue(q.when(ipEditorPermissions));
         });
     });
 
@@ -61,6 +78,7 @@ describe('IP Delivery Controller', function () {
         });
 
         it('should show loader while loading', function () {
+            initializeController();
             scope.$apply();
 
             expect(mockLoaderService.showLoader).toHaveBeenCalled();
@@ -68,6 +86,7 @@ describe('IP Delivery Controller', function () {
         });
 
         it('should call delivery service and put deliveries on the scope', function () {
+            initializeController();
             deliveriesPromise.resolve(deliveries);
             scope.$apply();
 
@@ -75,6 +94,23 @@ describe('IP Delivery Controller', function () {
             expect(scope.deliveries).toBe(deliveries);
             expect(mockLoaderService.hideLoader).toHaveBeenCalled();
             expect(mockLoaderService.hideLoader.calls.count()).toBe(1);
-        })
+        });
+
+        it('should set can confirm permission to true for IP_Editor', function () {
+            initializeController();
+            scope.$apply();
+
+            expect(mockUserService.retrieveUserPermissions).toHaveBeenCalled();
+            expect(scope.canConfirm).toBe(true);
+        });
+
+        it('should set can confirm permission to false for IP_viewer', function () {
+            mockUserService.retrieveUserPermissions.and.returnValue(q.when(ipViewerPermissions));
+            initializeController(mockUserService);
+            scope.$apply();
+
+            expect(mockUserService.retrieveUserPermissions).toHaveBeenCalled();
+            expect(scope.canConfirm).toBe(false);
+        });
     });
 });
