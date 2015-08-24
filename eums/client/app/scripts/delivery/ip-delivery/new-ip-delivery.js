d47ccca7f38843a729ee3069f9a86347b1b76819
francikas <francikas@gmail.com>
2015-08-24 11:52:57 +0300

    [#100137324] [nim, francis] New IP delivery page can add contact

--- eums/client/app/scripts/delivery/ip-delivery/new-ip-delivery.js
+++ eums/client/app/scripts/delivery/ip-delivery/new-ip-delivery.js
@@ -1,6 +1,29 @@
 'use strict';
 
 angular.module('NewIpDelivery', ['eums.config'])
-    .controller('NewIpDeliveryController', function ($scope) {
+    .controller('NewIpDeliveryController', function ($scope, IPService) {
         $scope.districts = [];
+        $scope.contact = {};
+
+        IPService.loadAllDistricts().then(function(response) {
+            $scope.districts = response.data.map(function (districtName) {
+                return {id: districtName, name: districtName};
+            });
+            $scope.districtsLoaded = true;
+        });
+
+        $scope.addContact = function () {
+            $scope.$broadcast('add-contact');
+        };
+
+        $scope.$on('contact-saved', function (event, contact) {
+            var contactInput = angular.element('#contact-select');
+            var contactSelect2Input = contactInput.siblings('div').find('a span.select2-chosen');
+            contactSelect2Input.text(contact.firstName + ' ' + contact.lastName);
+
+            $scope.contact = contact;
+            $scope.contact_person_id = contact._id;
+
+            event.stopPropagation();
+        });
     });
