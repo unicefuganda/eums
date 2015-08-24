4dcd8fb084cf1a554f38e0c7cd21e78b7b933101
NavaL <mnandri@thoughtworks.com>
2015-08-24 16:03:12 +0300

    [X] added  contact services

---
+++ eums/services/contacts.py
@@ -0,0 +1,11 @@
+
+class ContactService:
+    
+    def __init__(self, **kwargs):
+        self._id = kwargs.get('_id')
+        self.first_name = kwargs.get('firstName')
+        self.last_name = kwargs.get('lastName')
+        self.phone = kwargs.get('phone')
+
+    def full_name(self):
+        return "%s %s" % (self.first_name, self.last_name)
---
+++ eums/test/services/test_contacts_service.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from eums.services.contacts import ContactService
+
+
+class ContactServiceTest(TestCase):
+    def test_should_get_fullname(self):
+        contact_data = {
+            u'_id': 'contact_person_id',
+            u'firstName': u'chris',
+            u'lastName': u'george',
+            u'phone': u'+256781111111'}
+
+        contact = ContactService(**contact_data)
+
+        self.assertEqual(contact.full_name(), "chris george")
