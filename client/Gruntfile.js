faa30e00405154e07a5d5d47fabbc4af5c2d3ab3
DewaldV <dewald.viljoen@gmail.com>
2015-08-25 09:20:33 +0200

    [DewaldV] Remove some dead code fron gruntfile.

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -371,8 +371,6 @@ module.exports = function (grunt) {
 
     });
 
-//    grunt.loadNpmTasks('grunt-contrib-less');
-
     grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
         if (target === 'dist') {
             return grunt.task.run(['build', 'connect:dist:keepalive']);
