c9d22b5939d820e80f818c213251f3d50fec755d
Faris Mohammed <farismosman@gmail.com>
2015-08-25 16:28:40 +0200

    faris [#96260206] - calls service with page number

--- eums/client/app/scripts/reports/report-service.js
+++ eums/client/app/scripts/reports/report-service.js
@@ -2,6 +2,29 @@
 
 angular.module('ReportService', ['eums.config'])
     .factory('ReportService', function ($http, $q, EumsConfig) {
+
+        function buildReportUrlParams(params) {
+            var reportEndpoint = EumsConfig.BACKEND_URLS.IP_FEEDBACK_REPORT;
+            var query = params ? '?query=' + encodeURI(params.query) : undefined;
+            var page = params ? '?page=' + params.page : undefined;
+            var url;
+
+            if (params) {
+                if (params.query && params.page) {
+                    url = reportEndpoint + query + '&' + page
+                } else if (params.query) {
+                    url = reportEndpoint + query
+                } else if (params.page) {
+                    url = reportEndpoint + page
+                } else {
+                    url = reportEndpoint;
+                }
+            } else {
+                url = reportEndpoint
+            }
+            return url;
+        }
+
         return {
             allIpResponses: function () {
                 var result = $q.defer();
@@ -11,12 +34,9 @@ angular.module('ReportService', ['eums.config'])
                 return result.promise
             },
 
-            ipFeedbackReport: function (searchTerm) {
+            ipFeedbackReport: function (params) {
                 var result = $q.defer();
-                var reportEndpoint = EumsConfig.BACKEND_URLS.IP_FEEDBACK_REPORT;
-                var query = '?query=' + encodeURI(searchTerm);
-                var url = searchTerm? reportEndpoint + query : reportEndpoint;
-
+                var url = buildReportUrlParams(params);
                 $http.get(url)
                     .then(function (response) {
                         result.resolve(response.data);
