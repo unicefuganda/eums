40d62b14eca12e8ccaf7a00dce8196ca818ffb56
francikas <francikas@gmail.com>
2015-08-31 17:05:08 +0300

    [#100137782] naval/francis created middle man questions fixture script

--- eums/fixtures/questions.py
+++ eums/fixtures/questions.py
@@ -1,9 +1,9 @@
 from eums.fixtures.end_user_questions import *
 from eums.fixtures.ip_questions import *
 from eums.fixtures.web_questions import *
+from eums.fixtures.middle_man_questions import *
 
 
 def seed_questions_and_flows():
-    middle_man_flow, _ = Flow.objects.get_or_create(rapid_pro_id=2662, for_runnable_type=Runnable.MIDDLE_MAN)
 
     return {'END_USER_FLOW': end_user_flow, 'MIDDLE_MAN_FLOW': middle_man_flow, 'IP_FLOW': ip_flow, 'WEB_FLOW': web_flow}
