d17b0d0b20aeb8467e39dbe54beccd43b8445af1
Quintis Venter <quintis@quint.is>
2015-09-02 10:43:31 +0200

    add worker container with celery - config is wierd

--- scripts/deployment/celery.sh
+++ scripts/deployment/celery.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-source ~/.virtualenvs/eums/bin/activate
-cd /opt/app/eums
-export RAPIDPRO_API_TOKEN=b7aec884334247efd48d87f1498f6d35e1519224; celery worker -A eums --loglevel=INFO
+# source ~/.virtualenvs/eums/bin/activate
+cd /eums
+RAPIDPRO_API_TOKEN=b7aec884334247efd48d87f1498f6d35e1519224 celery worker -A eums --uid=worker --loglevel=INFO --broker=redis://redis/0
