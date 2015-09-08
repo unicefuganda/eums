d58290fda7191c5ae6bc976b7da683d039c47707
jobasiimwe <jobasiimwe@gmail.com>
2015-09-08 16:52:53 +0300

    job #102651244 fix to backend tests

--- eums/test/models/test_consignee.py
+++ eums/test/models/test_consignee.py
@@ -1,11 +1,12 @@
 from unittest import TestCase
 
-from eums.models import Consignee
+from eums.models import Consignee, DistributionPlan
 
 
 class ConsigneeTest(TestCase):
 
     def tearDown(self):
+        DistributionPlan.objects.all().delete()
         Consignee.objects.all().delete()
 
     def test_should_have_all_expected_fields(self):
