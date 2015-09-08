3f7adf9b71572db5f089532c15e7b86270d31279
Tom Oketch <tc.oketch@gmail.com>
2015-09-08 19:42:26 +0300

    [@tomclement] Remove extraneous describe block in contacts spec

--- eums/client/test/functional/contacts-spec.js
+++ eums/client/test/functional/contacts-spec.js
@@ -10,21 +10,20 @@ describe('Contacts', function () {
         loginPage.loginAs('admin', 'admin');
         contactsPage.visit();
     });
-    describe('Testing Contact displays', function () {
 
-        it('Existing contacts should be shown on the contacts page', function () {
-            expect(contactsPage.contactCount).toEqual(1);
-            expect(contactsPage.contactFirstNames).toContain('John');
-            expect(contactsPage.contactLastNames).toContain('Doe');
-            expect(contactsPage.contactPhoneNumbers).toContain('+256771234567');
-        });
-
-        it('Searching for contacts should show only relevant results', function () {
-            contactsPage.searchForThisContact('Non-existent Contact');
-            expect(contactsPage.contactCount).toEqual(0);
-
-            contactsPage.searchForThisContact('John');
-            expect(contactsPage.contactCount).toEqual(1);
-        });
+    it('Existing contacts should be shown on the contacts page', function () {
+        expect(contactsPage.contactCount).toEqual(1);
+        expect(contactsPage.contactFirstNames).toContain('John');
+        expect(contactsPage.contactLastNames).toContain('Doe');
+        expect(contactsPage.contactPhoneNumbers).toContain('+256771234567');
     });
+
+    it('Searching for contacts should show only relevant results', function () {
+        contactsPage.searchForThisContact('Non-existent Contact');
+        expect(contactsPage.contactCount).toEqual(0);
+
+        contactsPage.searchForThisContact('John');
+        expect(contactsPage.contactCount).toEqual(1);
+    });
+
 });
