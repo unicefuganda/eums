44bcc4013fbb5f849fb46c42c051ee8e9423f801
DewaldV <dewald.viljoen@gmail.com>
2015-09-11 10:00:04 +0200

    Added eums entrypoint config for new script.

--- eums.sh
+++ eums.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+set -e
 
-python manage.py migrate
-python manage.py setup_permissions
+if [ "${1}" = 'manage.py' ]; then
+  exec python "${@}"
+fi
 
-/usr/local/bin/uwsgi --emperor /etc/uwsgi/sites --env DJANGO_SETTINGS_MODULE=eums.settings --uid worker --gid worker
+if [ "${1}" = 'uwsgi' ]; then
+  python manage.py migrate
+  python manage.py setup_permissions
+
+  exec /usr/local/bin/uwsgi --emperor /etc/uwsgi/sites --env DJANGO_SETTINGS_MODULE=eums.settings --uid worker --gid worker
+fi
