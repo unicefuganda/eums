def5b767db9eaf2db0ae0edc8aff48919b07b1fd
Tom Oketch <tc.oketch@gmail.com>
2015-09-14 18:39:07 +0300

    [@tomclement #103075022] Adds functional test to ensure stock report is displayed

---
+++ eums/client/test/functional/pages/reports-page.js
@@ -0,0 +1,29 @@
+var ReportsPage = function () {};
+
+ReportsPage.prototype = Object.create({}, {
+    url: { get: function () { return '#/reports' }},
+    visit: {
+        value: function () { browser.get(this.url);
+    }},
+
+    selectConsignee: { value: function (input) {
+        element(by.id('s2id_select-ip')).click();
+        element(by.css('.select2-input.select2-focused')).clear().sendKeys(input);
+        element(by.css('.select2-results li')).click();
+    }},
+
+    stockDocumentNumbers: { get: function () { return element.all(by.repeater('reportItem in reportData').column('reportItem.document_number')).getText(); }},
+    stockReceivedValues: { get: function () { return element.all(by.repeater('reportItem in reportData').column('reportItem.total_value_received')).getText(); }},
+    stockBalances: { get: function () { return element.all(by.repeater('reportItem in reportData').column('reportItem.balance')).getText(); }},
+
+    selectFirstPO: { value: function () { element.all(by.repeater('reportItem in reportData').column('reportItem.document_number')).get(0).click(); }},
+
+    itemCodes: { get: function () { return element.all(by.repeater('item in reportItem.items').column('item.code')).getText(); }},
+    itemDescriptions: { get: function () { return element.all(by.repeater('item in reportItem.items').column('item.description')).getText(); }},
+    itemDeliveredQty: { get: function () { return element.all(by.repeater('item in reportItem.items').column('item.quantity_delivered')).getText(); }},
+    itemConfirmedQty: { get: function () { return element.all(by.repeater('item in reportItem.items').column('item.quantity_confirmed')).getText(); }},
+    itemDeliveryDate: { get: function () { return element.all(by.repeater('item in reportItem.items').column('item.date_delivered')).getText(); }},
+    itemBalances: { get: function () { return element.all(by.repeater('item in reportItem.items').column('item.balance')).getText(); }}
+});
+
+module.exports = new ReportsPage;
---
+++ eums/client/test/functional/reports-spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var reportsPage = require('./pages/reports-page.js');
+var loginPage = require('./pages/login-page.js');
+
+describe('Reports', function () {
+
+    it('should show the IP stock report', function () {
+        loginPage.visit();
+        loginPage.loginAs('admin', 'admin');
+        reportsPage.visit();
+
+        reportsPage.selectConsignee('Adjumani');
+        expect(reportsPage.stockDocumentNumbers).toContain('12345');
+        expect(reportsPage.stockReceivedValues).toContain('422.7');
+        expect(reportsPage.stockBalances).toContain('422.7');
+
+        reportsPage.selectFirstPO();
+        expect(reportsPage.itemCodes).toContain('S0782208');
+        expect(reportsPage.itemCodes).toContain('SL004638');
+        expect(reportsPage.itemDescriptions).toContain('Safety box f.used syrgs/ndls 5lt/BOX-25');
+        expect(reportsPage.itemDescriptions).toContain('Fact sheet2013 A4 2013 Full colour 2013 double s');
+        expect(reportsPage.itemDeliveredQty).toContain('30');
+        expect(reportsPage.itemDeliveredQty).toContain('3');
+        expect(reportsPage.itemConfirmedQty).toContain('30');
+        expect(reportsPage.itemConfirmedQty).toContain('0');
+        expect(reportsPage.itemDeliveryDate).toContain('2014-06-10');
+        expect(reportsPage.itemBalances).toContain('30');
+        expect(reportsPage.itemBalances).toContain('0');
+    });
+
+});
