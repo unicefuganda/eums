f54b0fb778c2e1bbb2c70c6e39c2ca2c10cfdbeb
Tom Oketch <tc.oketch@gmail.com>
2015-09-18 13:12:12 +0300

    Fix typo

--- scripts/deployment/buildConfigs.sh
+++ scripts/deployment/buildConfigs.sh
@@ -16,6 +16,6 @@ EMAIL_PASSWORD=$3
 cd /opt/app/contacts
 sed -i "s/stg.eum.unicefuganda.org/${HOST}/" config/config.json
 
-cd /opt/app/eums
+cd /opt/app/eums/eums
 sed -i -e "s/os.getenv('RAPIDPRO_API_TOKEN', 'invalid_token_if_no_token')/${RAPIDPRO_API_TOKEN}/g" settings.py
 sed -i -e "s/os.getenv('EMAIL_PASSWORD', 'invalid_if_no_email')/${EMAIL_PASSWORD}/g" settings.py
