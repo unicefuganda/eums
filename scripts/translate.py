25f59eac014010abc11d289cdebcfde7917d09af
wanderanimrod <wanderanimrod@gmail.com>
2015-10-19 15:03:15 +0300

    [nim, francis] [#X] adds responses and items to node

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,4 +1,5 @@
-from eums.models import ReleaseOrderItem, PurchaseOrderItem, DistributionPlanNode
+from eums.models import ReleaseOrderItem, PurchaseOrderItem, DistributionPlanNode, NumericAnswer, TextAnswer, \
+    MultipleChoiceAnswer
 
 
 def _extract_clean_fields(obj):
@@ -31,28 +32,34 @@ def serialise_purchase_order(purchase_order):
     return po_json
 
 
+def serialise_item(item):
+    item_json = _extract_clean_fields(item)
+    item_json['unit'] = item.unit.name if item.unit else 'null'
+    return item_json
+
+
 def serialise_sales_order(sales_order):
     return _extract_clean_fields(sales_order)
 
 
-def serialise_release_order_item(item_id):
-    release_order_item = ReleaseOrderItem.objects.get(pk=item_id)
-    ro_item_json = _extract_clean_fields(release_order_item)
-    ro_item_json['release_order'] = serialise_release_order(release_order_item.release_order)
+def serialise_release_order_item(item):
+    ro_item_json = _extract_clean_fields(item)
+    ro_item_json['release_order'] = serialise_release_order(item.release_order)
+    ro_item_json['item'] = serialise_item(item.item)
     return ro_item_json
 
 
-def serialise_purchase_order_item(item_id):
-    purchase_order_item = PurchaseOrderItem.objects.get(pk=item_id)
-    po_item_json = _extract_clean_fields(purchase_order_item)
-    po_item_json['purchase_order'] = serialise_purchase_order(purchase_order_item.purchase_order)
+def serialise_purchase_order_item(item):
+    po_item_json = _extract_clean_fields(item)
+    po_item_json['purchase_order'] = serialise_purchase_order(item.purchase_order)
+    po_item_json['item'] = serialise_item(item.item)
     return po_item_json
 
 
 def serialise_order_item(order_item):
     if isinstance(order_item, ReleaseOrderItem):
-        return serialise_release_order_item(order_item.id)
-    return serialise_purchase_order_item(order_item.id)
+        return serialise_release_order_item(order_item)
+    return serialise_purchase_order_item(order_item)
 
 
 def serialise_node(node):
@@ -60,7 +67,42 @@ def serialise_node(node):
     node_json['consignee'] = serialise_consignee(node.consignee)
     node_json['programme'] = serialise_programme(node.programme)
     node_json['order_item'] = serialise_order_item(node.item)
+    node_json['responses'] = serialise_node_responses(node)
+    if node.ip:
+        node_json['ip'] = serialise_consignee(node.ip)
     return node_json
+
+
+def serialise_node_responses(node):
+    numeric_answers = NumericAnswer.objects.filter(run__runnable=node)
+    text_answers = TextAnswer.objects.filter(run__runnable=node)
+    multiple_choice_answers = MultipleChoiceAnswer.objects.filter(run__runnable=node)
+    serialised_simple = map(lambda answer: serialise_simple_answer(answer), list(numeric_answers) + list(text_answers))
+    serialised_multiple = map(lambda answer: serialise_multiple_choice_answer(answer), multiple_choice_answers)
+    return serialised_simple + serialised_multiple
+
+
+def serialise_simple_answer(answer):
+    answer_json = _extract_clean_fields(answer)
+    answer_json['question'] = serialise_question(answer.question)
+    answer_json['run'] = serialise_run(answer.run)
+    return answer_json
+
+
+def serialise_multiple_choice_answer(answer):
+    answer_json = _extract_clean_fields(answer)
+    answer_json['run'] = serialise_run(answer.run)
+    answer_json['question'] = serialise_question(answer.question)
+    answer_json['value'] = answer.value.text
+    return answer_json
+
+
+def serialise_question(question):
+    return _extract_clean_fields(question)
+
+
+def serialise_run(run):
+    return _extract_clean_fields(run)
 
 
 def serialise_nodes():
