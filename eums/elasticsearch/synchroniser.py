0041ffbc20ee1123b029e21bfd2fee2c18a633e2
wanderanimrod <wanderanimrod@gmail.com>
2015-10-22 12:26:33 +0300

    [nim, francis] [#96262616] Synchroniser serialises nodes

--- eums/elasticsearch/synchroniser.py
+++ eums/elasticsearch/synchroniser.py
@@ -1,9 +1,12 @@
+from eums.elasticsearch.serialisers import serialise_nodes
 from eums.elasticsearch.sync_info import SyncInfo
 from eums.models import DistributionPlanNode as DeliveryNode
 
 
 def run():
     SyncInfo.objects.create()
+    nodes_to_sync = generate_nodes_to_sync()
+    serialise_nodes(nodes_to_sync)
 
 
 def generate_nodes_to_sync():
--- eums/test/elasticsearch/synchronisation/test_synchroniser.py
+++ eums/test/elasticsearch/synchronisation/test_synchroniser.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
+from mock import patch
 
 from eums.elasticsearch.sync_info import SyncInfo
 from eums.elasticsearch.synchroniser import generate_nodes_to_sync, run
@@ -10,7 +11,17 @@ class SynchroniserTest(TestCase):
     def tearDown(self):
         SyncInfo.objects.all().delete()
 
-    def test_should_create_a_sync_info_record_when_sync_starts(self):
+    @patch('eums.elasticsearch.synchroniser.generate_nodes_to_sync')
+    @patch('eums.elasticsearch.synchroniser.serialise_nodes')
+    def test_run_should_serialise_nodes_that_are_marked_for_sync(self, mock_serialiser, mock_generate_nodes):
+        mock_nodes = [1, 2]
+        mock_serialiser.return_value = None
+        mock_generate_nodes.return_value = mock_nodes
+        run()
+        mock_serialiser.assert_called_with(mock_nodes)
+
+    @patch('eums.elasticsearch.synchroniser.generate_nodes_to_sync')
+    def test_should_create_a_sync_info_record_when_sync_starts(self, _):
         self.assertEqual(SyncInfo.objects.count(), 0)
         run()
         self.assertEqual(SyncInfo.objects.count(), 1)
@@ -29,3 +40,4 @@ class SynchroniserTest(TestCase):
         self.assertIn(post_sync_node_one, nodes_to_sync)
         self.assertIn(post_sync_node_two, nodes_to_sync)
         self.assertNotIn(pre_sync_node, nodes_to_sync)
+
