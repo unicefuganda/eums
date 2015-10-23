6cb328e027d7bd67d5c7179a9fdd6e81de716e14
wanderanimrod <wanderanimrod@gmail.com>
2015-10-23 12:44:24 +0300

    [nim, francis] [#96262616] Nodes whose consignee data changes after last sync will be reserialised and pushed to elasticsearch

--- eums/elasticsearch/sync_data_generators.py
+++ eums/elasticsearch/sync_data_generators.py
@@ -1,14 +1,22 @@
 from celery.utils.log import get_task_logger
+from django.conf import settings
+import requests
 
 from eums.elasticsearch.mappings import setup_mappings
 from eums.elasticsearch.sync_info import SyncInfo
-from eums.models import DistributionPlanNode as DeliveryNode
+from eums.models import DistributionPlanNode as DeliveryNode, Consignee
 
 logger = get_task_logger(__name__)
 
 
 def generate_nodes_to_sync():
     last_sync = SyncInfo.last_successful_sync()
+    nodes_to_update = _find_nodes_to_update(last_sync)
+    new_nodes = _find_new_nodes(last_sync)
+    return list(nodes_to_update) + list(new_nodes)
+
+
+def _find_new_nodes(last_sync):
     if not last_sync:
         setup_mappings()
         return DeliveryNode.objects.all()
@@ -16,3 +24,19 @@ def generate_nodes_to_sync():
     return DeliveryNode.objects.filter(created__gte=last_sync_time)
 
 
+def _find_nodes_to_update(last_sync):
+    if last_sync:
+        changed_consignee_ids = Consignee.objects.filter(modified__gte=last_sync.start_time).values_list('id', flat=True)
+        query = {
+            "fields": [],
+            "filter": {
+                "terms": {
+                    "consignee.id": changed_consignee_ids
+                }
+            }
+        }
+        url = '%s/delivery_node/_search' % settings.ELASTIC_SEARCH_URL
+        response = requests.post(url, json=query)
+        node_ids = [hit['_id'] for hit in response.json()['hits']['hits']]
+        return DeliveryNode.objects.filter(pk__in=node_ids)
+    return []
--- eums/test/elasticsearch/synchronisation/test_sync_data_generators.py
+++ eums/test/elasticsearch/synchronisation/test_sync_data_generators.py
@@ -8,6 +8,7 @@ from eums.elasticsearch.mappings import DELIVERY_NODE_MAPPING
 from eums.elasticsearch.sync_info import SyncInfo
 from eums.elasticsearch.sync_data_generators import generate_nodes_to_sync
 from eums.rapid_pro.fake_response import FakeResponse
+from eums.test.factories.consignee_factory import ConsigneeFactory
 from eums.test.factories.delivery_node_factory import DeliveryNodeFactory
 from eums.models import DistributionPlanNode as DeliveryNode
 
@@ -33,9 +34,33 @@ class SyncDataGeneratorsTest(TestCase):
 
         nodes_to_sync = generate_nodes_to_sync()
 
-        self.assertEqual(nodes_to_sync.count(), 2)
+        self.assertEqual(len(nodes_to_sync), 2)
         self.assertIn(node_one, nodes_to_sync)
         self.assertIn(node_two, nodes_to_sync)
+
+    @patch('requests.post')
+    def test_should_add_node_related_to_changed_consignee_to_sync_data(self, mock_post):
+        consignee = ConsigneeFactory(location='Old location')
+        node = DeliveryNodeFactory(consignee=consignee)
+
+        SyncInfo.objects.create(status=SyncInfo.STATUS.SUCCESSFUL)
+        nodes_to_sync = generate_nodes_to_sync()
+        self.assertNotIn(node, nodes_to_sync)
+
+        consignee.location = 'New location'
+        consignee.save()
+        es_response = FakeResponse({"hits": {"hits": [{"_id": node.id}]}}, status_code=HTTP_200_OK)
+        mock_post.return_value = es_response
+        url = '%s/delivery_node/_search' % settings.ELASTIC_SEARCH_URL
+        expected_query = {'filter': {'terms': {'consignee.id': [consignee.id]}}, 'fields': []}
+
+        nodes_to_sync = generate_nodes_to_sync()
+
+        call_args = mock_post.call_args
+
+        self.assertEqual(call_args[0], (url,))
+        self.assertEqual(str(call_args[1]['json']), str(expected_query))
+        self.assertIn(node, nodes_to_sync)
 
     @patch('eums.elasticsearch.synchroniser.logger.error')
     @patch('requests.post')
@@ -49,6 +74,7 @@ class SyncDataGeneratorsTest(TestCase):
         TODO This is failing when all tests are run because we loose the time aspect of the 'created' field when
           querying nodes from the db. This happens only when running tests
     '''
+
     def xtest_should_include_new_nodes_in_sync_queryset(self):
         pre_sync_node = DeliveryNodeFactory()
 
@@ -59,8 +85,7 @@ class SyncDataGeneratorsTest(TestCase):
 
         nodes_to_sync = generate_nodes_to_sync()
 
-        self.assertEqual(nodes_to_sync.count(), 2)
+        self.assertEqual(len(nodes_to_sync), 2)
         self.assertIn(post_sync_node_one, nodes_to_sync)
         self.assertIn(post_sync_node_two, nodes_to_sync)
         self.assertNotIn(pre_sync_node, nodes_to_sync)
-
