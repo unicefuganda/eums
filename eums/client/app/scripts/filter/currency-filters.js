c61929b1fd167f8ad10566d126adb05c522f28a1
Chris George <cgeorge@nacgeorge.local>
2015-10-25 12:36:50 +0300

    [#106290904] chris - removing decimal places for non-compact numbers

--- eums/client/app/scripts/filter/currency-filters.js
+++ eums/client/app/scripts/filter/currency-filters.js
@@ -3,15 +3,7 @@
 angular.module('eums.currencyFilters', [])
     .filter('compactCurrency', function($filter, compactNumberFilter) {
         return function(amount) {
-            if (amount === undefined) {
-                amount = 0;
-            }
-            if (amount >= 1000000) {
-                return '$' + compactNumberFilter(amount);
-            } else if (amount >= 1000) {
-                return '$' + compactNumberFilter(amount);
-            }
-            return $filter('currency')(amount);
+            return '$' + compactNumberFilter(amount);
         };
     })
     .filter('compactNumber', function() {
@@ -21,9 +13,10 @@ angular.module('eums.currencyFilters', [])
             }
             if (amount >= 1000000) {
                 return (amount / 1.0e6).toFixed(1) + 'm';
-            } else if (amount >= 1000) {
+            }
+            if (amount >= 1000) {
                 return (amount / 1.0e3).toFixed(1) + 'k';
             }
-            return amount.toFixed(2);
+            return amount.toFixed(0);
         };
     });
--- eums/client/test/spec/filters/currency-filters-spec.js
+++ eums/client/test/spec/filters/currency-filters-spec.js
@@ -6,7 +6,7 @@ describe('EUMS currency filters', function() {
 
         it('should return back number when not in thousands or millions',
             inject(function(compactCurrencyFilter) {
-                expect(compactCurrencyFilter(567.88)).toBe('$567.88');
+                expect(compactCurrencyFilter(567.88)).toBe('$568');
             })
         );
 
@@ -36,7 +36,7 @@ describe('EUMS currency filters', function() {
 
         it('should return back $0.00 when amount is undefined',
             inject(function(compactCurrencyFilter) {
-                expect(compactCurrencyFilter(undefined)).toBe('$0.00');
+                expect(compactCurrencyFilter(undefined)).toBe('$0');
             })
         );
     });
@@ -45,13 +45,13 @@ describe('EUMS currency filters', function() {
 
         it('should return back two decimal places for smaller numbers',
             inject(function(compactNumberFilter) {
-                expect(compactNumberFilter(678.40)).toBe('678.40');
+                expect(compactNumberFilter(678.60)).toBe('679');
             })
         );
 
         it('should return back 0.00 when amount is undefined',
             inject(function(compactNumberFilter) {
-                expect(compactNumberFilter(undefined)).toBe('0.00');
+                expect(compactNumberFilter(undefined)).toBe('0');
             })
         );
     });
