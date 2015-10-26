81f01aaf6f51a06b18b5bfbca8cc834d3ca58adb
wanderanimrod <wanderanimrod@gmail.com>
2015-10-26 13:46:11 +0300

    [nim, francis] [#96260826] Merges conflicting migrations

---
+++ eums/migrations/0033_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eums', '0032_auto_20151026_1235'),
+        ('eums', '0032_auto_20151026_1213'),
+    ]
+
+    operations = [
+    ]
