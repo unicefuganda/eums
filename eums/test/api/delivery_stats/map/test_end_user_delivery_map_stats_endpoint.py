98dbda6495ed00fbfc487fe2c84cff2a469bb21b
NavaL <mnandri@thoughtworks.com>
2015-10-29 18:11:14 +0300

    #105093942 adapted map stats details to cater for end user view

---
+++ eums/test/api/delivery_stats/map/test_end_user_delivery_map_stats_endpoint.py
@@ -0,0 +1,146 @@
+import datetime
+
+from eums.models import MultipleChoiceQuestion, Run
+from eums.test.api.delivery_stats.delivery_stats_test_case import DeliveryStatsTestCase
+from eums.test.config import BACKEND_URL
+from eums.test.factories.answer_factory import MultipleChoiceAnswerFactory
+from eums.test.factories.consignee_factory import ConsigneeFactory
+from eums.models.distribution_plan_node import DistributionPlanNode as DeliveryNode
+from eums.test.factories.delivery_factory import DeliveryFactory
+from eums.test.factories.delivery_node_factory import DeliveryNodeFactory
+from eums.test.factories.programme_factory import ProgrammeFactory
+from eums.test.factories.purchase_order_item_factory import PurchaseOrderItemFactory
+from eums.test.factories.run_factory import RunFactory
+from eums.test.helpers.fake_datetime import FakeDate
+
+ENDPOINT_URL = BACKEND_URL + 'delivery-stats/map/'
+
+
+class EndUserDeliveryMapStatsEndPointTest(DeliveryStatsTestCase):
+    def setUp(self):
+        super(EndUserDeliveryMapStatsEndPointTest, self).setUp()
+        self.other_ip = ConsigneeFactory()
+        self.setup_responses()
+
+    def test_should_return_correct_json_object(self):
+        response = self.client.get(ENDPOINT_URL + '?treePosition=END_USER')
+        expected_stats = [
+            {'numberOfDeliveries': 6, 'location': u'Kampala', 'numberNotReceived': 3, 'hasIssues': 0, 'nonResponse': 1,
+             'numberReceived': 2, 'state': 'map-not-received', 'noIssues': 0}]
+        self.assert_ip_delivery_stats(response, expected_stats)
+
+    def test_should_filter_by_programme(self):
+        response = self.client.get('%s?programme=%s&treePosition=END_USER' % (ENDPOINT_URL, self.programme.id))
+
+        expected_stats = [
+            {"numberOfDeliveries": 1, "location": "Kampala", "numberNotReceived": 1, "hasIssues": 0, "nonResponse": 0,
+             "numberReceived": 0, "state": "map-not-received", "noIssues": 0}]
+
+        self.assert_ip_delivery_stats(response, expected_stats)
+
+    def test_should_filter_by_ip(self):
+        response = self.client.get('%s?ip=%s&treePosition=END_USER' % (ENDPOINT_URL, self.ip.id))
+
+        expected_stats = [
+            {"numberOfDeliveries": 1, "location": "Kampala", "numberNotReceived": 1, "hasIssues": 0, "nonResponse": 0,
+             "numberReceived": 0, "state": "map-not-received", "noIssues": 0}]
+
+        self.assert_ip_delivery_stats(response, expected_stats)
+
+    def test_should_filter_by_from_date(self):
+        response = self.client.get(
+            '%s?from=%s&treePosition=END_USER' % (ENDPOINT_URL, self.today + datetime.timedelta(days=2)))
+
+        expected_stats = [
+            {"numberOfDeliveries": 1, "location": "Kampala", "numberNotReceived": 1, "hasIssues": 0, "nonResponse": 0,
+             "numberReceived": 0, "state": "map-not-received", "noIssues": 0}]
+
+        self.assert_ip_delivery_stats(response, expected_stats)
+
+    def test_should_filter_by_to_date(self):
+        response = self.client.get(
+            '%s?to=%s&treePosition=END_USER' % (ENDPOINT_URL, self.today + datetime.timedelta(days=2)))
+
+        expected_stats = [
+            {"numberOfDeliveries": 5, "location": "Kampala", "numberNotReceived": 2, "hasIssues": 0, "nonResponse": 1,
+             "numberReceived": 2, "state": "map-received", "noIssues": 0}]
+
+        self.assert_ip_delivery_stats(response, expected_stats)
+
+    def test_should_filter_by_both_ip_and_programme(self):
+        response = self.client.get(
+            '%s?ip=%s&programme=%s&treePosition=END_USER' % (ENDPOINT_URL, self.ip.id, self.programme.id))
+
+        expected_stats = [
+            {"numberOfDeliveries": 1, "location": "Kampala", "numberNotReceived": 1, "hasIssues": 0, "nonResponse": 0,
+             "numberReceived": 0, "state": "map-not-received", "noIssues": 0}]
+        self.assert_ip_delivery_stats(response, expected_stats)
+
+        non_existing_ip_id = 22222222
+        response = self.client.get('%s?ip=%s&programme=%s&treePosition=END_USER' % (ENDPOINT_URL, non_existing_ip_id, self.programme.id))
+        self.assertEquals(0, len(response.data))
+
+        non_existing_programme_id = 33333333
+        response = self.client.get('%s?ip=%s&programme=%s&treePosition=END_USER' % (ENDPOINT_URL, self.ip.id, non_existing_programme_id))
+        self.assertEquals(0, len(response.data))
+
+    def assert_ip_delivery_stats(self, response, expected_stats):
+        self.assertEqual(len(response.data), len(expected_stats))
+        for stat in expected_stats:
+            self.assertIn(stat, response.data)
+
+    def setup_responses(self):
+        DeliveryNode.objects.all().delete()
+        MultipleChoiceQuestion.objects.all().delete()
+
+        from eums.fixtures.end_user_questions import seed_questions
+        questions, options = seed_questions()
+
+        po_item = PurchaseOrderItemFactory(quantity=100, value=1000)
+        end_user_node_one = DeliveryNodeFactory(tree_position=DeliveryNode.END_USER, track=True, quantity=10,
+                                                item=po_item)
+        MultipleChoiceAnswerFactory(
+            run=RunFactory(runnable=end_user_node_one, status=Run.STATUS.scheduled),
+            question=questions['WAS_PRODUCT_RECEIVED'],
+            value=options['PRODUCT_WAS_RECEIVED']
+        )
+        self.programme = ProgrammeFactory(name='my-program')
+        self.ip = ConsigneeFactory()
+
+        distribution_plan = DeliveryFactory(programme=self.programme)
+        self.today = FakeDate.today()
+
+        self.end_user_node_two = DeliveryNodeFactory(tree_position=DeliveryNode.END_USER, track=True, quantity=20,
+                                                     item=po_item, )
+        MultipleChoiceAnswerFactory(
+            run=RunFactory(runnable=self.end_user_node_two, status=Run.STATUS.scheduled),
+            question=questions['WAS_PRODUCT_RECEIVED'],
+            value=options['PRODUCT_WAS_RECEIVED']
+        )
+        end_user_node_three = DeliveryNodeFactory(tree_position=DeliveryNode.END_USER, track=True, quantity=30,
+                                                  item=po_item,
+                                                  distribution_plan=distribution_plan,
+                                                  consignee=self.ip,
+                                                  delivery_date=self.today + datetime.timedelta(days=3))
+        MultipleChoiceAnswerFactory(
+            run=RunFactory(runnable=end_user_node_three, status=Run.STATUS.scheduled),
+            question=questions['WAS_PRODUCT_RECEIVED'],
+            value=options['PRODUCT_WAS_NOT_RECEIVED']
+        )
+        end_user_node_four = DeliveryNodeFactory(tree_position=DeliveryNode.END_USER, track=True, quantity=40,
+                                                 item=po_item)
+        MultipleChoiceAnswerFactory(
+            run=RunFactory(runnable=end_user_node_four, status=Run.STATUS.scheduled),
+            question=questions['WAS_PRODUCT_RECEIVED'],
+            value=options['PRODUCT_WAS_NOT_RECEIVED']
+        )
+        end_user_node_five = DeliveryNodeFactory(tree_position=DeliveryNode.END_USER, track=True, quantity=50,
+                                                 item=po_item)
+        MultipleChoiceAnswerFactory(
+            run=RunFactory(runnable=end_user_node_five, status=Run.STATUS.scheduled),
+            question=questions['WAS_PRODUCT_RECEIVED'],
+            value=options['PRODUCT_WAS_NOT_RECEIVED']
+        )
+        non_response_node = DeliveryNodeFactory(tree_position=DeliveryNode.END_USER, track=True, quantity=60,
+                                                item=po_item)
+        RunFactory(runnable=non_response_node, status=Run.STATUS.scheduled)
