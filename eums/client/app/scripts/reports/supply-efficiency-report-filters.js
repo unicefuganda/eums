7e30cee8690cbea0a59eed793417d0e911837173
wanderanimrod <wanderanimrod@gmail.com>
2015-10-30 12:15:14 +0300

    [nim, francis] [#96260826] Extracts partials from efficiency report

---
+++ eums/client/app/scripts/reports/supply-efficiency-report-filters.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('SupplyEfficiencyReportFilters', [
+    'eums.config', 'ngTable', 'siTable', 'eums.ip', 'Consignee', 'Directives', 'Loader'])
+    .controller('SupplyEfficiencyReportFiltersController', function ($scope, IPService, LoaderService) {
+
+    });
+
