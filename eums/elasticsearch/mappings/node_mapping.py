ebfb1680b03002db27a291e2e5b7bfeae91f2dbd
wanderanimrod <wanderanimrod@gmail.com>
2015-11-02 16:58:21 +0300

    [nim, francis] [#96260826] Assigns correct types to node fields to aid aggregations and queries in elasticsearch

--- eums/elasticsearch/mappings/node_mapping.py
+++ eums/elasticsearch/mappings/node_mapping.py
@@ -16,6 +16,23 @@ DELIVERY_NODE_MAPPING = {
                         "type": "integer"
                     }
                 }
+            },
+            "location": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "tree_position": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "total_value": {
+                "type": "float"
+            },
+            "value_lost": {
+                "type": "float"
+            },
+            "delivery_delay": {
+                "type": "integer"
             }
         }
     }
