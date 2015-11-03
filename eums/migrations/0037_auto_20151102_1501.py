35d97d4c1beeae6494c1fe8b4adae30b784a8bb1
NavaL <mnandri@thoughtworks.com>
2015-11-03 11:51:10 +0300

    #105093942 +Job added datamigration to populate deliveries total values

--- eums/migrations/0037_auto_20151102_1501.py
+++ eums/migrations/0037_auto_20151102_1501.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.db.models import Q
 
 
 def transfer_total_value_to_runnable_value(apps, schema_editor):
     DeliveryNode = apps.get_model("eums", "DistributionPlanNode")
-    all_nodes = DeliveryNode.objects.all()
+    db_alias = schema_editor.connection.alias
+    all_nodes = DeliveryNode.objects.using(db_alias).all()
     for node in all_nodes:
         node.runnable_value = node.total_value
         node.save()
@@ -14,9 +16,9 @@ def transfer_total_value_to_runnable_value(apps, schema_editor):
 
 def transfer_runnable_value_to_total_value(apps, schema_editor):
     DeliveryNode = apps.get_model("eums", "DistributionPlanNode")
-    all_nodes = DeliveryNode.objects.all()
+    db_alias = schema_editor.connection.alias
+    all_nodes = DeliveryNode.objects.using(db_alias).filter(Q(total_value=None) | Q(total_value=0))
     for node in all_nodes:
-        if not node.total_value:
             node.total_value = node.runnable_value
             node.save()
 
