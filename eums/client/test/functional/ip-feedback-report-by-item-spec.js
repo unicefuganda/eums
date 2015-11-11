fe609b808c3c93a231827306042fdc25a6726adc
jobasiimwe <jobasiimwe@gmail.com>
2015-11-11 19:06:21 +0300

    #X removing fdescribe

--- eums/client/test/functional/ip-feedback-report-by-item-spec.js
+++ eums/client/test/functional/ip-feedback-report-by-item-spec.js
@@ -4,7 +4,7 @@ var ftUtils = require('./functional-test-utils.js');
 var loginPage = require('./pages/login-page.js');
 var ipFeedbackReportByItemPage = require('./pages/ip-feedback-report-by-item-page.js');
 
-fdescribe('IP Feedback Report By Item', function () {
+describe('IP Feedback Report By Item', function () {
 
     it('should filter by programme, ip, item description, Purchase Order Number or waybill', function () {
         loginPage.visit();
