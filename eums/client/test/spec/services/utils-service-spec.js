ce9babbe2048ecc28e7d117cbaddb1aa9e0e118e
Shenjian YUAN <sjyuan@thoughtworks.com>
2015-11-27 09:34:21 +0800

    [#defect] add unit test case

--- eums/client/test/spec/services/utils-service-spec.js
+++ eums/client/test/spec/services/utils-service-spec.js
@@ -8,9 +8,14 @@ describe('Utils Service', function () {
         });
     });
 
-    it('should format data as DD-MM-YYYY', function () {
+    it('should format 10/10/2015 as 10-Oct-2015', function () {
         var resultData = utilService.formatDate("10/10/2015");
         expect(resultData).toEqual("10-Oct-2015");
+    });
+
+    it('should format empty string as empty string', function () {
+        var resultData = utilService.formatDate("");
+        expect(resultData).toEqual("");
     });
 });
 
