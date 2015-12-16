d627649db5d0b0d0cbd2b8cdd31c9b0bb0610102
Yang Dong <don.yangdong@gmail.com>
2015-12-16 17:56:47 +0800

    merge migration script

---
+++ eums/migrations/0065_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eums', '0063_auto_20151215_1310'),
+        ('eums', '0064_auto_20151215_0921'),
+    ]
+
+    operations = [
+    ]
