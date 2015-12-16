1393c28e026a2d513a3915d8c04440edd6fd4a12
DuYing <18717394542@163.com>
2015-12-16 15:07:39 +0800

    [#96973726] - Ying add test for export_stock_report

---
+++ eums/test/exports/test_stock_report_csv_export_endpoint.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.test import override_settings
+from mock import patch
+from eums.test.api.authenticated_api_test_case import AuthenticatedAPITestCase
+
+ENDPOINT_URL = '/exports/stocks-report/'
+
+
+class TestStockCSVEndpoint(AuthenticatedAPITestCase):
+
+    @patch('eums.services.csv_export_service.generate_stock_feedback_report.delay')
+    def test_should_use_hostname(self, mock_generate_task_delay):
+        response = self.client.get(ENDPOINT_URL)
+        user = User.objects.get(username='test')
+
+        self.assertEqual(response.status_code, 200)
+        mock_generate_task_delay.assert_called_once_with(user, 'http://testserver/', [])
+
+    @override_settings(CELERY_ALWAYS_EAGER=True)
+    @patch('eums.services.csv_export_service.CSVExportService.notify')
+    @patch('eums.services.exporter.stock_feedback_report_csv_exporter.StockFeedbackReportExporter.notification_details')
+    @patch('eums.services.csv_export_service.CSVExportService.generate')
+    @patch('eums.services.exporter.stock_feedback_report_csv_exporter.ReportExporter.assemble_csv_data')
+    @patch('eums.services.exporter.stock_feedback_report_csv_exporter.StockFeedbackReportExporter.generate_exported_csv_file_name')
+    @patch('eums.api.stock_report.stock_report_endpoint.filter_stock_feedback_report')
+    def test_should_start_async_csv_generation_with_direct_deliveries(
+            self, mock_filter_stock_feedback_report,mock_generate_exported_csv_file_name,
+            mock_assemble_csv_data,
+            mock_generate_csv,
+            mock_notification_details, mock_notify_user):
+        expected_data = {'message': 'Generating CSV, you will be notified via email once it is done.'}
+
+        some_data = ['some data']
+        mock_assemble_csv_data.return_value = some_data
+        mock_filter_stock_feedback_report.return_value = []
+
+        file_name = 'stocks_feedback_report_1448892495779.csv'
+        mock_generate_exported_csv_file_name.return_value = file_name
+
+        mock_notification_details.return_value = ('subject', 'message')
+
+        response = self.client.get(ENDPOINT_URL)
+        user = User.objects.get(username='test')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
+        self.assertTrue(mock_generate_exported_csv_file_name.called)
+        self.assertTrue(mock_assemble_csv_data.called)
+
+        mock_generate_csv.assert_called_once_with(some_data, 'report/feedback', file_name)
+        mock_notify_user.assert_called_once_with(user, 'subject', 'message')
+
