f2a6dd0276a0abdc9b2eec06905b7c93c39e62d8
bfeng <bfeng@thoughtworks.com>
2015-12-26 00:44:06 +0800

    [#109227264] pass vision_password into the script file

--- scripts/deployment/unpack_deployment_and_install.sh
+++ scripts/deployment/unpack_deployment_and_install.sh
@@ -6,6 +6,7 @@ today=$1
 hostip=$2
 rapidpro_token=$3
 email_password=$4
+vision_password=$5
 
 echo "Unpacking deployment directory..."
 tar --force-local -xzvf deploy_latest.tar.gz
@@ -18,4 +19,4 @@ cd deploy_$today
 chmod a+x scripts/*.sh
 
 echo "Running install script..."
-scripts/install-image-eums.sh ${hostip} ${rapidpro_token} ${email_password}
+scripts/install-image-eums.sh ${hostip} ${rapidpro_token} ${email_password} ${vision_password}
