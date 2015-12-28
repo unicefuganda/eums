e5f458e27fbbeeb78587a94a13d976c1640b919a
bfeng <bfeng@thoughtworks.com>
2015-12-28 14:10:18 +0800

    [#] try to fix deploy issue

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -10,6 +10,8 @@ fi
 
 sudo build/deployment/pack_deployment.sh $today
 
+echo ${DEPLOY_USER_PASSWORD}
+
 echo "Copying deployment directory to server ..."
 sshpass -p "${DEPLOY_USER_PASSWORD}" scp -o StrictHostKeyChecking=no deploy_latest.tar.gz ${DEPLOY_USER}@${DEPLOY_HOST}:/home/${DEPLOY_USER}/
 
