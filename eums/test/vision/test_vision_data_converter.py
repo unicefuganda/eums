740a271b27d5bfd7c91081c2e03ca4da86a98be7
bfeng <fungbo@126.com>
2016-01-01 14:34:34 +0800

    [#109227264] add vision data converter

---
+++ eums/test/vision/test_vision_data_converter.py
@@ -0,0 +1,36 @@
+import datetime
+from unittest import TestCase
+
+from eums.vision.vision_data_converter import convert_vision_value
+
+
+class TestVisionDataConverter(TestCase):
+    def setUp(self):
+        self.key = ''
+
+    def test_should_convert_unicode_string_to_int(self):
+        value = u'0020173918'
+        self.assertEqual(convert_vision_value(self.key, value), 20173918)
+
+    def test_should_convert_date_string_to_datetime(self):
+        value = '/Date(1449118800000)/'
+        self.assertEqual(convert_vision_value(self.key, value), datetime.datetime(2015, 12, 3, 13, 0))
+
+    def test_should_convert_wbs_code(self):
+        key = 'WBS_REFERENCE'
+        value = '0060A007883001002'
+        self.assertEqual(convert_vision_value(key, value), '0060/A0/07/883')
+
+    def test_should_NOT_convert_common_str_or_digit(self):
+        value = 'UNICEF-AFGHANISTAN-KABUL'
+        self.assertEqual(convert_vision_value(self.key, value), 'UNICEF-AFGHANISTAN-KABUL')
+
+        value = 3655.16
+        self.assertEqual(convert_vision_value(self.key, value), 3655.16)
+
+    def test_should_NOT_convert_empty_value(self):
+        value = ''
+        self.assertEqual(convert_vision_value(self.key, value), '')
+
+        value = []
+        self.assertEqual(convert_vision_value(self.key, value), [])
---
+++ eums/vision/vision_data_converter.py
@@ -0,0 +1,18 @@
+import datetime
+import re
+
+
+def convert_vision_value(key, value):
+    if type(value) == unicode:
+        encoded_value = value.encode('ascii', 'ignore')
+        return int(encoded_value)
+
+    if type(value) == str:
+        matched_date = re.match(r'/Date\((\d+)\)/', value, re.I)
+        if matched_date:
+            return datetime.datetime.fromtimestamp(int(matched_date.group(1)) / 1000.0)
+
+    if key == 'WBS_REFERENCE' and value:
+        return re.sub(r'(.{4})(.{2})(.{2})', r'\1/\2/\3/', value[0:11])
+
+    return value
