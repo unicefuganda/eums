c42d86ef39eeda88703a14081891779a3da37907
WuLingzi <lotuswlz@sina.com>
2016-01-06 11:47:01 +0800

    configuration for production

--- scripts/deployment/unpack_deployment_and_install.sh
+++ scripts/deployment/unpack_deployment_and_install.sh
@@ -8,6 +8,7 @@ rapidpro_token=$3
 email_password=$4
 vision_user=$5
 vision_password=$6
+secret_key=$7
 
 echo "Unpacking deployment directory..."
 tar --force-local -xzvf deploy_latest.tar.gz
@@ -20,4 +21,4 @@ cd deploy_$today
 chmod a+x scripts/*.sh
 
 echo "Running install script..."
-scripts/install-image-eums.sh ${hostip} ${rapidpro_token} ${email_password} ${vision_user} ${vision_password}
+scripts/install-image-eums.sh ${hostip} ${rapidpro_token} ${email_password} ${vision_user} ${vision_password} ${secret_key}
