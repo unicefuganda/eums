72adae4cf08735cbcb100f8691a98eb4135a3283
WuLingzi <lotuswlz@sina.com>
2016-01-07 09:49:29 +0800

    revert rapid pro setting.

--- eums/settings.py
+++ eums/settings.py
@@ -103,7 +103,7 @@ RAPIDPRO_URLS = {
 RAPIDPRO_EXTRAS = {'CONTACT_NAME': 'contactName', 'SENDER': 'sender', 'PRODUCT': 'product'}
 
 # WARNING: Never turn this on unless it is a live instance of the app (Staging or Prod. Not Dev, Test, or QA).
-RAPIDPRO_LIVE = True
+RAPIDPRO_LIVE = False
 
 VISION_USER = os.getenv('VISION_USER', 'invalid_vision_user')
 VISION_PASSWORD = os.getenv('VISION_PASSWORD', 'invalid_vision_password')
