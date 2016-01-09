5aa0f35cf7a4168a491fbed78a938bcbf2bdf3e2
bfeng <fungbo@126.com>
2016-01-09 19:05:08 +0800

    [#X] moving the districts.json instead of copying

--- scripts/deployment/buildConfigs.sh
+++ scripts/deployment/buildConfigs.sh
@@ -7,6 +7,7 @@ echo "Making sure we have the right IPs for the contacts service ..."
 cd /opt/app/eums/eums/client
 
 grunt build-staging:$1
+grunt build-staging:$1
 
 HOST=$1
 RAPIDPRO_API_TOKEN=$2
