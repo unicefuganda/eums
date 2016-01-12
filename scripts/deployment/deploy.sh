15079c339772bc7a0e3c0367451ad8ece0eb3ab1
WuLingzi <lotuswlz@sina.com>
2016-01-12 10:29:59 +0800

    [#108334330] cathy & chris - add quote to each argument for unpack shell

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -24,9 +24,9 @@ sshpass -p "${DEPLOY_USER_PASSWORD}" scp build/deployment/unpack_deployment_and_
 echo "running the unpack script via ssh on the server ..."
 sshpass -p "${DEPLOY_USER_PASSWORD}" ssh ${DEPLOY_USER}@${DEPLOY_HOST} "cd /home/${DEPLOY_USER}/ \
 && sudo ./unpack_deployment_and_install.sh \
-$today ${EUMS_HOST} ${RAPIDPRO_API_TOKEN} ${EMAIL_PASSWORD} \
-${VISION_USER} ${VISION_PASSWORD} ${VISION_BUSINESS_AREA_CODE} ${TIME_ZONE} ${DJANGO_SECRET_KEY} \
-${MAP_LATITUDE} ${MAP_LONGITUDE} ${MAP_LEVEL}"
+$today '${EUMS_HOST}' '${RAPIDPRO_API_TOKEN}' '${EMAIL_PASSWORD}' \
+'${VISION_USER}' '${VISION_PASSWORD}' '${VISION_BUSINESS_AREA_CODE}' '${TIME_ZONE}' '${DJANGO_SECRET_KEY}' \
+'${MAP_LATITUDE}' '${MAP_LONGITUDE}' '${MAP_LEVEL}'"
 
 # uninstall ssh-pass
 sudo apt-get -y --purge remove sshpass
