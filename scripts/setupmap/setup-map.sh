7a65b9aafeac83fda5cf597c4eadbc56fbc4f6fd
bfeng <fungbo@126.com>
2016-01-20 18:00:14 +0800

    [#X] change district name locator to 'name' when we apply a new map

--- scripts/setupmap/setup-map.sh
+++ scripts/setupmap/setup-map.sh
@@ -21,17 +21,22 @@ if [ -f /opt/map/districts.json ]; then
     cd /opt/app/eums/eums/client/config
     sed -i.bak -e 's/"CENTER": \[\(.*\), \(.*\)\],/"CENTER": \['${LATITUDE}', '${LONGITUDE}'],/g' development.json
     sed -i.bak -e 's/"ZOOM_LEVEL": \(.*\)/"ZOOM_LEVEL": '${ZOOM_LEVEL}'/g' development.json
+    sed -i.bak -e 's/"DISTRICT_NAME_LOCATOR": \(.*\)/"DISTRICT_NAME_LOCATOR": "name"/g' development.json
     rm development.json.bak
     sed -i.bak -e 's/"CENTER": \[\(.*\), \(.*\)\],/"CENTER": \['${LATITUDE}', '${LONGITUDE}'],/g' environment.json
     sed -i.bak -e 's/"ZOOM_LEVEL": \(.*\)/"ZOOM_LEVEL": '${ZOOM_LEVEL}'/g' environment.json
+    sed -i.bak -e 's/"DISTRICT_NAME_LOCATOR": \(.*\)/"DISTRICT_NAME_LOCATOR": "name"/g'  environment.json
     rm environment.json.bak
     sed -i.bak -e 's/"CENTER": \[\(.*\), \(.*\)\],/"CENTER": \['${LATITUDE}', '${LONGITUDE}'],/g' production.json
     sed -i.bak -e 's/"ZOOM_LEVEL": \(.*\)/"ZOOM_LEVEL": '${ZOOM_LEVEL}'/g' production.json
+    sed -i.bak -e 's/"DISTRICT_NAME_LOCATOR": \(.*\)/"DISTRICT_NAME_LOCATOR": "name"/g'  production.json
     rm production.json.bak
     sed -i.bak -e 's/"CENTER": \[\(.*\), \(.*\)\],/"CENTER": \['${LATITUDE}', '${LONGITUDE}'],/g' staging.json
     sed -i.bak -e 's/"ZOOM_LEVEL": \(.*\)/"ZOOM_LEVEL": '${ZOOM_LEVEL}'/g' staging.json
+    sed -i.bak -e 's/"DISTRICT_NAME_LOCATOR": \(.*\)/"DISTRICT_NAME_LOCATOR": "name"/g'  staging.json
     rm staging.json.bak
     sed -i.bak -e 's/"CENTER": \[\(.*\), \(.*\)\],/"CENTER": \['${LATITUDE}', '${LONGITUDE}'],/g' test.json
     sed -i.bak -e 's/"ZOOM_LEVEL": \(.*\)/"ZOOM_LEVEL": '${ZOOM_LEVEL}'/g' test.json
+    sed -i.bak -e 's/"DISTRICT_NAME_LOCATOR": \(.*\)/"DISTRICT_NAME_LOCATOR": "name"/g'  test.json
     rm test.json.bak
 fi
