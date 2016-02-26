8931f31ebdf1a9d825c2ddf1b9d77e4d4150c32a
bfeng <fungbo@126.com>
2016-02-26 16:47:07 +0800

    [#X] shenjian & bo - fix the bug that superuser don't have the group

--- eums/api/current_user/current_user_endpoint.py
+++ eums/api/current_user/current_user_endpoint.py
@@ -1,10 +1,16 @@
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from eums.auth import GROUP_UNICEF_ADMIN
+
 
 @csrf_exempt
 def current_user(request):
     user_profile = getattr(request.user, 'user_profile', None)
+    try:
+        group = request.user.groups.values_list('name')[0][0]
+    except IndexError:
+        group = GROUP_UNICEF_ADMIN
 
     data = {
         'userid': request.user.id,
@@ -12,7 +18,7 @@ def current_user(request):
         'first_name': request.user.first_name,
         'last_name': request.user.last_name,
         'email': request.user.email,
-        'group': request.user.groups.values_list('name')[0][0],
+        'group': group,
         'consignee_id': request.user.user_profile.consignee_id if user_profile else None
     }
     return JsonResponse(data)
