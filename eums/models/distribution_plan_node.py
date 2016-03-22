f61a6d482c8488a1b1b7cacdcbb0bf827b3d143f
WuLingzi <lotuswlz@sina.com>
2016-03-22 17:12:11 +0800

    Revert "[#X] all items sent from IP will be tracked"
    This reverts commit b806bbbbd9c386c87ebddfa4bc209836d446b4aa.

--- eums/models/distribution_plan_node.py
+++ eums/models/distribution_plan_node.py
@@ -146,7 +146,7 @@ class DistributionPlanNode(Runnable):
             if self.is_assigned_to_self:
                 self.track = False
             else:
-                self.track = True
+                self.track = self.arcs_in.filter(source__track=True).exists()
             self.save()
 
     def update_balance(self):
