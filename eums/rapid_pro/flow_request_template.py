7c3596ac685338c7a8197091f0d9eda2682981c8
yingguo <yingguo@thoughtworks.com>
2017-05-02 18:06:12 +0800

    api migration to V2 : fix bug of failing sending text

--- eums/rapid_pro/flow_request_template.py
+++ eums/rapid_pro/flow_request_template.py
@@ -4,8 +4,7 @@ from eums.request.dict_template import DictTemplate
 class FlowRequestTemplate(DictTemplate):
     payload = {
         "flow": '${flow}',
-        "phone": ['${phone}'],
-        "contacts": ['${contact_id}'],
+        "urns": ['tel:${phone}'],
         "extra": {
             'contactName': '${contact_name}',
             'sender': '${sender}',
